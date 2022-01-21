0d50fe3046c8e74b87a78a25fa50953e5262e7da
Nate Rush <naterush1997@gmail.com>
2022-01-21 09:19:33 -0500

    add description

--- trymito.io/pages/_app.tsx
+++ trymito.io/pages/_app.tsx
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="description" content="Mito is the fastest way to do Python data science. Edit your data in a spreadsheet, and generate Python code automatically."/>
       </Head>
       <Component {...pageProps} />
     </>
