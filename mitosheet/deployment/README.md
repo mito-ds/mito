81e4ad47bd07879056a669c8524a72e398d4a83f
Nate Rush <naterush1997@gmail.com>
2022-01-27 11:37:25 -0800

    mitosheet: add deployment scripts and workflow

---
+++ mitosheet/deployment/README.md
@@ -0,0 +1,3 @@
+# Deployment
+
+This folder contains files helpful in bumping the version of the current package, and for deploying this to PyPi (or TestPyPi).
---
+++ mitosheet/deployment/deploy.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Mito.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+File that contains utilities for deploying a new version of Mito to PyPi,
+assuming the correct PyPi credentials are on the machine.
+"""
+import subprocess
+import sys
+
+
+def deploy_current_mito_version_to_pypi(on_dev: bool):
+    """
+    Deploys the current local version of Mito to PyPi.
+    """
+    cmd = []
+    if on_dev:
+        cmd = ["python3", "setup.py", "sdist", "bdist_wheel", "upload", "--repository", "testpypi"]
+    else:
+        cmd = ["python3", "setup.py", "sdist", "bdist_wheel", "upload"]
+
+    deploy_results = subprocess.run(
+        cmd,
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE,
+        universal_newlines=True
+    )
+    if deploy_results.returncode != 0:
+        raise Exception("Failed to deploy to PyPi with output:", deploy_results.stdout, deploy_results.stderr)
+
+
+def main():
+    """
+    Deploy to PyPi with `python3 deployment/deploy.py [dev | main]`.
+
+    Note that it will deploy whatever package is currently the defined package
+    in the package.json, which could either be mitosheet or mitosheet3. 
+    """
+
+    # We either deploy app or staging, default staging
+    if len(sys.argv) > 1:
+        deploy_location = sys.argv[1]
+    else:
+        raise Exception(f'Please choose a valid deploy location: dev | main')
+        
+    if deploy_location not in ['dev', 'main']:
+        raise Exception(f'Invalid deploy location: {deploy_location}. Please choose from dev | main.')
+
+    # Then, we actually deploy Mito to PyPi, if it has not been deployed yet
+    deploy_current_mito_version_to_pypi(deploy_location == 'dev')
+
+if __name__ == '__main__':
+    main()
