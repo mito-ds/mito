da701d8f51011eaca74f7833307c4a2819b90d94
Nate Rush <naterush1997@gmail.com>
2022-02-14 16:08:17 -0800

    mitosheet: fix inverse logic in test decorators

--- mitosheet/mitosheet/tests/decorators.py
+++ mitosheet/mitosheet/tests/decorators.py
@@ -9,12 +9,12 @@ from mitosheet.saved_analyses.schema_utils import is_prev_version
 
 
 pandas_pre_1_only = pytest.mark.skipif(
-    pd.__version__.startswith('0.'), 
+    pd.__version__.startswith('1.'), 
     reason='This test only runs on earlier versions of Pandas. API inconsistencies make it fail on earlier versions'
 )
 
 pandas_post_1_only = pytest.mark.skipif(
-    pd.__version__.startswith('1.'), 
+    pd.__version__.startswith('0.'), 
     reason='This test only runs on later versions of Pandas. API inconsistencies make it fail on earlier versions'
 )
 
