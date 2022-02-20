743fd995b1c06b928edbbcff7e6a1c66ee19aaf0
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2022-02-20 18:01:55 -0500

    mitosheet: incorporate the safety filter toggle into the graph param state

--- mitosheet/src/components/elements/Toggle.tsx
+++ mitosheet/src/components/elements/Toggle.tsx
@@ -7,12 +7,14 @@ import '../../../css/elements/Toggle.css'
 
 interface ToggleProps {
     /** 
-        * @param value - The state of the toggle. On or Off.
+        * @param value - The state of the toggle. True (On) or False (Off)
     */
     value: boolean;
 
-
-    onChange?: (newValue: string) => void;
+    /**
+        * @param onChange - Handles actually changing the value of the filter 
+    */
+    onChange: () => void;
 }
 
 /**
@@ -22,15 +24,21 @@ interface ToggleProps {
  * The Toggle component utilizes the html input element with a type checkbox, so that we 
  * follow the design patterns of html elements. In order to create our custom designed toggle, 
  * we hide the default html checkbox and style the label property. 
+ * 
+ * Inspired by: https://stackoverflow.com/questions/39846282/how-to-add-the-text-on-and-off-to-toggle-button and 
+ * https://www.youtube.com/watch?v=N8BZvfRD_eU
  */
 const Toggle = (props: ToggleProps): JSX.Element => {
-    // const [isOn, setIsOn] = useState(props.value)
-    console.log(props)
 
     return (
-        <label className="switch">
-            <input type="checkbox" />
-            <div className="slider round"></div>
+        <label className="toggle-label" >
+            {/* 
+                Its important that the onClick event handler be on the input instead of the label because
+                when the label is clicked, it also triggers the input's onClick event. If the onClick was registered
+                on the label, this would cause it to be fired twice each time the user clicks.  
+            */}
+            <input type="checkbox" checked={props.value} onClick={() => { props.onChange() }} />
+            <div className="toggle"></div>
         </label>
     )
 }
