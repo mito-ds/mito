19caf42a88d982a5db1faae4f608acb9b086ddc5
Nate Rush <naterush1997@gmail.com>
2022-02-28 13:58:53 -0800

    mitosheet: fix unexplicit promise

--- mitosheet/src/components/Mito.tsx
+++ mitosheet/src/components/Mito.tsx
@@ -164,7 +164,7 @@ export const Mito = (props: MitoProps): JSX.Element => {
 
         // We log that the mitosheet has rendered explicitly, so that we can
         // tell if an installation is broken
-        props.mitoAPI.sendLogMessage('mitosheet_rendered');
+        void props.mitoAPI.sendLogMessage('mitosheet_rendered');
 
         return () => {
             if (window.setMitoStateMap) {
