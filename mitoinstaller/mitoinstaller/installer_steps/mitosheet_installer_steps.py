a03d5fb0be8cb94eb2f6ff1ce38b10541190d85a
Nate Rush <naterush1997@gmail.com>
2022-04-20 16:01:18 -0400

    mitoinstaller: add actual install commands

--- mitoinstaller/mitoinstaller/installer_steps/mitosheet_installer_steps.py
+++ mitoinstaller/mitoinstaller/installer_steps/mitosheet_installer_steps.py
@@ -55,9 +55,8 @@ def install_step_mitosheet_install_mitosheet():
     install_pip_packages('mitosheet', test_pypi='--test-pypi' in sys.argv)
 
 def install_step_mitosheet_activate_notebook_extension():
-    #run_command(['jupyter', 'nbextension', 'install', 'mitosheet'])
-    #run_command(['jupyter', 'nbextension', 'enable', 'mitosheet'])
-    print("TODO: fix this step when mitosheet for notebooks is deployed!")
+    run_command(['jupyter', 'nbextension', 'install', '--py', '--user', 'mitosheet'])
+    run_command(['jupyter', 'nbextension', 'enable', '--py', '--user', 'mitosheet'])
 
     
 MITOSHEET_INSTALLER_STEPS = [
