75047d328c7bff1d79ec216ee603539fba83d92c
Nate Rush <naterush1997@gmail.com>
2022-06-13 12:45:08 -0700

    mitosheet: fix tests

--- mitosheet/mitosheet/tests/step_performers/test_promote_row_to_header.py
+++ mitosheet/mitosheet/tests/step_performers/test_promote_row_to_header.py
@@ -10,6 +10,7 @@ Contains tests for Promote Row To Header
 import pandas as pd
 import pytest
 from mitosheet.tests.test_utils import create_mito_wrapper_dfs
+from mitosheet.tests.decorators import pandas_post_1_2_only
 
 PROMOTE_ROW_TO_HEADER_TESTS = [
     (
@@ -40,26 +41,6 @@ PROMOTE_ROW_TO_HEADER_TESTS = [
         'A', 
         [
             pd.DataFrame({1: [2, 3]}, index=['B', 'C'])
-        ]
-    ),
-    (
-        [
-            pd.DataFrame({'A': [1, 2, 3]}, index=pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']))
-        ],
-        0, 
-        pd.to_datetime('12-22-1997'), 
-        [
-            pd.DataFrame({1: [2, 3]}, index=pd.to_datetime(['12-23-1997', '12-24-1997']))
-        ]
-    ),
-    (
-        [
-            pd.DataFrame({'A': [1, 2, 3]}, index=pd.to_timedelta(['1 days', '2 days', '3 days']))
-        ],
-        0, 
-        pd.to_timedelta('1 days'), 
-        [
-            pd.DataFrame({1: [2, 3]}, index=pd.to_timedelta(['2 days', '3 days']))
         ]
     ),
     (
@@ -105,6 +86,39 @@ PROMOTE_ROW_TO_HEADER_TESTS = [
 ]
 @pytest.mark.parametrize("input_dfs, sheet_index, index, output_dfs", PROMOTE_ROW_TO_HEADER_TESTS)
 def test_fill_na(input_dfs, sheet_index, index, output_dfs):
+    mito = create_mito_wrapper_dfs(*input_dfs)
+
+    mito.promote_row_to_header(sheet_index, index)
+
+    assert len(mito.dfs) == len(output_dfs)
+    for actual, expected in zip(mito.dfs, output_dfs):
+        assert actual.equals(expected)
+
+
+DATE_INDEX_TESTS = [(
+        [
+            pd.DataFrame({'A': [1, 2, 3]}, index=pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']))
+        ],
+        0, 
+        pd.to_datetime('12-22-1997'), 
+        [
+            pd.DataFrame({1: [2, 3]}, index=pd.to_datetime(['12-23-1997', '12-24-1997']))
+        ]
+    ),
+    (
+        [
+            pd.DataFrame({'A': [1, 2, 3]}, index=pd.to_timedelta(['1 days', '2 days', '3 days']))
+        ],
+        0, 
+        pd.to_timedelta('1 days'), 
+        [
+            pd.DataFrame({1: [2, 3]}, index=pd.to_timedelta(['2 days', '3 days']))
+        ]
+    ),
+]
+@pandas_post_1_2_only
+@pytest.mark.parametrize("input_dfs, sheet_index, index, output_dfs", DATE_INDEX_TESTS)
+def test_promote_row_to_header_timedelta_index(input_dfs, sheet_index, index, output_dfs):
     mito = create_mito_wrapper_dfs(*input_dfs)
 
     mito.promote_row_to_header(sheet_index, index)
