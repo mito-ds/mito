09c941dcd4c5ead108181ebf1824ea2d92c042b6
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2022-06-14 12:50:54 -0400

    mitohseet: add new icons for recent step types

---
+++ mitosheet/src/components/icons/SplitTextToColumnsIcon.tsx
@@ -0,0 +1,29 @@
+// Copyright (c) Mito
+
+import React from 'react';
+
+const SplitTextToColumnsIcon = (): JSX.Element => {
+    return (
+        <svg width="12" height="14" viewBox="0 0 12 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="0.5" y="0.5" width="11" height="13" rx="0.5" stroke="#494650"/>
+            <path d="M5 3.5L2 3.49999" stroke="#494650" strokeMiterlimit="10" strokeLinecap="round"/>
+            <path d="M5 7L2 6.99999" stroke="#494650" strokeMiterlimit="10" strokeLinecap="round"/>
+            <path d="M5 10.5L2 10.5" stroke="#494650" strokeMiterlimit="10" strokeLinecap="round"/>
+            <path d="M5 3.5L2 3.49999" stroke="#494650" strokeMiterlimit="10" strokeLinecap="round"/>
+            <path d="M5 7L2 6.99999" stroke="#494650" strokeMiterlimit="10" strokeLinecap="round"/>
+            <path d="M5 10.5L2 10.5" stroke="#494650" strokeMiterlimit="10" strokeLinecap="round"/>
+            <path d="M10 3.5L7 3.49999" stroke="#494650" strokeMiterlimit="10" strokeLinecap="round"/>
+            <path d="M10 7L7 6.99999" stroke="#494650" strokeMiterlimit="10" strokeLinecap="round"/>
+            <path d="M10 10.5L7 10.5" stroke="#494650" strokeMiterlimit="10" strokeLinecap="round"/>
+        </svg>
+    )
+}
+
+export default SplitTextToColumnsIcon;
+
+
+
+
+
+
+
