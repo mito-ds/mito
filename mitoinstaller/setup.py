e4c364d0b34a8b1000d81bd5eb726335abdf5c09
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2022-08-21 11:17:40 -0400

    mitoinstaller: add helpful params

--- mitoinstaller/setup.py
+++ mitoinstaller/setup.py
@@ -3,10 +3,14 @@ from mitoinstaller import __version__
 from setuptools import find_packages
 
 setup(
-    name='mitoinstaller',
-    version=__version__,
-    packages=find_packages(where='.', exclude='tests'),
-    install_requires=['analytics-python', 'colorama', 'termcolor'],
-    license='AGPL-3.0-only',
-    long_description='The mitoinstaller package allows you to install the mitosheet package in your local JupyterLab instance. It logs anonymous data about installation, including if it is successful or why it failed.',
+    name                = 'mitoinstaller',
+    version             = __version__,
+    packages            = find_packages(where='.', exclude='tests'),
+    install_requires    = ['analytics-python', 'colorama', 'termcolor'],
+    author              = 'Mito',
+    author_email        = 'naterush1997@gmail.com',
+    license             = 'AGPL-3.0-only',
+    url                 = 'https://github.com/mito-ds/monorepo',
+    long_description    = 'The mitoinstaller package allows you to install the mitosheet package in your local JupyterLab instance. It logs anonymous data about installation, including if it is successful or why it failed.',
+    keywords            = ['Jupyter', 'Widgets', 'IPython'],
 )
