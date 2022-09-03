2ce06735060bb54444b0441a83e7b4a618ec2566
Nate Rush <naterush1997@gmail.com>
2022-09-03 14:45:32 -0700

    metaprogramming: cleanup files, make way for new metaprogramming

---
+++ mitosheet/dev/metaprogramming/__main__.py
@@ -0,0 +1,49 @@
+
+import argparse
+import traceback
+
+from metaprogramming.create_new_step import create_new_step
+from metaprogramming.create_new_icon import create_new_icon
+
+
+def execute_create_new_step(args: argparse.Namespace) -> None:
+    create_new_step()
+
+def execute_create_new_icon(args: argparse.Namespace) -> None:
+    create_new_icon(args.name)
+
+
+def execute_create_new_taskpane(args: argparse.Namespace) -> None:
+    print("HERE t")
+    pass
+
+
+
+def main() -> None:
+    """
+    Responsible for running the meta programming commands
+    """
+
+    parser = argparse.ArgumentParser(description='Run a metaprogramming command')
+    subparsers = parser.add_subparsers()
+
+    parser_create_new_step = subparsers.add_parser('step', help='Create a new step.')
+    parser_create_new_step.set_defaults(func=execute_create_new_step)
+
+    parser_create_new_icon = subparsers.add_parser('icon', help='Create a new icon from the most recent item in the downloads folder.')
+    parser_create_new_icon.add_argument('--name', required=True, help='The name of the icon to create. E.g. UpArrow, DownArrow')
+    parser_create_new_icon.set_defaults(func=execute_create_new_icon)
+
+    parser_create_new_taskpane = subparsers.add_parser('taskpane', help='Create a new taskpane.')
+    parser_create_new_taskpane.add_argument('--name', required=True, help='The name of the taskpane to create. E.g. Download, Import')
+    parser_create_new_taskpane.set_defaults(func=execute_create_new_taskpane)
+    args = parser.parse_args()
+
+    try:
+        args.func(args)
+    except Exception as e:
+        print(traceback.format_exc())
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
