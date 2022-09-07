834d094d52dfeb064353bf9148beede6d38a9b83
Nate Rush <naterush1997@gmail.com>
2022-09-07 16:49:09 -0400

    mitosheet: move to 3.7

--- mitosheet/setup.py
+++ mitosheet/setup.py
@@ -249,10 +249,10 @@ elif name == 'mitosheet' or name == 'mitosheet3' or name == 'mitosheet-private':
             "License :: OSI Approved :: GNU Affero General Public License v3",
             "Programming Language :: Python",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
             "Framework :: Jupyter",
         ],
     )
