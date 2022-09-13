58710e6c57f79dc6cf94965d14f6a494544ec81d
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2022-09-13 11:01:32 -0400

    mitosheet: style button

--- mitosheet/src/components/icons/JLabIcon/MitoIcon.ts
+++ mitosheet/src/components/icons/JLabIcon/MitoIcon.ts
@@ -3,8 +3,16 @@ import { LabIcon } from '@jupyterlab/ui-components';
 
 export const mitoJLabIcon = new LabIcon({
     name: 'mitosheet:mitosheet-jlab-icon',
-    svgstr: '<svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="0.871094" y="2.87109" width="5.52567" height="5.3994" fill="#D0B9FE"/><ellipse cx="3.63393" cy="2.80053" rx="2.76283" ry="2.0603" fill="#D0B9FE"/><ellipse cx="3.63393" cy="8.20092" rx="2.76283" ry="2.0603" fill="#D0B9FE"/></svg>'
-
+    svgstr: '<svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg">\
+    <rect x="0.871094" y="2.87109" width="5.52567" height="5.3994" fill="#D0B9FE"/>\
+    <ellipse cx="3.63393" cy="2.80053" rx="2.76283" ry="2.0603" fill="#D0B9FE"/>\
+    <ellipse cx="3.63393" cy="8.20092" rx="2.76283" ry="2.0603" fill="#D0B9FE"/>\
+    <path d="M0.871094 2.94922H3.2966C3.57827 2.94922 3.80661 3.17755 3.80661 3.45922V3.45922C3.80661 3.74088 3.57827 3.96922 3.29661 3.96922H0.871094V2.94922Z" fill="#9D6CFF"/>\
+    <path d="M0.871094 7.03125H3.2966C3.57827 7.03125 3.80661 7.25958 3.80661 7.54125V7.54125C3.80661 7.82292 3.57827 8.05125 3.29661 8.05125H0.871094V7.03125Z" fill="#9D6CFF"/>\
+    <path d="M6.39673 4.99023H3.97122C3.68955 4.99023 3.46122 5.21857 3.46122 5.50023V5.50023C3.46122 5.7819 3.68955 6.01023 3.97122 6.01023H6.39673V4.99023Z" fill="#9D6CFF"/>\
+    </svg>'
 });
+
+
 
 
