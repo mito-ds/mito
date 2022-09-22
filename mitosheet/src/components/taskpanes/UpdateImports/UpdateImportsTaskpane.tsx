573d1847ca686d26f30215a9890717f07f2d7216
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2022-09-22 08:55:16 -0400

    mitosheet: generate new taskpane

---
+++ mitosheet/src/components/taskpanes/UpdateImports/UpdateImportsTaskpane.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import MitoAPI from "../../../jupyter/api";
+import { AnalysisData, SheetData, UIState, UserProfile } from "../../../types"
+
+import DefaultTaskpane from "../DefaultTaskpane/DefaultTaskpane";
+import DefaultTaskpaneBody from "../DefaultTaskpane/DefaultTaskpaneBody";
+import DefaultTaskpaneHeader from "../DefaultTaskpane/DefaultTaskpaneHeader";
+
+
+interface updateImportsTaskpaneProps {
+    mitoAPI: MitoAPI;
+    userProfile: UserProfile;
+    setUIState: React.Dispatch<React.SetStateAction<UIState>>;
+    analysisData: AnalysisData;
+    sheetDataArray: SheetData[];
+    selectedSheetIndex: number;
+}
+
+
+
+
+
+/* 
+    This is the updateImports taskpane.
+*/
+const updateImportsTaskpane = (props: updateImportsTaskpaneProps): JSX.Element => {
+
+    
+
+    
+
+    
+
+    
+
+    return (
+        <DefaultTaskpane>
+            <DefaultTaskpaneHeader 
+                header="Update Imports"
+                setUIState={props.setUIState}           
+            />
+            <DefaultTaskpaneBody>
+                
+            </DefaultTaskpaneBody>
+        </DefaultTaskpane>
+    )
+}
+
+export default updateImportsTaskpane;
