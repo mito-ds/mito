3b50d4454fe50cd16e2f37f1cca626cf4244f739
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2022-09-29 10:50:30 -0400

    mitosheet: update existing_import_update to work with multi sheet import steps

--- mitosheet/mitosheet/updates/existing_imports.py
+++ mitosheet/mitosheet/updates/existing_imports.py
@@ -3,7 +3,7 @@
 
 # Copyright (c) Saga Inc.
 # Distributed under the terms of the GPL License.
-from ast import Dict
+from copy import copy
 from typing import Any, List
 from mitosheet.step import Step
 from mitosheet.types import StepsManagerType
@@ -11,55 +11,75 @@ from mitosheet.step_performers import EVENT_TYPE_TO_STEP_PERFORMER
 from mitosheet.step_performers.dataframe_import import DataframeImportStepPerformer
 from mitosheet.step_performers.import_steps.excel_import import ExcelImportStepPerformer
 from mitosheet.step_performers.import_steps.simple_import import SimpleImportStepPerformer
+from mitosheet.utils import create_step_id
 
 
 EXISTING_IMPORTS_UPDATE_EVENT = 'existing_import_update'
 EXISTING_IMPORTS_PARAMS = ['updated_imports']
 
+
 def execute_existing_imports_update(steps_manager: StepsManagerType, updated_imports) -> None:
+    """
+    Updates the step list with new import steps
 
-    for i in range(len(steps_manager.steps_including_skipped)):
+    We must preserve the order of the sheets within the import. Consider this example:
+    In original step (step id: abc): imports sheet_1 and sheet_2 from .xlsx file
+    In updated imports, replaces those files with file_1.csv and file_2.csv
+    The updated_import obj for file_1 and file_2 will both have step id: abc, but we must ensure
+    that file_1.csv gets imported before file_2.csv so it replaces the correct file. 
+
+    Assumptions:
+    1. If we add multiple sheets in one step, the sheets get added to state in the same order as the params
+    """
+    # Note: we use while loop so we can update the length of the step list
+    # in this algorithm and still reach the end of the step list
+    i = 0
+    while i < len(steps_manager.steps_including_skipped):
         step = steps_manager.steps_including_skipped[i]
+        number_of_times_this_step_updated = 0
 
-        final_import_updates = []
-        
-        #TODO: Combine the updated_imports by step id so if there are multiple updates to one step, we can handle it properly
         for updated_import in updated_imports:
-            updated_import_step_id = updated_import['step_id']
-
-            # Find other steps with the same step_id
-                # If they are the same import type, combine them
-                # If they are different import types, create a new step id for one of them and add it to the end as new import step
-                new_simple_import_steps = []
-                new_excel_import_steps = []
-                new_df_import_step = []
-
-        # For each import that we're updating, find the original import step and update the params
-        for updated_import in updated_imports:
-            updated_import_step_id = updated_import['step_id']
         
-            if updated_import_step_id == step.step_id:
+            if updated_import['step_id'] != step.step_id:
+                continue 
 
+            if number_of_times_this_step_updated > 0:
+                # If we've already updated this step, then create a new step_id so we don't
+                # overwrite the previous step update.
+                # TODO: We should combine steps that are the same step_id and have the same configuration. 
+                # For example, if importing 2 sheets from the same .xlsx file with the same configuration, 
+                # that should just be one step. 
+                updated_import['step_id'] = create_step_id()
+            
+            # Update the step_type
+            updated_import_type = updated_import['type']
+            if updated_import_type == 'csv':
+                step_type = SimpleImportStepPerformer.step_type()
+            if updated_import_type == 'excel':
+                step_type = ExcelImportStepPerformer.step_type()
+            if updated_import_type == 'df':
+                step_type = DataframeImportStepPerformer.step_type()
+                
+            step_performer = EVENT_TYPE_TO_STEP_PERFORMER[step_type + '_edit']
 
-                # Update the step_type
-                updated_import_type = updated_import['type']
-                if updated_import_type == 'csv':
-                    step.step_type = SimpleImportStepPerformer.step_type()
-                if updated_import_type == 'excel':
-                    step.step_type = ExcelImportStepPerformer.step_type()
-                if updated_import_type == 'df':
-                    step.step_type = DataframeImportStepPerformer.step_type()
-                    
-                step_performer = EVENT_TYPE_TO_STEP_PERFORMER[step.step_type + '_edit']
+            # Create the new step from the import_params
+            new_step = Step(
+                step_performer.step_type(), updated_import['step_id'], updated_import["import_params"]
+            )
 
-                # First, we make a new step
-                new_step = Step(
-                    step_performer.step_type(), updated_import_step_id, updated_import["import_params"]
-                )
+            if number_of_times_this_step_updated > 0:
+                # Insert the new step without overwriting any existing steps
+                steps_manager.steps_including_skipped.insert(i + number_of_times_this_step_updated, new_step)
+            else: 
+                # Overwrite existing step with new step
+                steps_manager.steps_including_skipped[i + number_of_times_this_step_updated] = new_step
+            
+            # Mark that we've already replaced this step_id so that if we 
+            # try to update it again, we create a new step
+            number_of_times_this_step_updated = number_of_times_this_step_updated + 1
+            
+        i = i + 1
 
-                step.params = new_step.params
-
-    
     # Refresh the anlaysis starting from the first step
     steps_manager.execute_and_update_steps(steps_manager.steps_including_skipped, 0)
 
