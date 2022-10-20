b5a3edbc692ce9c3a9a48c80462c6b8fb9267129
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2022-10-20 14:27:17 -0400

    mitosheet: cleanup

--- mitosheet/mitosheet/mito_config.py
+++ mitosheet/mitosheet/mito_config.py
@@ -4,7 +4,6 @@
 # Copyright (c) Saga Inc.
 # Distributed under the terms of the GPL License.
 
-import json
 from typing import Any, Dict, Optional
 
 # These variables must match the variables defined in 
@@ -12,6 +11,7 @@ from typing import Any, Dict, Optional
 MEC_VERSION = 'mec_version'
 SUPPORT_EMAIL = 'support_email'
 
+# The default values to use if the mec does not define them
 DEFAULT_SUPPORT_EMAIL = 'founders@sagacollab.com'
 
 # When updating the MEC_VERSION, add a function here 
@@ -32,27 +32,27 @@ def upgrade_mito_enterprise_configuration(mec: Optional[Dict[str, Any]]) -> Opti
 
 class MitoConfig:
     """
-    The MitoConfig class is repsonsible for reading in the settings from the 
-    mito_helper_configuration package if it is installed and returning the configuration variables
-    to be used by the rest of the app. 
+    The MitoConfig class is repsonsible for 1) reading the settings from the 
+    mito_helper_configuration package if it is installed and 2) returning the configuration variables
+    used by the rest of the app. 
 
     If the mito_helper_configuration package does not exist or does not set every configuration option, 
     the MitoConfig class sets defaults. 
     """
     def __init__(self, mito_enterprise_configuration: Optional[Dict[str, Any]]):
-        print(mito_enterprise_configuration)
         self.mec = upgrade_mito_enterprise_configuration(mito_enterprise_configuration)
-
 
     @property
     def support_email(self) -> str:
-        if self.mec is None:
+        if self.mec is None or self.mec[SUPPORT_EMAIL] is None:
             return DEFAULT_SUPPORT_EMAIL
         return self.mec[SUPPORT_EMAIL]
+
+    # Add new mito configuration options here ...
 
     @property 
     def mito_config(self) -> Dict[str, Any]:
         return {
-                SUPPORT_EMAIL: self.support_email
-            }
+            SUPPORT_EMAIL: self.support_email
+        }
 
