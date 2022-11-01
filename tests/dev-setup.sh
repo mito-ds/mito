1e2fdf259806525f72b90d2390bcbe49a919b289
Nate Rush <naterush1997@gmail.com>
2022-11-01 14:41:49 -0700

    tests: add frontend test of mito

--- tests/dev-setup.sh
+++ tests/dev-setup.sh
@@ -1,3 +1,26 @@
 
+# Create venv and install requirements
+python3 -m venv venv;
+source venv/bin/activate;
+pip install -r requirements.txt;
 
-pip install -r requirements.txt
+# Install necessary node packages
+npm install
+
+# Install mitosheet
+cd ../mitosheet
+
+# Install Python dependencies
+pip install -e ".[test, deploy]"
+
+# Install the npm dependences
+npm install
+
+# Setup JupyterLab development
+jupyter labextension develop . --overwrite
+
+# Setup Jupyter Notebook development
+jupyter nbextension uninstall mitosheet # NOTE: not sure why this first is needed. Somehow, it gets installed in the setup.py...
+jupyter nbextension install --py --symlink --sys-prefix mitosheet
+jupyter nbextension enable --py --sys-prefix mitosheet   
+
