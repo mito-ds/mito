20665ef465a2df815c1e8876baae110c221f9c6a
Aaron Diamond-Reivich <aarondr77@gmail.com>
2022-12-19 14:18:27 -0500

    mitosheet: add utility for mito_config tests

--- mitosheet/mitosheet/tests/test_mito_config.py
+++ mitosheet/mitosheet/tests/test_mito_config.py
@@ -14,6 +14,14 @@ def delete_env_var_if_exists(env_var: str) -> None:
     if os.environ.get(env_var) is not None:
         del os.environ[env_var]
 
+def delete_all_mito_config_environment_variables() -> None:
+    """
+    Deletes all of the MITO CONFIG environment variables that are used in the MEC
+    """
+    delete_env_var_if_exists(MITO_CONFIG_KEY_VERSION)
+    delete_env_var_if_exists(MITO_CONFIG_KEY_SUPPORT_EMAIL)
+    delete_env_var_if_exists(MITO_CONFIG_KEY_CODE_SNIPPETS_SUPPORT_EMAIL)
+
 def test_keys_did_not_change():
     # We must not change these keys so we can still read old 
     # mito configs that users have not upgraded to the most 
@@ -23,11 +31,9 @@ def test_keys_did_not_change():
     assert MITO_CONFIG_KEY_SUPPORT_EMAIL == 'MITO_CONFIG_SUPPORT_EMAIL'
     assert MITO_CONFIG_KEY_CODE_SNIPPETS_SUPPORT_EMAIL == 'MITO_CONFIG_CODE_SNIPPETS_SUPPORT_EMAIL'
 
-
 def test_none_works():
     # Delete the environmnet variables so we can test the none condition
-    delete_env_var_if_exists(MITO_CONFIG_KEY_SUPPORT_EMAIL)
-    delete_env_var_if_exists(MITO_CONFIG_KEY_VERSION)
+    delete_all_mito_config_environment_variables()
 
     mito_config = MitoConfig()
     mito_config_dict = mito_config.get_mito_config()
@@ -39,8 +45,7 @@ def test_none_works():
 
 def test_none_config_version_key_is_string():
     # Delete the environmnet variables so we can test the none condition
-    delete_env_var_if_exists(MITO_CONFIG_KEY_SUPPORT_EMAIL)
-    delete_env_var_if_exists(MITO_CONFIG_KEY_VERSION)
+    delete_all_mito_config_environment_variables()
 
     mito_config = MitoConfig()
     mito_config_dict = mito_config.get_mito_config()
@@ -61,8 +66,7 @@ def test_version_2_works():
     }    
 
     # Delete the environmnet variables for the next test
-    delete_env_var_if_exists(MITO_CONFIG_KEY_SUPPORT_EMAIL)
-    delete_env_var_if_exists(MITO_CONFIG_KEY_VERSION)
+    delete_all_mito_config_environment_variables()
 
 def test_mito_config_update_version_1_to_2():
     # Set environment variables
@@ -78,5 +82,4 @@ def test_mito_config_update_version_1_to_2():
     }    
 
     # Delete the environmnet variables for the next test
-    delete_env_var_if_exists(MITO_CONFIG_KEY_SUPPORT_EMAIL)
-    delete_env_var_if_exists(MITO_CONFIG_KEY_VERSION)
+    delete_all_mito_config_environment_variables()
