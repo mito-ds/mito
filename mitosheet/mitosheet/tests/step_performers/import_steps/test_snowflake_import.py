8f5b475b87f5da27c5c1c6cb4d82d40cc06d819a
Aaron Diamond-Reivich <aarondr77@gmail.com>
2023-01-12 11:44:44 -0500

    mitosheet: remove tests we haven't implemented yet

--- mitosheet/mitosheet/tests/step_performers/import_steps/test_snowflake_import.py
+++ mitosheet/mitosheet/tests/step_performers/import_steps/test_snowflake_import.py
@@ -12,27 +12,26 @@ import pytest
 from mitosheet.tests.decorators import python_post_3_6_only
 from mitosheet.tests.test_utils import create_mito_wrapper_dfs
 
-SNOWFLAKEIMPORT_TESTS = [
-    (
-        [
-            pd.DataFrame({'A': [1, 2, 3], 'B': [1.0, 2.0, 3.0], 'C': [True, False, True], 'D': ["string", "with spaces", "and/!other@characters"], 'E': pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']), 'F': pd.to_timedelta(['1 days', '2 days', '3 days'])})
-        ],
-        "put", 
-        "params", 
-        "here",
-        [
-            pd.DataFrame({'A': [1, 2, 3], 'B': [1.0, 2.0, 3.0], 'C': [True, False, True], 'D': ["string", "with spaces", "and/!other@characters"], 'E': pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']), 'F': pd.to_timedelta(['1 days', '2 days', '3 days'])})
-        ]
-    ),
-]
-@python_post_3_6_only
-@pytest.mark.parametrize("input_dfs, snowflake_credentials, query_params, output_dfs", SNOWFLAKEIMPORT_TESTS)
-def test_snowflakeimport(input_dfs, snowflake_credentials, query_params, output_dfs):
-    # mito = create_mito_wrapper_dfs(*input_dfs)
+# SNOWFLAKEIMPORT_TESTS = [
+#     (
+#         [
+#             pd.DataFrame({'A': [1, 2, 3], 'B': [1.0, 2.0, 3.0], 'C': [True, False, True], 'D': ["string", "with spaces", "and/!other@characters"], 'E': pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']), 'F': pd.to_timedelta(['1 days', '2 days', '3 days'])})
+#         ],
+#         "put", 
+#         "params", 
+#         "here",
+#         [
+#             pd.DataFrame({'A': [1, 2, 3], 'B': [1.0, 2.0, 3.0], 'C': [True, False, True], 'D': ["string", "with spaces", "and/!other@characters"], 'E': pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']), 'F': pd.to_timedelta(['1 days', '2 days', '3 days'])})
+#         ]
+#     ),
+# ]
+# @python_post_3_6_only
+# @pytest.mark.parametrize("input_dfs, snowflake_credentials, query_params, output_dfs", SNOWFLAKEIMPORT_TESTS)
+# def test_snowflakeimport(input_dfs, snowflake_credentials, query_params, output_dfs):
+#     mito = create_mito_wrapper_dfs(*input_dfs)
 
-    # mito.snowflakeimport(snowflake_credentials, query_params)
+#     mito.snowflakeimport(snowflake_credentials, query_params)
 
-    # assert len(mito.dfs) == len(output_dfs)
-    # for actual, expected in zip(mito.dfs, output_dfs):
-    #     assert actual.equals(expected)
-    assert True
+#     assert len(mito.dfs) == len(output_dfs)
+#     for actual, expected in zip(mito.dfs, output_dfs):
+#         assert actual.equals(expected)
