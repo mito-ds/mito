62f6140400b91e28c41d87b91796a3e9c15b9c5a
Nate Rush <naterush1997@gmail.com>
2023-01-25 12:03:55 -0800

    mitosheet: bump version, I forgot

--- mitosheet/mitosheet/step_performers/column_steps/set_column_formula.py
+++ mitosheet/mitosheet/step_performers/column_steps/set_column_formula.py
@@ -33,7 +33,7 @@ class SetColumnFormulaStepPerformer(StepPerformer):
 
     @classmethod
     def step_version(cls) -> int:
-        return 2
+        return 3
 
     @classmethod
     def step_type(cls) -> str:
