4a5dd7e71be985badd3cb50ac4fc5a340c5dc6c7
Nate Rush <naterush1997@gmail.com>
2023-03-04 21:19:58 -0700

    mitosheet: support rolling window calculations

--- mitosheet/mitosheet/public/v3/types/decorators.py
+++ mitosheet/mitosheet/public/v3/types/decorators.py
@@ -9,6 +9,7 @@ from typing import Any, Callable
 from datetime import datetime, timedelta
 
 import pandas as pd
+from pandas.core.window import Rolling
 from mitosheet.public.v3.types.utils import CONVERSION_FUNCTIONS
 
 from mitosheet.types import PrimitiveTypeName
@@ -33,14 +34,27 @@ def cast_values_in_arg_to_type(
             final_args = []
             conversion_function = CONVERSION_FUNCTIONS[target_primitive_type_name]
 
+            # For every arguement, go through and cast them to the correct type
             for arg in args:
-                
-                if isinstance(arg, pd.Series):
-                    final_args.append(arg.apply(conversion_function)) # Why doesn't type work?
-                
-                elif is_primitive_value(arg):
-                    final_args.append(conversion_function(arg))
 
+                if is_primitive_value(arg):
+                    final_args.append(conversion_function(arg))
+                
+                elif isinstance(arg, pd.Series):
+                    final_args.append(arg.apply(conversion_function)) # Why doesn't type work?
+
+                elif isinstance(arg, pd.DataFrame):
+                    final_args.append(arg.apply(lambda c: c.apply(conversion_function)))
+
+                elif isinstance(arg, Rolling):
+                    obj = arg.obj
+                    if isinstance(obj, pd.Series):
+                        new_obj = obj.apply(conversion_function)
+                    else:
+                        new_obj = obj.apply(lambda c: c.apply(conversion_function))
+
+                    final_args.append(new_obj.rolling(window=arg.window))
+                
             return sheet_function(*final_args)        
         return wrapped_sheet_function
     return wrap
--- mitosheet/mitosheet/tests/public/v3/sheet_functions/test_sum.py
+++ mitosheet/mitosheet/tests/public/v3/sheet_functions/test_sum.py
@@ -14,22 +14,36 @@ from mitosheet.public.v3.sheet_functions.number_functions import SUM
 
 
 SUM_NUMBER_TESTS = [
+    # Just constant tests
+    ([2, 3], 5),
+    ([2.0, 3.0], 5.0),
+    ([2.0, '$3.0'], 5.0),
+    ([2.0, True], 3.0),
+
+    # Constants and series
     ([2, pd.Series([1,2,3])], pd.Series([3,4,5])),
     ([2.0, pd.Series([1,2,3])], pd.Series([3.0,4.0,5.0])),
     ([2.0, pd.Series([1,2,None])], pd.Series([3.0,4.0,None])),
     ([2.0, pd.Series(['1', '2', '3'])], pd.Series([3.0, 4.0, 5.0])),
     ([2.0, pd.Series(['1.0', '2.0', '3.0'])], pd.Series([3.0, 4.0, 5.0])),
     ([2.0, pd.Series(['$1.00', '$2.00', '$3.00'])], pd.Series([3.0, 4.0, 5.0])),
-    ([2, 3], 5),
-    ([2.0, 3.0], 5.0),
-    ([2.0, '$3.0'], 5.0),
-    ([2.0, True], 3.0),
+    
+    # Dataframes
+    ([pd.DataFrame({'a': [1, 1, 1], 'b': [2, 2, 2]}), pd.Series([1,2,3])], pd.Series([10,11,12])),
+    ([pd.DataFrame({'a': [1, 1, 1], 'b': [2, 2, 2]}), 2], 11),
+    ([pd.DataFrame({'a': ['$1', '$1', '$1'], 'b': [2, None, 2]}), pd.Series([1,2,3])], pd.Series([8.0,9.0,10.0])),
+
+    # Rolling windows
+    ([pd.Series([1, 2, 3]).rolling(window=2), 1], pd.Series([4, 6, 4])), #A0 = B0:B1 + 1
+    ([pd.DataFrame({'B': [1, 2, 3], 'C': [4, 5, 6]}).rolling(window=2), 1], pd.Series([13, 17, 10])), #A0 = B0:C1 + 1
+    ([pd.DataFrame({'B': [1, 2, 3], 'C': [4, 5, 6]}).rolling(window=3), 1], pd.Series([22, 17, 10])), #A0 = B0:C2 + 1
 ]
 
 @pytest.mark.parametrize("_argv,expected", SUM_NUMBER_TESTS)
 def test_sum_works_for_series_and_number(_argv, expected):
     result = SUM(*_argv)
     if isinstance(result, pd.Series):
+        print(result)
         assert result.equals(expected)
     else: 
         assert result == expected
