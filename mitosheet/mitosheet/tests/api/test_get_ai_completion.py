602c91ce201c09268c56e8af50b46993bf005e19
Nate Rush <naterush1997@gmail.com>
2023-04-04 11:18:56 -0700

    mitosheet: add test for mito api rate limit

--- mitosheet/mitosheet/tests/api/test_get_ai_completion.py
+++ mitosheet/mitosheet/tests/api/test_get_ai_completion.py
@@ -5,6 +5,8 @@ import json
 from mitosheet.tests.test_utils import create_mito_wrapper_dfs
 from mitosheet.api.get_ai_completion import get_ai_completion
 from mitosheet.tests.decorators import requires_open_ai_credentials
+from mitosheet.user.db import get_user_field, set_user_field
+from mitosheet.user.schemas import UJ_AI_MITO_API_NUM_USAGES
 
 @requires_open_ai_credentials
 def test_get_ai_completion():
@@ -25,18 +27,54 @@ def test_get_ai_completion():
         # We add this since this test is flakey
         assert len(json.loads(completion)['error']) > 0
 
-@requires_open_ai_credentials
-def test_get_ai_completion_with_no_api_key_errors():
+def test_get_ai_completion_with_no_api_key_works():
     mito = create_mito_wrapper_dfs()
 
-    key = os.environ['OPENAI_API_KEY']
-    del os.environ['OPENAI_API_KEY']
+    if 'OPENAI_API_KEY' in os.environ:
+        key = os.environ['OPENAI_API_KEY']
+        del os.environ['OPENAI_API_KEY']
+        num_usages = get_user_field(UJ_AI_MITO_API_NUM_USAGES)
+    else:
+        key = None
+        num_usages = 0
+
+    set_user_field(UJ_AI_MITO_API_NUM_USAGES, 0)
 
     completion = get_ai_completion({
         'user_input': 'test',
         'selection': None
     }, mito.mito_backend.steps_manager)
 
-    assert len(json.loads(completion)['error']) > 0
+    assert json.loads(completion)['user_input'] == 'test'
+    assert len(json.loads(completion)['prompt_version']) > 0
+    assert len(json.loads(completion)['prompt']) > 0
+    assert len(json.loads(completion)['completion']) > 0
 
-    os.environ['OPENAI_API_KEY'] = key
+    if key is not None:
+        os.environ['OPENAI_API_KEY'] = key
+    set_user_field(UJ_AI_MITO_API_NUM_USAGES, num_usages)
+
+def test_get_ai_completion_with_no_api_key_errors_if_above_rate_limit():
+    mito = create_mito_wrapper_dfs()
+
+    if 'OPENAI_API_KEY' in os.environ:
+        key = os.environ['OPENAI_API_KEY']
+        del os.environ['OPENAI_API_KEY']
+        num_usages = get_user_field(UJ_AI_MITO_API_NUM_USAGES)
+    else:
+        key = None
+        num_usages = 0
+
+    set_user_field(UJ_AI_MITO_API_NUM_USAGES, 20)
+
+    completion = get_ai_completion({
+        'user_input': 'test',
+        'selection': None
+    }, mito.mito_backend.steps_manager)
+
+    assert 'You have used Mito AI 20 times.' in json.loads(completion)['error']
+
+
+    if key is not None:
+        os.environ['OPENAI_API_KEY'] = key
+    set_user_field(UJ_AI_MITO_API_NUM_USAGES, num_usages)
