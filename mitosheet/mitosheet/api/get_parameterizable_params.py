0a22bbed2e53bcc3b3fb349223593124f96b0bce
Nate Rush <naterush1997@gmail.com>
2023-05-01 15:49:25 -0400

    mitosheet: fix up type issue

--- mitosheet/mitosheet/api/get_parameterizable_params.py
+++ mitosheet/mitosheet/api/get_parameterizable_params.py
@@ -5,7 +5,7 @@
 # Distributed under the terms of the GPL License.
 
 import json
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
 from mitosheet.code_chunks.code_chunk_utils import get_code_chunks
 from mitosheet.types import StepsManagerType
 from mitosheet.updates.args_update import is_string_arg_to_mitosheet_call
@@ -14,12 +14,12 @@ from mitosheet.updates.args_update import is_string_arg_to_mitosheet_call
 def get_parameterizable_params(params: Dict[str, Any], steps_manager: StepsManagerType) -> str:
 
 
-        all_parameterizable_params = []
+        all_parameterizable_params: List[Tuple[str, str]] = []
 
         # First, get the original arguments to the mitosheet - we only let you parameterize df names for now
         for arg in steps_manager.original_args_raw_strings:
                 if not is_string_arg_to_mitosheet_call(arg):
-                        all_parameterizable_params.append([arg, 'df_name'])
+                        all_parameterizable_params.append((arg, 'df_name'))
     
         # Get optimized code chunk, and get their parameterizable params
         code_chunks = get_code_chunks(steps_manager.steps_including_skipped[:steps_manager.curr_step_idx + 1], optimize=True)
