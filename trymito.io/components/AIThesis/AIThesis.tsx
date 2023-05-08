547fd7131edc1a32df26479f5e9c9dbc3202cf96
Aaron Diamond-Reivich <aarondr77@gmail.com>
2023-05-08 18:15:04 +0200

    trymito: fix build

--- trymito.io/components/AIThesis/AIThesis.tsx
+++ trymito.io/components/AIThesis/AIThesis.tsx
@@ -40,7 +40,7 @@ const AIThesis = (): JSX.Element => {
                 >
                     <p>
                         The more the AI knows about your data and analysis, the better chance the code it generates is useful. 
-                        Spreadsheets are really good at giving the AI the context it needs because they understand your data's structure, content, and edit history. 
+                        Spreadsheets are really good at giving the AI the context it needs because they understand your data&aspo;s structure, content, and edit history. 
                     </p>
                 </ExpandableCard>
                 <ExpandableCard 
@@ -53,7 +53,7 @@ const AIThesis = (): JSX.Element => {
                 >
                     <p>
                         AIs are far from perfect, but its important that your analysis is.
-                        Spreadsheets are the most efficient tool for catching and correcting the AI's mistakes.
+                        Spreadsheets are the most efficient tool for catching and correcting the AI&aspo;s mistakes.
                     </p>
                 </ExpandableCard>
             </div>
