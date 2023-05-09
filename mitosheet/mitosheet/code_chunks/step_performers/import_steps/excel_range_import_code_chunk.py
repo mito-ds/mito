47abd417a052d2103b674b0c49b7bdbc1bcfba83
Nate Rush <naterush1997@gmail.com>
2023-05-09 07:52:43 -0500

    mitosheet: fix duplicated variable

--- mitosheet/mitosheet/code_chunks/step_performers/import_steps/excel_range_import_code_chunk.py
+++ mitosheet/mitosheet/code_chunks/step_performers/import_steps/excel_range_import_code_chunk.py
@@ -37,7 +37,6 @@ EXCEL_RANGE_END_CONDITION_BOTTOM_LEFT_CORNER_VALUE_STARTS_WITH = 'bottom left co
 EXCEL_RANGE_END_CONDITION_BOTTOM_LEFT_CORNER_VALUE_CONTAINS = 'bottom left corner value contains'
 EXCEL_RANGE_END_CONDITIONAL_BOTTOM_LEFT_CORNER_CONSECUTIVE_EMPTY_CELLS = 'bottom left corner consecutive empty cells'
 EXCEL_RANGE_END_CONDITIONAL_BOTTOM_LEFT_CORNER_CONSECUTIVE_EMPTY_CELLS_IN_FIRST_COLUMN = 'bottom left corner consecutive empty cells in first column'
-EXCEL_RANGE_CONDITION_ROW_ENTIRELY_EMPTY = 'row entirely empty'
 EXCEL_RANGE_END_CONDITION_ROW_ENTIRELY_EMPTY = 'row entirely empty'
 EXCEL_RANGE_END_CONDITION_NUMBER_OF_EMPTY_ROWS = 'number of empty rows'
 EXCEL_RANGE_END_CONDTIONS = [
@@ -47,7 +46,6 @@ EXCEL_RANGE_END_CONDTIONS = [
     EXCEL_RANGE_END_CONDITION_BOTTOM_LEFT_CORNER_VALUE_CONTAINS,
     EXCEL_RANGE_END_CONDITIONAL_BOTTOM_LEFT_CORNER_CONSECUTIVE_EMPTY_CELLS,
     EXCEL_RANGE_END_CONDITIONAL_BOTTOM_LEFT_CORNER_CONSECUTIVE_EMPTY_CELLS_IN_FIRST_COLUMN,
-    EXCEL_RANGE_CONDITION_ROW_ENTIRELY_EMPTY,
     EXCEL_RANGE_END_CONDITION_ROW_ENTIRELY_EMPTY,
     EXCEL_RANGE_END_CONDITION_NUMBER_OF_EMPTY_ROWS
 ]
@@ -91,7 +89,7 @@ def get_table_range_params(sheet: Dict[str, Union[str, int]], start_condition: A
     bottom_left_value_contains = end_condition['value'] if end_condition['type'] == EXCEL_RANGE_END_CONDITION_BOTTOM_LEFT_CORNER_VALUE_CONTAINS else None
     bottom_left_consecutive_empty_cells = end_condition['value'] if end_condition['type'] == EXCEL_RANGE_END_CONDITIONAL_BOTTOM_LEFT_CORNER_CONSECUTIVE_EMPTY_CELLS else None
     bottom_left_consecutive_empty_cells_in_first_column = end_condition['value'] if end_condition['type'] == EXCEL_RANGE_END_CONDITIONAL_BOTTOM_LEFT_CORNER_CONSECUTIVE_EMPTY_CELLS_IN_FIRST_COLUMN else None
-    row_entirely_empty = True if end_condition['type'] == EXCEL_RANGE_CONDITION_ROW_ENTIRELY_EMPTY else None
+    row_entirely_empty = True if end_condition['type'] == EXCEL_RANGE_END_CONDITION_ROW_ENTIRELY_EMPTY else None
     number_of_empty_rows = end_condition['value'] if end_condition['type'] == EXCEL_RANGE_END_CONDITION_NUMBER_OF_EMPTY_ROWS else None
 
     num_columns = column_end_condition['value'] if column_end_condition['type'] == EXCEL_RANGE_COLUMN_END_CONDITION_NUM_COLUMNS else None
