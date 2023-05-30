45dbaaa9d8362d3380f087828d96118cabe01906
Nate Rush <naterush1997@gmail.com>
2023-05-30 09:20:12 -0700

    mitosheet: move eyeball to the right

--- mitosheet/src/components/taskpanes/AITransformation/AITransformationResultSection.tsx
+++ mitosheet/src/components/taskpanes/AITransformation/AITransformationResultSection.tsx
@@ -32,135 +32,133 @@ const AITransformationResultSection = (props: AITransformationResultSectionProps
 
     const logParams: Record<string, unknown> = {
         prompt_version: props.params.prompt_version,
-        // Do a little work to avoid length limits
         prompt: props.params.prompt.split('\n'),
         completion: props.params.completion.split('\n'),
         edited_completion: props.params.edited_completion.split('\n')
     }
 
+
     return (
         <div
             className="ai-transformation-result-section"
         >
-            {result.last_line_value !== undefined && result.last_line_value !== null && 
-                <p><span>Value:</span> {result.last_line_value}</p>
-            }
-            {result.prints.length > 0 && 
-                <>
-                    <p><span>Printed:</span></p>
-                    <pre>{result.prints}</pre>
-                </>
-            }
-            {result.created_dataframe_names.map(dfName => {
-                const sheetIndex = props.sheetDataArray.findIndex(sd => sd.dfName === dfName);
-                const sheetData = props.sheetDataArray[sheetIndex];
-                const numRows = sheetData?.numRows || 0;
-                const numColumns = sheetData?.numColumns || 0;
-                return (
-                    <div 
-                        key={dfName}
-                        onClick={() => {
-                            props.setUIState(prevUIState => {
-                                return {
-                                    ...prevUIState,
-                                    selectedSheetIndex: sheetIndex !== -1 ? sheetIndex : prevUIState.selectedSheetIndex
-                                }
-                            })
-                        }}>
-                        <span className={classNames({'text-color-recon-created': props.isMostRecentResult})}>Created:</span> <span className="text-underline">{dfName}</span>  ({numRows} rows, {numColumns} columns)
-                    </div>
-                )
-            })}
-            {Object.entries(result.modified_dataframes_recons).map(([dfName, modifiedDataframeRecon]) => {
-                const columnReconData = modifiedDataframeRecon.column_recon;
-                const sheetIndex = props.sheetDataArray.findIndex(sd => sd.dfName === dfName);
-                const rowChange = modifiedDataframeRecon.num_added_or_removed_rows;
-                const rowChangeTest = rowChange !== 0 ? (rowChange < 0 ? `(Removed ${rowChange * -1} rows)` : `(Added ${rowChange} rows)`) : undefined;
-                return (
-                    <div key={dfName}>
-                        <div 
+            <Row justify="space-between">
+                <Col span={22.5}>
+                    {result.last_line_value !== undefined && result.last_line_value !== null && 
+                        <p><span>Value:</span> {result.last_line_value}</p>
+                    }
+                    {result.prints.length > 0 && 
+                        <>
+                            <p><span>Printed:</span></p>
+                            <pre>{result.prints}</pre>
+                        </>
+                    }
+                    {result.created_dataframe_names.map(dfName => {
+                        const sheetIndex = props.sheetDataArray.findIndex(sd => sd.dfName === dfName);
+                        const sheetData = props.sheetDataArray[sheetIndex];
+                        const numRows = sheetData?.numRows || 0;
+                        const numColumns = sheetData?.numColumns || 0;
+                        return (
+                            <div 
+                                key={dfName}
+                                onClick={() => {
+                                    props.setUIState(prevUIState => {
+                                        return {
+                                            ...prevUIState,
+                                            selectedSheetIndex: sheetIndex !== -1 ? sheetIndex : prevUIState.selectedSheetIndex
+                                        }
+                                    })
+                                }}>
+                                <span className={classNames({'text-color-recon-created': props.isMostRecentResult})}>Created:</span> <span className="text-underline">{dfName}</span>  ({numRows} rows, {numColumns} columns)
+                            </div>
+                        )
+                    })}
+                    {Object.entries(result.modified_dataframes_recons).map(([dfName, modifiedDataframeRecon]) => {
+                        const columnReconData = modifiedDataframeRecon.column_recon;
+                        const sheetIndex = props.sheetDataArray.findIndex(sd => sd.dfName === dfName);
+                        const rowChange = modifiedDataframeRecon.num_added_or_removed_rows;
+                        const rowChangeTest = rowChange !== 0 ? (rowChange < 0 ? `(Removed ${rowChange * -1} rows)` : `(Added ${rowChange} rows)`) : undefined;
+                        return (
+                            <div key={dfName}>
+                                <div 
+                                    onClick={() => {
+                                        props.setUIState(prevUIState => {
+                                            return {
+                                                ...prevUIState,
+                                                selectedSheetIndex: sheetIndex !== -1 ? sheetIndex : prevUIState.selectedSheetIndex
+                                            }
+                                        })
+                                    }}>
+                                    <span className={classNames({'text-color-recon-modified': props.isMostRecentResult})}>Modified:</span> <span className="text-underline">{dfName}</span> {rowChangeTest}
+                                </div>
+                                {columnReconData.created_columns.map((ch, index) => {
+                                    return <div key={dfName + 'added' + index} className="ml-5px"><span className={classNames({'text-color-recon-created': props.isMostRecentResult})}>Added column: </span>{getDisplayColumnHeader(ch)}</div>
+                                })}
+                                {columnReconData.modified_columns.map((ch, index) => {
+                                    return <div key={dfName + 'modified' + index} className="ml-5px"><span className={classNames({'text-color-recon-modified': props.isMostRecentResult})}>Modified column: </span>{getDisplayColumnHeader(ch)}</div>
+                                })}
+                                {Object.entries(columnReconData.renamed_columns).map(([oldCh, newCh], index) => {
+                                    return <div key={dfName + 'renamed' + index} className="ml-5px"><span className={classNames({'text-color-recon-modified': props.isMostRecentResult})}>Renamed column: </span>{getDisplayColumnHeader(oldCh)} to {getDisplayColumnHeader(newCh)} </div>
+                                })}
+                                {columnReconData.deleted_columns.map((ch, index) => {
+                                    return <div key={dfName + 'removed' + index} className="ml-5px">Deleted column: {getDisplayColumnHeader(ch)}</div>
+                                })}
+                            </div>
+                        )
+                    })}
+                    {result.deleted_dataframe_names.map(dfName => {
+                        return (
+                            <div key={dfName}>
+                                <span>Deleted:</span> <span>{dfName}</span> 
+                            </div>
+                        )
+                    })}
+                    
+                    {(result.last_line_value === undefined || result.last_line_value === null) 
+                        && result.created_dataframe_names.length === 0 
+                        && Object.entries(result.modified_dataframes_recons).length === 0 
+                        && result.prints.length === 0
+                        && result.deleted_dataframe_names.length === 0 && 
+                        <p>
+                            No changes
+                        </p>
+                    }
+                </Col>
+                <Col span={1.5}>
+                    {(result.created_dataframe_names.length > 0 || Object.entries(result.modified_dataframes_recons).length > 0 || result.deleted_dataframe_names.length > 0) &&
+                        props.isMostRecentResult &&
+                        <EyeIcon 
+                            variant={props.uiState.dataRecon !== undefined ? 'selected' : 'unselected'} 
                             onClick={() => {
                                 props.setUIState(prevUIState => {
+
+                                    // If the user clicks the eye when the recon is displayed, 
+                                    // turn off the recon
+                                    if (prevUIState.dataRecon !== undefined) {
+                                        return {
+                                            ...prevUIState, 
+                                            dataRecon: undefined
+                                        }
+                                    }
+
+                                    // If the recon is already displayed, then display it. 
+                                    const newDataRecon = {
+                                        created_dataframe_names: result.created_dataframe_names,
+                                        deleted_dataframe_names: result.deleted_dataframe_names,
+                                        modified_dataframes_recons: result.modified_dataframes_recons
+                                    } 
+
                                     return {
-                                        ...prevUIState,
-                                        selectedSheetIndex: sheetIndex !== -1 ? sheetIndex : prevUIState.selectedSheetIndex
+                                        ...prevUIState, 
+                                        dataRecon: newDataRecon
                                     }
                                 })
-                            }}>
-                            <span className={classNames({'text-color-recon-modified': props.isMostRecentResult})}>Modified:</span> <span className="text-underline">{dfName}</span> {rowChangeTest}
-                        </div>
-                        {columnReconData.created_columns.map((ch, index) => {
-                            return <div key={dfName + 'added' + index} className="ml-5px"><span className={classNames({'text-color-recon-created': props.isMostRecentResult})}>Added column: </span>{getDisplayColumnHeader(ch)}</div>
-                        })}
-                        {columnReconData.modified_columns.map((ch, index) => {
-                            return <div key={dfName + 'modified' + index} className="ml-5px"><span className={classNames({'text-color-recon-modified': props.isMostRecentResult})}>Modified column: </span>{getDisplayColumnHeader(ch)}</div>
-                        })}
-                        {Object.entries(columnReconData.renamed_columns).map(([oldCh, newCh], index) => {
-                            return <div key={dfName + 'renamed' + index} className="ml-5px"><span className={classNames({'text-color-recon-modified': props.isMostRecentResult})}>Renamed column: </span>{getDisplayColumnHeader(oldCh)} to {getDisplayColumnHeader(newCh)} </div>
-                        })}
-                        {columnReconData.deleted_columns.map((ch, index) => {
-                            return <div key={dfName + 'removed' + index} className="ml-5px">Deleted column: {getDisplayColumnHeader(ch)}</div>
-                        })}
-                    </div>
-                )
-            })}
-            {result.deleted_dataframe_names.map(dfName => {
-                return (
-                    <div key={dfName}>
-                        <span>Deleted:</span> <span>{dfName}</span> 
-                    </div>
-                )
-            })}
+                            }}
+                        /> 
+                    }
+                </Col>
+            </Row>
             
-            {(result.last_line_value === undefined || result.last_line_value === null) 
-                && result.created_dataframe_names.length === 0 
-                && Object.entries(result.modified_dataframes_recons).length === 0 
-                && result.prints.length === 0
-                && result.deleted_dataframe_names.length === 0 && 
-                <p>
-                    No changes
-                </p>
-            }
-            {(result.created_dataframe_names.length > 0 || Object.entries(result.modified_dataframes_recons).length > 0 || result.deleted_dataframe_names.length > 0) &&
-                props.isMostRecentResult &&
-                <Row 
-                    justify="space-between" 
-                    onClick={() => {
-                        props.setUIState(prevUIState => {
-
-                            // If the user clicks the eye when the recon is displayed, 
-                            // turn off the recon
-                            if (prevUIState.dataRecon !== undefined) {
-                                return {
-                                    ...prevUIState, 
-                                    dataRecon: undefined
-                                }
-                            }
-
-                            // If the recon is already displayed, then display it. 
-                            const newDataRecon = {
-                                created_dataframe_names: result.created_dataframe_names,
-                                deleted_dataframe_names: result.deleted_dataframe_names,
-                                modified_dataframes_recons: result.modified_dataframes_recons
-                            } 
-
-                            return {
-                                ...prevUIState, 
-                                dataRecon: newDataRecon
-                            }
-                        })
-                    }}
-                >
-                    <Col>
-                        <p className="text-body-2">
-                            View changes in sheet
-                        </p>
-                    </Col>
-                    <Col offsetRight={.5}>
-                        <EyeIcon variant={props.uiState.dataRecon !== undefined ? 'selected' : 'unselected'} /> 
-                    </Col>
-                </Row>
-            }
             <Row justify="space-between" align="center" suppressTopBottomMargin>
                 <Col>
                     <p className="text-body-2">
