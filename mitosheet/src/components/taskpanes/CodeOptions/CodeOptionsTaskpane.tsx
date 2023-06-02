3866f0974adb316f459f01f20a3b3e2efb32d9eb
Nate Rush <naterush1997@gmail.com>
2023-06-02 15:33:41 -0700

    mitosheet: remove random code options from taskpane

--- mitosheet/src/components/taskpanes/CodeOptions/CodeOptionsTaskpane.tsx
+++ mitosheet/src/components/taskpanes/CodeOptions/CodeOptionsTaskpane.tsx
@@ -13,7 +13,6 @@ import Input from "../../elements/Input";
 import { useDebouncedEffect } from "../../../hooks/useDebouncedEffect";
 import CodeOptionsParameters from "./CodeOptionsParameters";
 import CollapsibleSection from "../../layout/CollapsibleSection";
-import Spacer from "../../layout/Spacer";
 
 
 interface CodeOptionsTaskpaneProps {
@@ -91,39 +90,6 @@ const CodeOptionsTaskpane = (props: CodeOptionsTaskpaneProps): JSX.Element => {
                         codeOptions={codeOptions}
                         setCodeOptions={setCodeOptions}
                     />
-                </CollapsibleSection>
-                <Spacer px={10}/>
-                <CollapsibleSection title={'Code Export Options'}>
-                    <Row justify='space-between' align='center'>
-                        <Col>
-                            <LabelAndTooltip tooltip="A Python function is a reusable block of code that performs a specific task. It takes input, processes it, and returns output, making your code more organized and able to be easily rerun on new datasets.">
-                                Write to .py file
-                            </LabelAndTooltip>
-                        </Col>
-                        <Col>
-                            <Toggle 
-                                value={props.analysisData.codeOptions.as_function} 
-                                onChange={function (): void {
-                                    const newCodeOptions = {...codeOptions};
-                                    newCodeOptions.as_function = !newCodeOptions.as_function;
-                                    setCodeOptions(newCodeOptions);
-                                }}
-                            />
-                        </Col>
-                    </Row>
-                    <Row justify='space-between' align='center'>
-                        <Col>
-                            <LabelAndTooltip tooltip="Give your function a short, descriptive name descring what it does.">
-                                File Path
-                            </LabelAndTooltip>
-                        </Col>
-                        <Col>
-                            <Input
-                                value={''}
-                                placeholder="path/to/file.py"
-                            ></Input>
-                        </Col>
-                    </Row>
                 </CollapsibleSection>
             </DefaultTaskpaneBody>
         </DefaultTaskpane>
