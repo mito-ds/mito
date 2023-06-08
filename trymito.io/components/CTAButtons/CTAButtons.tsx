75f926a04a22483800aefbcff2c312da3e3c768b
Nate Rush <naterush1997@gmail.com>
2023-06-08 11:30:17 -0700

    trymito.io: add product hunt

--- trymito.io/components/CTAButtons/CTAButtons.tsx
+++ trymito.io/components/CTAButtons/CTAButtons.tsx
@@ -16,10 +16,13 @@ const CTAButtons = (props: {variant: 'download' | 'contact' | 'try jupyterlite'}
                 />
             }
             {props.variant === 'try jupyterlite' && 
-                <TextButton 
-                    text='Try Mito'
-                    href={JUPYTERLITE_MITO_LINK}
-                />
+                <>
+                    <TextButton 
+                        text='Try Mito'
+                        href={JUPYTERLITE_MITO_LINK}
+                    />
+                    <a href="https://www.producthunt.com/posts/mito-ai?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-mito&#0045;ai" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=398626&theme=light" alt="Mito&#0032;AI - Automate&#0032;Excel&#0032;reports&#0032;with&#0032;AI | Product Hunt" style={{width: '250px', height: '30px'}} width="250" height="54" /></a>
+                </>
             }
             {props.variant === 'contact' && 
                 <TextButton 
@@ -27,6 +30,7 @@ const CTAButtons = (props: {variant: 'download' | 'contact' | 'try jupyterlite'}
                     href="mailto:founders@sagacollab.com"
                 />
             }
+            
             <h2 className={styles.cta_subbutton}>
                 <Link href='/plans'>
                     <a>
