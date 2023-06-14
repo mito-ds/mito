b057b58e28c054238574c10aa7d734d5e7903087
Nate Rush <naterush1997@gmail.com>
2023-06-14 13:10:43 -0700

    mitosheet: add tests for quote in file path

--- mitosheet/mitosheet/code_chunks/export_to_file_code_chunk.py
+++ mitosheet/mitosheet/code_chunks/export_to_file_code_chunk.py
@@ -8,7 +8,7 @@ from typing import Dict, List, Tuple
 
 from mitosheet.code_chunks.code_chunk import CodeChunk
 from mitosheet.state import State
-from mitosheet.transpiler.transpile_utils import TAB
+from mitosheet.transpiler.transpile_utils import TAB, column_header_to_transpiled_code
 
 
 class ExportToFileCodeChunk(CodeChunk):
@@ -29,16 +29,16 @@ class ExportToFileCodeChunk(CodeChunk):
     def get_code(self) -> Tuple[List[str], List[str]]:
         if self.export_type == 'csv':
             return [
-                f"{self.post_state.df_names[sheet_index]}.to_csv(r'{export_location}', index=False)"
+                f"{self.post_state.df_names[sheet_index]}.to_csv(r{column_header_to_transpiled_code(export_location)}, index=False)"
                 for sheet_index, export_location in self.sheet_index_to_export_location.items()
             ], []
         elif self.export_type == 'excel':
             # If there is only one sheet being exported, we can avoid creating the pd.ExcelWriter
             if len(self.sheet_index_to_export_location) == 1:
                 for sheet_index, export_location in self.sheet_index_to_export_location.items():
-                    return [f"{self.post_state.df_names[sheet_index]}.to_excel(r'{self.file_name}', sheet_name='{export_location}', index={False})"], []
+                    return [f"{self.post_state.df_names[sheet_index]}.to_excel(r{column_header_to_transpiled_code(self.file_name)}, sheet_name='{export_location}', index={False})"], []
 
-            return [f"with pd.ExcelWriter(r'{self.file_name}') as writer:"] + [
+            return [f"with pd.ExcelWriter(r{column_header_to_transpiled_code(self.file_name)}) as writer:"] + [
                 f'{TAB}{self.post_state.df_names[sheet_index]}.to_excel(writer, sheet_name="{export_location}", index={False})'
                 for sheet_index, export_location in self.sheet_index_to_export_location.items()
             ], ['import pandas as pd']
@@ -48,9 +48,9 @@ class ExportToFileCodeChunk(CodeChunk):
     def get_parameterizable_params(self) -> List[Tuple[str, str]]:
         if self.export_type == 'csv':
             return [
-                (f"r'{export_location}'", 'file_name') for export_location in self.sheet_index_to_export_location.values()
+                (f"r{column_header_to_transpiled_code(export_location)}", 'file_name') for export_location in self.sheet_index_to_export_location.values()
             ]
         elif self.export_type == 'excel':
-            return [(f"r'{self.file_name}'", 'file_name')]
+            return [(f"r{column_header_to_transpiled_code(self.file_name)}", 'file_name')]
         else:
             raise ValueError(f'Not a valid file type: {self.export_type}')
--- mitosheet/mitosheet/tests/test_transpile.py
+++ mitosheet/mitosheet/tests/test_transpile.py
@@ -584,11 +584,11 @@ df = function()"""
 def test_transpiled_with_export_to_csv_singular():
     df = pd.DataFrame({'A': [1, 2, 3]})
     mito = create_mito_wrapper(df, arg_names=['df'])
-    mito.export_to_file('csv', [0], 'test.csv')
+    mito.export_to_file('csv', [0], 'te"st.csv')
 
-    assert [('df', 'df_name'), ("r'test.csv'", 'file_name')] == get_parameterizable_params({}, mito.mito_backend.steps_manager)
+    assert [('df', 'df_name'), ("r'te" + '"' + "st.csv'", 'file_name')] == get_parameterizable_params({}, mito.mito_backend.steps_manager)
 
-    mito.code_options_update({'as_function': True, 'function_name': 'function', 'function_params': {'path': "r'test.csv'"}})
+    mito.code_options_update({'as_function': True, 'function_name': 'function', 'function_params': {'path': "r'te" + '"' + "st.csv'"}})
 
     assert "\n".join(mito.transpiled_code) == """
 def function(df, path):
@@ -596,7 +596,7 @@ def function(df, path):
     
     return df
 
-path = r'test.csv'
+path = r'te"st.csv'
 
 df = function(df, path)"""
 
@@ -624,11 +624,11 @@ df1, df2 = function(df1, df2, path_0, path_1)"""
 def test_transpiled_with_export_to_xlsx_single():
     df = pd.DataFrame({'A': [1, 2, 3]})
     mito = create_mito_wrapper(df, arg_names=['df'])
-    mito.export_to_file('excel', [0], 'test.xlsx')
+    mito.export_to_file('excel', [0], "te'st.xlsx")
 
-    assert [('df', 'df_name'), ("r'test.xlsx'", 'file_name')] == get_parameterizable_params({}, mito.mito_backend.steps_manager)
+    assert [('df', 'df_name'), ('r"te' + "'" + 'st.xlsx"', 'file_name')] == get_parameterizable_params({}, mito.mito_backend.steps_manager)
 
-    mito.code_options_update({'as_function': True, 'function_name': 'function', 'function_params': {'path': "r'test.xlsx'"}})
+    mito.code_options_update({'as_function': True, 'function_name': 'function', 'function_params': {'path': 'r"te' + "'" + 'st.xlsx"'}})
 
     assert "\n".join(mito.transpiled_code) == """
 def function(df, path):
@@ -636,7 +636,7 @@ def function(df, path):
     
     return df
 
-path = r'test.xlsx'
+path = r"te'st.xlsx"
 
 df = function(df, path)"""
 
