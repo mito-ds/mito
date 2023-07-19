77f0724c31623c7a032e33db7d169b7177c29d14
Martha Cryan <martha.cryan326@gmail.com>
2023-07-19 09:54:05 -0700

    Add basic support for header color definition

--- mitosheet/mitosheet/pro/download/formatting.py
+++ mitosheet/mitosheet/pro/download/formatting.py
@@ -18,24 +18,17 @@ def add_formatting_to_excel_sheet(
 
     NOTE: this is a Mito Pro feature.
     """
-    df = steps_manager.dfs[sheet_index]
     sheet_name = steps_manager.curr_step.df_names[sheet_index]
+    formats = steps_manager.curr_step.df_formats
+    format = formats[1]
 
-    # TODO: fix up below
-    return
-
-    format_data_map = steps_manager.curr_step.column_format_types[sheet_index]
-
-    workbook  = writer.book
+    workbook = writer.book
     worksheet = writer.sheets[sheet_name]
 
-    ## This formatting code is wrapped in a if False until we deploy our first pro features
-    for column_index, column_header in enumerate(df.keys()):
-        column_id = steps_manager.curr_step.column_ids.get_column_id_by_header(sheet_index, column_header)
-        column_dtype = str(df[column_header].dtype)
-        format_data = format_data_map[column_id]
-        excel_column = get_excel_range_from_column_index(column_index)
-
-        if (is_number_dtype(column_dtype)):
-            format = workbook.add_format({'num_format': 'General'})
-            worksheet.set_column(excel_column, None, format)
+    print(format['headers']['backgroundColor'])
+    header = workbook.add_format({
+        "font_color": format['headers']['color'],
+        "bg_color": format['headers']['backgroundColor']
+    })
+    worksheet.set_row(0, None, header)
+            
