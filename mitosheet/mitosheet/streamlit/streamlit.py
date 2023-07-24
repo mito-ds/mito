ce2f898cb39b46bff01b26f86c3d18d95979bab2
Nate Rush <naterush1997@gmail.com>
2023-07-24 13:33:22 -0700

    mitosheet: remove untyped function

--- mitosheet/mitosheet/streamlit/streamlit.py
+++ mitosheet/mitosheet/streamlit/streamlit.py
@@ -6,12 +6,7 @@ st.set_page_config(layout="wide")
 
 st.subheader("Dataframe Created from File Upload")
 
-def get_loan_data(date: str, include_duplicates: bool):
-    pass
-
-
 new_dfs, code = mito_component(
-    importers=[get_loan_data],
     df_names=['df1000'],
     key="mito_component"
 )
