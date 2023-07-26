93c3245086372d9b60404dcf506600f63d028d90
Martha Cryan <martha.cryan326@gmail.com>
2023-07-26 15:45:24 -0700

    Refactor to use get

--- mitosheet/mitosheet/api/get_dataframe_as_excel.py
+++ mitosheet/mitosheet/api/get_dataframe_as_excel.py
@@ -39,16 +39,16 @@ def get_dataframe_as_excel(params: Dict[str, Any], steps_manager: StepsManagerTy
 
             # Add formatting to the sheet for pro users
             format = steps_manager.curr_step.df_formats[sheet_index]
-            if allow_formatting and format['headers']: 
+            if allow_formatting: 
                 add_formatting_to_excel_sheet(
                     writer,
                     sheet_name,
-                    header_background_color=format['headers']['backgroundColor'],
-                    header_font_color=format['headers']['color'],
-                    even_background_color=format['rows']['even']['backgroundColor'],
-                    even_font_color=format['rows']['even']['color'],
-                    odd_background_color=format['rows']['odd']['backgroundColor'],
-                    odd_font_color=format['rows']['odd']['color']
+                    header_background_color=format.get('headers', {}).get('backgroundColor'),
+                    header_font_color=format.get('headers', {}).get('color'),
+                    even_background_color=format.get('rows', {}).get('even', {}).get('backgroundColor'),
+                    even_font_color=format.get('rows', {}).get('even', {}).get('color'),
+                    odd_background_color=format.get('rows', {}).get('odd', {}).get('backgroundColor'),
+                    odd_font_color=format.get('rows', {}).get('odd', {}).get('color')
                 )
     
     # Go back to the start of the buffer
