f81ba2daf21bd854887aef8ca7e7fcd4ee2f9cad
Aaron Diamond-Reivich <aarondr77@gmail.com>
2023-07-28 17:14:20 -0400

    mitosheet: fix filter icon color

--- mitosheet/src/mito/components/icons/FilterIcons.tsx
+++ mitosheet/src/mito/components/icons/FilterIcons.tsx
@@ -9,7 +9,7 @@ export const FilterIcon = (props: {purpleOrDark?: 'dark' | 'purple', nonEmpty?: 
     if (props.nonEmpty) {
         return (
             <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12.681 1H1.31902C1.0608 1 0.90821 1.31983 1.0608 1.55011L5.72061 8.58635V13L8.27939 11.8614V8.6887L12.9392 1.55011C13.0918 1.30704 12.9392 1 12.681 1Z" fill="var(--mito-highlight)" stroke="var(--mito-text)" strokeMiterlimit="10" strokeLinecap="round"/>
+                <path d="M12.681 1H1.31902C1.0608 1 0.90821 1.31983 1.0608 1.55011L5.72061 8.58635V13L8.27939 11.8614V8.6887L12.9392 1.55011C13.0918 1.30704 12.9392 1 12.681 1Z" fill="var(--mito-highlight)" stroke="var(--mito-highlight)" strokeMiterlimit="10" strokeLinecap="round"/>
             </svg>
         )
     }
