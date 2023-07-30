1d9a230289d46932bc51818c9774458d6d93f73c
Nate Rush <naterush1997@gmail.com>
2023-07-30 09:43:36 -0700

    streamlit: update white -> mito-background

--- mitosheet/src/mito/components/icons/GraphIcon.tsx
+++ mitosheet/src/mito/components/icons/GraphIcon.tsx
@@ -5,10 +5,10 @@ const GraphIcon = (props: {variant?: IconVariant}): JSX.Element => {
     if (props.variant === 'light') {
         return (
             <svg width="13" height="15" viewBox="0 0 13 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 5.5874V14.3589" stroke="white" strokeMiterlimit="10" strokeLinecap="round"/>
-                <path d="M4.64453 9.67969V14.3588" stroke="white" strokeMiterlimit="10" strokeLinecap="round"/>
-                <path d="M8.28906 1V14.3588" stroke="white" strokeMiterlimit="10" strokeLinecap="round"/>
-                <path d="M11.9336 5.5874V14.3589" stroke="white" strokeMiterlimit="10" strokeLinecap="round"/>
+                <path d="M1 5.5874V14.3589" stroke="var(--mito-background)" strokeMiterlimit="10" strokeLinecap="round"/>
+                <path d="M4.64453 9.67969V14.3588" stroke="var(--mito-background)" strokeMiterlimit="10" strokeLinecap="round"/>
+                <path d="M8.28906 1V14.3588" stroke="var(--mito-background)" strokeMiterlimit="10" strokeLinecap="round"/>
+                <path d="M11.9336 5.5874V14.3589" stroke="var(--mito-background)" strokeMiterlimit="10" strokeLinecap="round"/>
             </svg>
         )
     } else {
