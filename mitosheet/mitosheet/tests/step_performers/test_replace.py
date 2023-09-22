387b9997933d66731de2bb73ec6b529edc636452
Martha Cryan <martha.cryan326@gmail.com>
2023-09-22 13:50:26 -0500

    Add more tests

--- mitosheet/mitosheet/tests/step_performers/test_replace.py
+++ mitosheet/mitosheet/tests/step_performers/test_replace.py
@@ -36,6 +36,79 @@ REPLACE_TESTS = [
                 'F': pd.to_timedelta(['1 days', '2 days', '4 days'])
             })
         ]
+    ),(
+        [
+            pd.DataFrame({
+                'A': [1, 2, 3],
+                'B': [1.0, 2.0, 3.0], 
+                'C': [True, False, True], 
+                'D': ["string", "with spaces", "and/!other@characters3"], 
+                'E': pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']), 
+                'F': pd.to_timedelta(['1 days', '2 days', '3 days'])
+            })
+        ],
+        0,
+        "i", 
+        "abc", 
+        [
+            pd.DataFrame({
+                'A': [1, 2, 3],
+                'B': [1.0, 2.0, 3.0], 
+                'C': [True, False, True], 
+                'D': ["strabcng", "wabcth spaces", "and/!other@characters3"], 
+                'E': pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']), 
+                'F': pd.to_timedelta(['1 days', '2 days', '3 days'])
+            })
+        ]
+    ),(
+        [
+            pd.DataFrame({
+                'A': [1, 2, 3],
+                'B': [1.0, 2.0, 3.0], 
+                'C': [True, False, True], 
+                'D': ["string", "with spaces", "and/!other@characters3"], 
+                'E': pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']), 
+                'F': pd.to_timedelta(['1 days', '2 days', '3 days'])
+            })
+        ],
+        0,
+        "with spaces", 
+        "abc", 
+        [
+            pd.DataFrame({
+                'A': [1, 2, 3],
+                'B': [1.0, 2.0, 3.0], 
+                'C': [True, False, True], 
+                'D': ["string", "abc", "and/!other@characters3"], 
+                'E': pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']), 
+                'F': pd.to_timedelta(['1 days', '2 days', '3 days'])
+            })
+        ]
+    ),
+    (
+        [
+            pd.DataFrame({
+                'A': [1, 2, 3],
+                'B': [1.0, 2.0, 3.0], 
+                'C': [True, False, True], 
+                'D': ["string", "with spaces", "and/!other@characters3"], 
+                'E': pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']), 
+                'F': pd.to_timedelta(['1 days', '2 days', '3 days'])
+            })
+        ],
+        0,
+        "true", 
+        "false", 
+        [
+            pd.DataFrame({
+                'A': [1, 2, 3],
+                'B': [1.0, 2.0, 3.0], 
+                'C': [False, False, False], 
+                'D': ["string", "with spaces", "and/!other@characters3"], 
+                'E': pd.to_datetime(['12-22-1997', '12-23-1997', '12-24-1997']), 
+                'F': pd.to_timedelta(['1 days', '2 days', '3 days'])
+            })
+        ],
     ),
 ]
 @pytest.mark.parametrize("input_dfs, sheet_index, search_value, replace_value, output_dfs", REPLACE_TESTS)
