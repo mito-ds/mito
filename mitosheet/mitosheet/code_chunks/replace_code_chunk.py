4b32a403222ea0ec654a8d788fe7ad4091e7f228
Martha Cryan <martha.cryan326@gmail.com>
2023-09-25 19:44:40 -0500

    Address review

--- mitosheet/mitosheet/code_chunks/replace_code_chunk.py
+++ mitosheet/mitosheet/code_chunks/replace_code_chunk.py
@@ -19,42 +19,42 @@ class ReplaceCodeChunk(CodeChunk):
         self.sheet_index = sheet_index
         self.search_value = search_value
         self.replace_value = replace_value
+        self.df = self.prev_state.dfs[self.sheet_index]
+        self.df_name = self.prev_state.df_names[self.sheet_index]
 
     def get_display_name(self) -> str:
         return 'Replace'
     
     def get_description_comment(self) -> str:
-        return f"Replace {self.search_value} with {self.replace_value} in {self.post_state.df_names[self.sheet_index]}"
+        return f"Replace {self.search_value} with {self.replace_value} in {self.prev_state.df_names[self.sheet_index]}"
 
     def get_code(self) -> Tuple[List[str], List[str]]:
         search_value = self.search_value
         replace_value = self.replace_value
-        df = self.post_state.dfs[self.sheet_index]
-        df_name = self.post_state.df_names[self.sheet_index]
 
         # The shorter code chunk is for dataframes that *don't* have any boolean columns
         # Boolean columns are a special case, because when we convert them to str then back
         # to bool, they all become True. So we have to convert them back to bool with a custom
         # function.
         code_chunk = [
-            f'{df_name} = {df_name}.astype(str).replace("(?i){search_value}", "{replace_value}", regex=True).astype({df_name}.dtypes.to_dict())',
+            f'{self.df_name} = {self.df_name}.astype(str).replace("(?i){search_value}", "{replace_value}", regex=True).astype({self.df_name}.dtypes.to_dict())',
         ]
 
-        if (any(df.dtypes == 'timedelta') and pd.__version__ < 1.4):
+        if (any(self.df.dtypes == 'timedelta') and pd.__version__ < 1.4):
             raise MitoError(
                 'version_error',
                 'Pandas version error',
                 'This version of pandas doesn\'t support replacing values in timedelta columns. Please upgrade to pandas 1.2 or later.',
             )
-        if any(df.dtypes == bool):
+        if any(self.df.dtypes == bool):
             code_chunk = [
-                f"non_bool_cols, bool_cols = {df_name}.select_dtypes(exclude='bool'), {df_name}.select_dtypes(include='bool')",
-                f"{df_name}[non_bool_cols.columns] = non_bool_cols.astype(str).replace('(?i){search_value}', '{replace_value}', regex=True).astype(non_bool_cols.dtypes.to_dict())",
-                f"{df_name}[bool_cols.columns] = bool_cols.astype(str).replace('(?i){search_value}', '{replace_value}', regex=True).applymap(cast_string_to_bool).astype(bool)",
+                f"non_bool_cols, bool_cols = {self.df_name}.select_dtypes(exclude='bool'), {self.df_name}.select_dtypes(include='bool')",
+                f"{self.df_name}[non_bool_cols.columns] = non_bool_cols.astype(str).replace('(?i){search_value}', '{replace_value}', regex=True).astype(non_bool_cols.dtypes.to_dict())",
+                f"{self.df_name}[bool_cols.columns] = bool_cols.astype(str).replace('(?i){search_value}', '{replace_value}', regex=True).applymap(cast_string_to_bool).astype(bool)",
             ]
 
         # Then, we always replace the search_value inside the column headers
-        code_chunk.append(f"{df_name}.columns = {df_name}.columns.str.replace('(?i){search_value}', '{replace_value}', regex=True)")
+        code_chunk.append(f"{self.df_name}.columns = {self.df_name}.columns.str.replace('(?i){search_value}', '{replace_value}', regex=True)")
         return code_chunk, []
 
     def get_edited_sheet_indexes(self) -> List[int]:
