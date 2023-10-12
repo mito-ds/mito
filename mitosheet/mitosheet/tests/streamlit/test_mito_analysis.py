4a44289dfcc34515a5e2699e527f8ecabb8da2e0
Martha Cryan <martha.cryan326@gmail.com>
2023-10-12 18:47:41 -0500

    Tests

--- mitosheet/mitosheet/tests/streamlit/test_mito_analysis.py
+++ mitosheet/mitosheet/tests/streamlit/test_mito_analysis.py
@@ -15,71 +15,6 @@ def function(file_name_import_csv_0, file_name_import_excel_0, file_name_export_
         'Sheet1'
     ], skiprows=0)
     Sheet1 = sheet_df_dictonary['Sheet1']
-    
-    txt.to_csv(file_name_export_csv_0, index=False)
-    
-    with pd.ExcelWriter(file_name_export_excel_0, engine="openpyxl") as writer:
-        Sheet1.to_excel(writer, sheet_name="Sheet1", index=False)
-    
-    return txt, Sheet1
-"""
-    tmp_file1 = str(tmp_path / 'txt.csv')
-    tmp_file2 = str(tmp_path / 'file.xlsx')
-    tmp_exportfile1 = str(tmp_path / 'export.csv')
-    tmp_exportfile2 = str(tmp_path / 'export.xlsx')
-    df1 = pd.DataFrame({'A': [1], 'B': [2]})
-    df1.to_csv(tmp_file1, index=False)
-    df1.to_excel(tmp_file2, index=False)
-    param_metadata = [
-        {
-            'initial_value': f"r'{tmp_file1}'",
-            'type': 'file_name',
-            'subtype': 'file_name_import_csv',
-            'name': 'file_name_import_csv_0',
-            'required': False
-        },
-        {
-            'initial_value': f"r'{tmp_file2}'",
-            'type': 'file_name',
-            'subtype': 'file_name_import_excel',
-            'name': 'file_name_import_excel_0',
-            'required': False
-        },
-        {
-            'initial_value': f"r'{tmp_exportfile1}'",
-            'type': 'file_name',
-            'subtype': 'file_name_export_csv',
-            'name': 'file_name_export_csv_0',
-            'required': False
-        },
-        {
-            'initial_value': f"r'{tmp_exportfile2}'",
-            'type': 'file_name',
-            'subtype': 'file_name_export_excel',
-            'name': 'file_name_export_excel_0',
-            'required': False
-        }
-    ]
-    
-    analysis = MitoAnalysis('', None, fully_parameterized_function, param_metadata)
-    result = analysis.run()
-    assert result is not None
-    pd.testing.assert_frame_equal(result[0], df1)
-    pd.testing.assert_frame_equal(result[1], df1)
-
-
-@requires_streamlit
-def test_mito_analysis(tmp_path):
-    fully_parameterized_function = f"""from mitosheet.public.v3 import *
-import pandas as pd
-
-def function(file_name_import_csv_0, file_name_import_excel_0, file_name_export_csv_0, file_name_export_excel_0):
-    txt = pd.read_csv(file_name_import_csv_0)
-    
-    sheet_df_dictonary = pd.read_excel(file_name_import_excel_0, engine='openpyxl', sheet_name=[
-        'Sheet1'
-    ], skiprows=0)
-    Sheet1 = sheet_df_dictonary['Sheet1']
 
     txt.insert(1, 'HelloWorld', SUM(1,2))
     
@@ -134,3 +69,26 @@ def function(file_name_import_csv_0, file_name_import_excel_0, file_name_export_
     assert result is not None
     pd.testing.assert_frame_equal(result[0], expected_df)
     pd.testing.assert_frame_equal(result[1], df1)
+
+@requires_streamlit
+def test_run_fail():
+    fully_parameterized_function = f"""from mitosheet.public.v3 import *
+import pandas as pd
+
+def function(import_dataframe_0):
+    return import_dataframe_0
+"""
+    param_metadata = [
+        {
+            'initial_value': 'test_df_name',
+            'type': 'df_name',
+            'subtype': 'import_dataframe',
+            'required': True,
+            'name': 'import_dataframe_0'
+        },
+    ]
+    
+    analysis = MitoAnalysis('', None, fully_parameterized_function, param_metadata)
+    with pytest.raises(NotImplementedError):
+        analysis.run()
+    
