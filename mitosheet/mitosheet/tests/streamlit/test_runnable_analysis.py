9562c1e6dafccbe9943ee976a58894d1010a9835
Nate Rush <naterush1997@gmail.com>
2023-10-24 10:17:39 -0700

    mitosheet: update run test to pass by name

--- mitosheet/mitosheet/tests/streamlit/test_runnable_analysis.py
+++ mitosheet/mitosheet/tests/streamlit/test_runnable_analysis.py
@@ -490,5 +490,5 @@ def function_srjr(file_name_import_csv_0):
 
     # Run with a df rather than a file path
     df = pd.DataFrame({'A': [1, 2, 3]})
-    result = analysis.run(df)
+    result = analysis.run(file_name_import_csv_0=df)
     assert result is not None
