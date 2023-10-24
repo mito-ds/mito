bd1c170d2e32cd3e2b9dc3fb2de363832ef5b812
Nate Rush <naterush1997@gmail.com>
2023-10-24 09:26:26 -0700

    dash: add RunnableAnalysis to dash v1 interface

--- mitosheet/mitosheet/mito_dash/v1/__init__.py
+++ mitosheet/mitosheet/mito_dash/v1/__init__.py
@@ -1,3 +1,4 @@
 from unittest.mock import patch
 from mitosheet.mito_dash.v1.spreadsheet import Spreadsheet
 from mitosheet.mito_dash.v1.mito_callback import mito_callback
+from mitosheet.streamlit.v1 import RunnableAnalysis
