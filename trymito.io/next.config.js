1fc20eaae9d662319335e98eadfd8e7290b0edd1
Aaron Diamond-Reivich <aarondr77@gmail.com>
2023-10-24 22:53:07 -0400

    trymito: 301 redirect attempt

--- trymito.io/next.config.js
+++ trymito.io/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/quantifying-mitos-impact-on-analyst-python-productivity/',
+        destination: 'https://www.trymito.io/blog/quantifying-mitos-impact-on-analyst-python-productivity/',
+        permanent: true,
+      },
+    ];
+  },
 }
