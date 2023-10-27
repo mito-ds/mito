c5691f276d2eb72e1f5cb4af1f7ed6b625f6e2b7
Martha Cryan <martha.cryan326@gmail.com>
2023-10-27 13:26:56 -0500

    Lint

--- mitosheet/src/mito/components/icons/MoreIcon.tsx
+++ mitosheet/src/mito/components/icons/MoreIcon.tsx
@@ -6,10 +6,10 @@ import React from 'react';
 const MoreIcon = (): JSX.Element => {
     return (
         <svg width="18" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7.82461 10.1703L9.87158 12.3703L7.82461 14.5703M4.00492 12.3703H6.60714H9.20937" stroke="#9D6CFF" stroke-miterlimit="10" stroke-linecap="round"/>
-            <rect x="11.02" y="10.3164" width="2.18333" height="4.38333" rx="1.09167" stroke="black" stroke-width="0.75"/>
-            <rect x="11.0181" y="4.44531" width="2.18333" height="4.38333" rx="1.09167" stroke="black" stroke-width="0.75"/>
-            <rect x="7.35144" y="4.44531" width="2.18333" height="4.38333" rx="1.09167" stroke="black" stroke-width="0.75"/>
+            <path d="M7.82461 10.1703L9.87158 12.3703L7.82461 14.5703M4.00492 12.3703H6.60714H9.20937" stroke="#9D6CFF" strokeMiterlimit="10" strokeLinecap="round"/>
+            <rect x="11.02" y="10.3164" width="2.18333" height="4.38333" rx="1.09167" stroke="black" strokeWidth="0.75"/>
+            <rect x="11.0181" y="4.44531" width="2.18333" height="4.38333" rx="1.09167" stroke="black" strokeWidth="0.75"/>
+            <rect x="7.35144" y="4.44531" width="2.18333" height="4.38333" rx="1.09167" stroke="black" strokeWidth="0.75"/>
             <rect x="9.87158" y="14.3398" width="0.733333" height="0.733333" fill="black"/>
             <rect x="5.50977" y="8.46875" width="0.733333" height="0.733333" fill="black"/>
         </svg>
