54a61843ca6299607f8be4dc5813b68ec5dda56b
Martha Cryan <martha.cryan326@gmail.com>
2023-10-30 12:26:48 -0500

    Comment

--- mitosheet/src/mito/types.tsx
+++ mitosheet/src/mito/types.tsx
@@ -997,6 +997,7 @@ export interface BaseAction<Type, StaticType> {
     type: Type;
     staticType: StaticType;
     // The short title for the action. Should be title case, as you want to display it.
+    // If undefined, the toolbar will not display a title in the toolbar. 
     shortTitle?: string
 
     // The optional long title for the action.
