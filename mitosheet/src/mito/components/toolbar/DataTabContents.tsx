becb3fcc26dbf59127d4153b33d67a8cb682ac23
Martha Cryan <martha.cryan326@gmail.com>
2023-11-02 17:35:48 -0500

    Lint

--- mitosheet/src/mito/components/toolbar/DataTabContents.tsx
+++ mitosheet/src/mito/components/toolbar/DataTabContents.tsx
@@ -48,6 +48,7 @@ export const DataTabContents = (
                 action={action}
                 setEditorState={props.setEditorState}
                 iconOverride={<ImportIcon />}
+                key={action.staticType}
             />)
         })}
 
