65a33571a7713e6cca064abd71ff44dec304ed5e
Nate Rush <naterush1997@gmail.com>
2023-11-07 17:09:47 -0800

    mitosheet: change use live updating hook to be able to stay open

--- mitosheet/src/mito/components/taskpanes/taskpanes.tsx
+++ mitosheet/src/mito/components/taskpanes/taskpanes.tsx
@@ -135,16 +135,11 @@ export type TaskpaneInfo =
     through some other method. 
 */ 
 export const EDITING_TASKPANES: TaskpaneType[] = [
-    TaskpaneType.PIVOT, 
-    TaskpaneType.MERGE, 
-    TaskpaneType.CONCAT,
     TaskpaneType.DROP_DUPLICATES,
     TaskpaneType.IMPORT_FILES,
     TaskpaneType.DOWNLOAD,
     TaskpaneType.SPLIT_TEXT_TO_COLUMNS,
     TaskpaneType.FILL_NA,
-    TaskpaneType.MELT,
-    TaskpaneType.SET_DATAFRAME_FORMAT,
     TaskpaneType.SNOWFLAKEIMPORT,
     TaskpaneType.EXCEL_RANGE_IMPORT,
     TaskpaneType.EXPORT_TO_FILE,
