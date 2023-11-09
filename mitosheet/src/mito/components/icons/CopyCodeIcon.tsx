9a5ba74d1ea43fc73bfeca98d8454cb7dcff9cd5
Martha Cryan <martha.cryan326@gmail.com>
2023-11-08 21:37:32 -0600

    Fix copy code icon alignment

--- mitosheet/src/mito/components/icons/CopyCodeIcon.tsx
+++ mitosheet/src/mito/components/icons/CopyCodeIcon.tsx
@@ -14,7 +14,7 @@ const CopyIcon = (props: { success?: boolean }): JSX.Element => {
         )
     }
     return (
-        <svg width="18" height="22" viewBox="0 1 18 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="18" height="22" viewBox="0 1 18 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="1.00781" y="3.03516" width="14" height="18" rx="1.5" stroke="var(--mito-highlight)"/>
             <path d="M2 3.42969C2 2.32512 2.89543 1.42969 4 1.42969H15C16.1046 1.42969 17 2.32512 17 3.42969V18.4297C17 19.5343 16.1046 20.4297 15 20.4297H4C2.89543 20.4297 2 19.5343 2 18.4297V3.42969Z" fill="var(--mito-background-off)"/>
             <rect x="2.99219" y="0.96875" width="14" height="18" rx="1.5" stroke="var(--mito-text)"/>
