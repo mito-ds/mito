c93463241ecb4e97f602fb3a7d1389678f9ef84a
Martha Cryan <martha.cryan326@gmail.com>
2023-12-01 13:56:03 -0600

    Use medium-large width for context menus

--- mitosheet/src/mito/components/endo/IndexHeaderDropdown.tsx
+++ mitosheet/src/mito/components/endo/IndexHeaderDropdown.tsx
@@ -7,7 +7,7 @@ import Dropdown from '../elements/Dropdown';
 import DropdownItem from '../elements/DropdownItem';
 import DropdownSectionSeperator from '../elements/DropdownSectionSeperator';
 import { TaskpaneType } from '../taskpanes/taskpanes';
-import { getPropsForDropdownItem } from './utils';
+import { getPropsForContextMenuDropdownItem } from './utils';
 import { Actions } from '../../utils/actions';
 
 /*
@@ -36,28 +36,28 @@ export default function IndexHeaderDropdown(props: {
         <Dropdown
             display={props.display}
             closeDropdown={() => props.setOpenIndexHeaderDropdown(undefined)}
-            width='large'
+            width='medium-large'
         >
             <DropdownItem
-                {...getPropsForDropdownItem(props.actions.buildTimeActions[ActionEnum.Copy], props.closeOpenEditingPopups)}
+                {...getPropsForContextMenuDropdownItem(props.actions.buildTimeActions[ActionEnum.Copy], props.closeOpenEditingPopups)}
                 title='Copy Row'
             />
 
             <DropdownSectionSeperator isDropdownSectionSeperator={true}/>
 
             <DropdownItem
-                {...getPropsForDropdownItem(props.actions.buildTimeActions[ActionEnum.Delete], props.closeOpenEditingPopups)}
+                {...getPropsForContextMenuDropdownItem(props.actions.buildTimeActions[ActionEnum.Delete], props.closeOpenEditingPopups)}
                 title='Delete Row'
             />
 
             <DropdownSectionSeperator isDropdownSectionSeperator={true}/>
 
-            <DropdownItem {...getPropsForDropdownItem(props.actions.buildTimeActions[ActionEnum.Promote_Row_To_Header], props.closeOpenEditingPopups)}/>
+            <DropdownItem {...getPropsForContextMenuDropdownItem(props.actions.buildTimeActions[ActionEnum.Promote_Row_To_Header], props.closeOpenEditingPopups)}/>
 
             <DropdownSectionSeperator isDropdownSectionSeperator={true}/>
 
-            <DropdownItem {...getPropsForDropdownItem(props.actions.buildTimeActions[ActionEnum.RESET_AND_DROP_INDEX], props.closeOpenEditingPopups)}/>
-            <DropdownItem {...getPropsForDropdownItem(props.actions.buildTimeActions[ActionEnum.RESET_AND_KEEP_INDEX], props.closeOpenEditingPopups)}/>
+            <DropdownItem {...getPropsForContextMenuDropdownItem(props.actions.buildTimeActions[ActionEnum.RESET_AND_DROP_INDEX], props.closeOpenEditingPopups)}/>
+            <DropdownItem {...getPropsForContextMenuDropdownItem(props.actions.buildTimeActions[ActionEnum.RESET_AND_KEEP_INDEX], props.closeOpenEditingPopups)}/>
         </Dropdown>
     )
 }
