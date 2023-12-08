040180f87a423d05e2e3a152b80bcbde2ee91b4b
Nate Rush <naterush1997@gmail.com>
2023-12-08 12:04:42 -0800

    tests: add insert bar tests

--- tests/streamlit_ui_tests/streamlit.spec.ts
+++ tests/streamlit_ui_tests/streamlit.spec.ts
@@ -49,6 +49,10 @@ const getColumnHeaderContainer = async (mito: FrameLocator, columnName: string):
   return mito.locator('.endo-column-header-container').locator('div').filter({ hasText: columnName }).first();
 }
 
+const clickTab = async (page: Page, mito: FrameLocator, tabName: string): Promise<void> => {
+  // Button with .mito-toolbar-tabbar-tabname that has text tabName
+  await mito.locator('.mito-toolbar-tabbar-tabname').filter({ hasText: tabName }).first().click();
+}
 
 
 test('Can render Mito spreadsheet', async ({ page }) => {
@@ -310,6 +314,119 @@ test.describe('Home Tab Buttons', () => {
 
     // Check there is no AI button
     await expect(mito.getByRole('button', { name: 'AI' })).not.toBeVisible();
+  });
+
+});
+
+test.describe('Insert Tab Buttons', () => {
+
+  test('Test Pivot', async ({ page }) => {
+    const mito = await getMitoFrameWithTestCSV(page);
+    await clickTab(page, mito, 'Insert');
+
+    await clickButtonAndAwaitResponse(page, mito, { name: 'Pivot', exact: true });
+
+    await checkOpenTaskpane(mito, 'Create Pivot Table test_pivot');
+    // We test functionality elsewhere, so we skip here
+  });
+
+  test('Test Unpivot', async ({ page }) => {
+    const mito = await getMitoFrameWithTestCSV(page);
+    await clickTab(page, mito, 'Insert');
+
+    await clickButtonAndAwaitResponse(page, mito, 'Unpivot');
+    await checkOpenTaskpane(mito, 'Unpivot Dataframe');
+
+    // Check that column headers variable and value exist
+    const variable = await getColumnHeaderContainer(mito, 'variable');
+    await expect(variable).toBeVisible();
+    const value = await getColumnHeaderContainer(mito, 'value');
+    await expect(value).toBeVisible();
+
+    // Toggle .multi-toggle-box-row with text Column1
+    await mito.locator('.multi-toggle-box-row').filter({ hasText: 'Column1' }).first().click();
+    await awaitResponse(page);
+
+    // Check that column1 is now in the variable column
+    const Column1 = await getColumnHeaderContainer(mito, 'Column1');
+    await expect(Column1).toBeVisible();
+  });
+
+  test('Test Transpose', async ({ page }) => {
+    const mito = await getMitoFrameWithTestCSV(page);
+    await clickTab(page, mito, 'Insert');
+
+    await clickButtonAndAwaitResponse(page, mito, 'Transpose');
+
+    // Check that the .endo-column-header-container with text Column1 exists
+    const Column0 = await getColumnHeaderContainer(mito, '0');
+    await expect(Column0).toBeVisible();
+  });
+
+  test('Test Merge', async ({ page }) => {
+    const mito = await getMitoFrameWithTestCSV(page);
+    await importCSV(page, mito, 'test.csv');
+    await clickTab(page, mito, 'Insert');
+
+    await clickButtonAndAwaitResponse(page, mito, { name: 'Merge', exact: true });
+
+    await expect(mito.getByText('Merge Dataframes')).toBeVisible();
+    // We test merge functionality elsewhere, so we skip here
+  });
+
+  test('Test Concat', async ({ page }) => {
+    const mito = await getMitoFrameWithTestCSV(page);
+    await importCSV(page, mito, 'test.csv');
+    await clickTab(page, mito, 'Insert');
+
+    await clickButtonAndAwaitResponse(page, mito, 'Concat');
+
+    await expect(mito.getByText('Concatenate Sheet')).toBeVisible();
+    // We test concat functionality elsewhere, so we skip here
+
+  });
+
+  test('Test Anti-merge', async ({ page }) => {
+    const mito = await getMitoFrameWithTestCSV(page);
+    await importCSV(page, mito, 'test.csv');
+    await clickTab(page, mito, 'Insert');
+
+    await clickButtonAndAwaitResponse(page, mito, 'Anti-merge');
+
+    await expect(mito.getByText('Merge Dataframes')).toBeVisible();
+    await expect(mito.getByText('unique in left')).toBeVisible();
+    // We test anti-merge functionality elsewhere, so we skip here
+  });
+
+  test('Test Graph', async ({ page }) => {
+    const mito = await getMitoFrameWithTestCSV(page);
+    await clickTab(page, mito, 'Insert');
+
+    await clickButtonAndAwaitResponse(page, mito, { name: 'Graph', exact: true });
+
+    await expect(mito.getByText('Setup Graph')).toBeVisible();
+    // We test graph functionality elsewhere, so we skip here
+  });
+
+  test('Test Graph (scatter)', async ({ page }) => {
+    const mito = await getMitoFrameWithTestCSV(page);
+    await clickTab(page, mito, 'Insert');
+
+    await clickButtonAndAwaitResponse(page, mito, { name: 'Create an interactive scatter plot.' });
+
+    await expect(mito.getByText('Setup Graph')).toBeVisible();
+    await expect(mito.getByText('Scatter')).toBeVisible();
+  });
+
+  test('Test Graph (line)', async ({ page }) => {
+    const mito = await getMitoFrameWithTestCSV(page);
+    await clickTab(page, mito, 'Insert');
+
+    await clickButtonAndAwaitResponse(page, mito, { name: 'Create an interactive line graph.' });
+
+    await expect(mito.getByText('Setup Graph')).toBeVisible();
+    await expect(mito.getByText('line', {exact: true})).toBeVisible();
+
   });
 
 });
