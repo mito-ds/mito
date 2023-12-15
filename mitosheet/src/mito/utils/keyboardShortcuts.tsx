746564165851589dd7f5e579877838089cda59f1
Nate Rush <naterush1997@gmail.com>
2023-12-14 16:21:08 -0800

    mitosheet: skip if use keyboard shortcuts on select columns

--- mitosheet/src/mito/utils/keyboardShortcuts.tsx
+++ mitosheet/src/mito/utils/keyboardShortcuts.tsx
@@ -65,6 +65,7 @@ export const keyboardShortcuts: KeyboardShortcut[] = [
             ctrlKey: true,
             keys: [' ']
         },
+        skipIfInTextInput: true,
         action: ActionEnum.Select_Columns
     },
     {
