270fe2481332766ce4f28dc5a09c212f14c22488
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-01-02 12:06:01 -0500

    trymito: add www to sitemap

--- trymito.io/pages/sitemap.xml.tsx
+++ trymito.io/pages/sitemap.xml.tsx
@@ -6,7 +6,7 @@ import { getGlossaryPageInfo, getPageContentJsonArray } from '../utils/excel-to-
 import { getPosts } from '../utils/posts';
 import { SLUG_REDIRECTS } from './blog';
 
-const WEBSITE_HOST_URL = 'https://trymito.io';
+const WEBSITE_HOST_URL = 'https://www.trymito.io';
 
 function generateSiteMap(glossarySlugs: string[], blogPostSlugs: string[]) {
     return `<?xml version="1.0" encoding="UTF-8"?>
