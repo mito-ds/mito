35470203a83d718db3a0d2c1ab2752b5db80998e
Martha Cryan <martha.cryan326@gmail.com>
2024-01-09 11:42:26 -0600

    Update logic for bar and scatter plots

--- mitosheet/src/mito/components/taskpanes/Graph/graphUtils.tsx
+++ mitosheet/src/mito/components/taskpanes/Graph/graphUtils.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import { ColumnID, ColumnIDsMap, EditorState, GraphDataDict, GraphID, GraphParamsBackend, GraphParamsFrontend, SheetData, UIState } from "../../../types"
 import { intersection } from "../../../utils/arrays"
 import { getDisplayColumnHeader } from "../../../utils/columnHeaders"
-import { isDatetimeDtype } from "../../../utils/dtypes"
+import { isDatetimeDtype, isNumberDtype } from "../../../utils/dtypes"
 import { convertStringToFloatOrUndefined } from "../../../utils/numbers"
 import { convertToStringOrUndefined } from "../../../utils/strings"
 import DropdownItem from "../../elements/DropdownItem"
@@ -16,7 +16,7 @@ const DO_NOT_CHANGE_PAPER_BGCOLOR_DEFAULT = '#FFFFFF'
 const DO_NOT_CHANGE_PLOT_BGCOLOR_DEFAULT = '#E6EBF5'
 const DO_NOT_CHANGE_TITLE_FONT_COLOR_DEFAULT = '#2F3E5D'
 
-const getAxisColumnIDs = (graphType?: GraphType, selectedColumnIds?: ColumnID[]): {
+const getAxisColumnIDs = (sheetData: SheetData, graphType?: GraphType, selectedColumnIds?: ColumnID[]): {
     x_axis_column_ids: ColumnID[],
     y_axis_column_ids: ColumnID[]
 } => {
@@ -39,9 +39,21 @@ const getAxisColumnIDs = (graphType?: GraphType, selectedColumnIds?: ColumnID[])
             }
         }
     } else {
-        return {
-            x_axis_column_ids: [],
-            y_axis_column_ids: selectedColumnIds
+        if (selectedColumnIds.length === 1) {
+            return {
+                x_axis_column_ids: [],
+                y_axis_column_ids: selectedColumnIds
+            }
+        } else if (!isNumberDtype(sheetData.columnDtypeMap[selectedColumnIds[0]])) {
+            return {
+                x_axis_column_ids: [selectedColumnIds[0]],
+                y_axis_column_ids: selectedColumnIds.slice(1)
+            }
+        } else {
+            return {
+                x_axis_column_ids: [],
+                y_axis_column_ids: selectedColumnIds
+            }
         }
     }
 }
@@ -50,7 +62,7 @@ const getAxisColumnIDs = (graphType?: GraphType, selectedColumnIds?: ColumnID[])
 // unless a graph type is provided
 export const getDefaultGraphParams = (sheetDataArray: SheetData[], sheetIndex: number, graphType?: GraphType, selectedColumnIds?: ColumnID[]): GraphParamsFrontend => {
     graphType = graphType || GraphType.BAR
-    const axis_column_ids = getAxisColumnIDs(graphType, selectedColumnIds)
+    const axis_column_ids = getAxisColumnIDs(sheetDataArray[sheetIndex], graphType, selectedColumnIds)
     return {
         graphPreprocessing: {
             safety_filter_turned_on_by_user: true
