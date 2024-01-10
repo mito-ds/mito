2299e9f8fd1ea42797f04c03295d5c29e2b33132
Nate Rush <naterush1997@gmail.com>
2024-01-09 16:15:06 -0800

    test: update windows scripts

--- tests/windows-setup.bat
+++ tests/windows-setup.bat
@@ -1,18 +1,13 @@
-# Create venv and install requirements
-python3 -m venv venv;
-venv\Scripts\activate;
-pip install -r requirements.txt;
+python3 -m venv venv
+venv\Scripts\activate.bat
+pip install -r requirements.txt
 
-# Install necessary node packages
 npm install
 npx playwright install chromium firefox chrome msedge
 
-# Install mitosheet
 cd ../mitosheet
 
-# Install Python dependencies
 pip install -e ".[test]"
 
-# Install the npm dependences for Mitosheet, and build JS
 npm install
 npm run build
