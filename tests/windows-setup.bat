c8d628b33dacf140f7161948e2cbec6330726d6b
Nate Rush <naterush1997@gmail.com>
2024-01-09 16:24:11 -0800

    test: update windows setup script

--- tests/windows-setup.bat
+++ tests/windows-setup.bat
@@ -1,13 +1,20 @@
+@echo off
+
+REM Create and activate virtual environment
 python3 -m venv venv
-venv\Scripts\activate.bat
+call venv\Scripts\activate.bat
+
+REM Install Python dependencies
 pip install -r requirements.txt
 
+REM Install Node.js dependencies and Playwright browsers
 npm install
 npx playwright install chromium firefox chrome msedge
 
+REM Navigate to the mitosheet directory and install dependencies
 cd ../mitosheet
-
 pip install -e ".[test]"
 
+REM Install and build npm packages
 npm install
 npm run build
