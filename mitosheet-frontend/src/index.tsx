3f318e4366b74d3bbf38d1fc471088ed47f2fb65
Nate Rush <naterush1997@gmail.com>
2024-01-20 10:55:23 -0800

    mitosheet-frontend: import exports to give all we need

--- mitosheet-frontend/src/index.tsx
+++ mitosheet-frontend/src/index.tsx
@@ -1,13 +1,20 @@
 export { Mito } from './Mito';
-export { 
-    AnalysisData, GraphDataBackend, GraphDataDict, GraphParamsBackend, PublicInterfaceVersion, SheetData, UserProfile,
-    MitoTheme, MitoEnterpriseConfigKey
-} from "./types"
+export {
+    AnalysisData, GraphDataBackend, GraphDataDict, GraphParamsBackend, MitoEnterpriseConfigKey, MitoTheme, PublicInterfaceVersion, SheetData, UserProfile
+} from "./types";
 
 export { MitoAPI, MitoResponse } from './api/api';
 export { MAX_WAIT_FOR_SEND_CREATION, SendFunction, SendFunctionError, SendFunctionReturnType } from "./api/send";
 
 export { waitUntilConditionReturnsTrueOrTimeout } from "./utils/time";
 
-export { convertBackendtoFrontendGraphParams } from "./components/taskpanes/Graph/graphUtils"
+export { convertBackendtoFrontendGraphParams } from "./components/taskpanes/Graph/graphUtils";
 export { isInJupyterLab, isInJupyterNotebook } from './utils/location';
+
+
+export { MITO_TOOLBAR_OPEN_SEARCH_ID, MITO_TOOLBAR_REDO_ID, MITO_TOOLBAR_UNDO_ID } from './components/toolbar/Toolbar';
+export { containsGeneratedCodeOfAnalysis, containsMitosheetCallWithAnyAnalysisToReplay, containsMitosheetCallWithSpecificAnalysisToReplay, getArgsFromMitosheetCallCode, getCodeString, getLastNonEmptyLine, isMitosheetCallCode, removeWhitespaceInPythonCode } from './utils/code';
+
+export { getOperatingSystem, keyboardShortcuts } from './utils/keyboardShortcuts';
+
+export { getRandomId } from './api/api';
