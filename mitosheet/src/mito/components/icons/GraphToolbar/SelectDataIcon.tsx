4038c453368eef25ac5f4ca597416d19e97ab102
Martha Cryan <martha.cryan326@gmail.com>
2024-02-05 15:11:02 -0600

    Lint

--- mitosheet/src/mito/components/icons/GraphToolbar/SelectDataIcon.tsx
+++ mitosheet/src/mito/components/icons/GraphToolbar/SelectDataIcon.tsx
@@ -5,17 +5,17 @@ import React from 'react';
 const SelectDataIcon = (): JSX.Element => {
     return (
         <svg width="28" height="23" viewBox="0 0 28 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="0.75" y="0.863281" width="23.731" height="15.7777" fill="white" stroke="black" stroke-width="0.5"/>
-<path d="M8.80776 0.914062V16.2874M16.7694 0.914062V16.2874M0.5 6.33995H24.731M0.5 11.4644H24.731" stroke="black" stroke-width="0.5"/>
-<path d="M1 1H17V11.5H1V1Z" fill="#D0B9FE"/>
-<path d="M0.722656 6.24855V0.816406H16.8908V6.24855M0.722656 6.24855H8.84697M0.722656 6.24855V11.4985H8.84697M16.8908 6.24855V11.4985H8.84697M16.8908 6.24855H8.84697M8.84697 6.24855V11.4985M8.84697 6.24855V0.816406" stroke="#9D6CFF" stroke-width="0.5"/>
-<rect x="12.5" y="14.9922" width="6.19258" height="6.89496" fill="#F5F5F5" stroke="#F5F5F5"/>
-<rect x="20.8076" y="12.0469" width="6.19258" height="9.84244" fill="#F5F5F5" stroke="#F5F5F5"/>
-<rect x="16.6543" y="8.36328" width="6.19258" height="13.5268" fill="#F5F5F5" stroke="#F5F5F5"/>
-<rect x="13.25" y="15.7422" width="4.69258" height="5.39496" fill="#C8C6C3" stroke="#797774" stroke-width="0.5"/>
-<rect x="21.5576" y="12.7969" width="4.69258" height="8.34244" fill="#D0B9FE" stroke="#9D6CFF" stroke-width="0.5"/>
-<rect x="17.4043" y="9.11328" width="4.69258" height="12.0268" fill="white" stroke="black" stroke-width="0.5"/>
-</svg>
+            <rect x="0.75" y="0.863281" width="23.731" height="15.7777" fill="white" stroke="black" strokeWidth="0.5"/>
+            <path d="M8.80776 0.914062V16.2874M16.7694 0.914062V16.2874M0.5 6.33995H24.731M0.5 11.4644H24.731" stroke="black" strokeWidth="0.5"/>
+            <path d="M1 1H17V11.5H1V1Z" fill="#D0B9FE"/>
+            <path d="M0.722656 6.24855V0.816406H16.8908V6.24855M0.722656 6.24855H8.84697M0.722656 6.24855V11.4985H8.84697M16.8908 6.24855V11.4985H8.84697M16.8908 6.24855H8.84697M8.84697 6.24855V11.4985M8.84697 6.24855V0.816406" stroke="#9D6CFF" strokeWidth="0.5"/>
+            <rect x="12.5" y="14.9922" width="6.19258" height="6.89496" fill="#F5F5F5" stroke="#F5F5F5"/>
+            <rect x="20.8076" y="12.0469" width="6.19258" height="9.84244" fill="#F5F5F5" stroke="#F5F5F5"/>
+            <rect x="16.6543" y="8.36328" width="6.19258" height="13.5268" fill="#F5F5F5" stroke="#F5F5F5"/>
+            <rect x="13.25" y="15.7422" width="4.69258" height="5.39496" fill="#C8C6C3" stroke="#797774" strokeWidth="0.5"/>
+            <rect x="21.5576" y="12.7969" width="4.69258" height="8.34244" fill="#D0B9FE" stroke="#9D6CFF" strokeWidth="0.5"/>
+            <rect x="17.4043" y="9.11328" width="4.69258" height="12.0268" fill="white" stroke="black" strokeWidth="0.5"/>
+        </svg>
 
     )
 }
