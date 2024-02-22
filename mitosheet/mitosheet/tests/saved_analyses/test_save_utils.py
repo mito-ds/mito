9c43386f558082d4a865af78cc71455baabd705c
Martha Cryan <martha.cryan326@gmail.com>
2024-02-21 19:51:32 -0600

    Add separate test for code

--- mitosheet/mitosheet/tests/saved_analyses/test_save_utils.py
+++ mitosheet/mitosheet/tests/saved_analyses/test_save_utils.py
@@ -280,20 +280,22 @@ def test_upgrades_old_analysis_before_replaying_it():
     assert new_mito.dfs[1].equals(
         pd.DataFrame({'A': [123], 'B_sum': [123]})
     )
-    # Check that the code saved is correct
-    assert json.loads(new_mito.analysis_data_json)['code'] == [
-        "from mitosheet.public.v1 import *", 
-        "", 
-        "# Pivoted df1 into df1_pivot", 
-        "tmp_df = df1[['B', 'A']].copy()", 
-        "pivot_table = tmp_df.pivot_table(\n    index=['A'],\n    values=['B'],\n    aggfunc={'B': ['sum']}\n)", 
-        "pivot_table = pivot_table.set_axis([flatten_column_header(col) for col in pivot_table.keys()], axis=1)", 
-        "df1_pivot = pivot_table.reset_index()", 
-        "", 
-        "# Renamed headers for compatibility with previous Mito versions", 
-        "# Rename headers to make them work with Mito", 
-        "df1_pivot.rename(columns={\"B sum\": \"B_sum\"}, inplace=True)", 
-        ""
+
+def test_save_analysis_has_code():
+    mito = create_mito_wrapper_with_data([1, 2, 3])
+    mito.filter(0, 'A', 'And', FC_NUMBER_EXACTLY, 2)
+
+    random_name = 'UUID-test_save' + str(random.random())
+    mito.save_analysis(random_name)
+
+    saved_analysis = read_and_upgrade_analysis(random_name, ['df1'])
+    assert len(saved_analysis['steps_data']) == 1
+    assert saved_analysis['code'] == [
+        'from mitosheet.public.v3 import *',
+        '',
+        '# Filtered A',
+        "df1 = df1[df1['A'] == 2]",
+        ''
     ]
 
 def test_save_analysis_saves_skipped_steps():
