f1af07784f3eec858cbd06a8fc88105431d386b0
Nate Rush <naterush1997@gmail.com>
2024-02-23 12:56:48 +0100

    mitosheet: fix up type issues in taskpanes

--- mitosheet/src/mito/components/taskpanes/DefaultTaskpane/DefaultTaskpaneErrorBoundry.tsx
+++ mitosheet/src/mito/components/taskpanes/DefaultTaskpane/DefaultTaskpaneErrorBoundry.tsx
@@ -7,45 +7,46 @@ import { MitoAPI } from '../../../api/api';
 type PropsType = {
     setUIState: React.Dispatch<React.SetStateAction<UIState>>;
     taskpaneHeader: string;
-    mitoAPI?: MitoAPI
+    mitoAPI?: MitoAPI,
+    children: React.ReactNode
 };
 
 // If the error is not caught, the error will be logged to the console and the taskpane will be closed.
 // This is to prevent the entire sheet from crashing, and just allows the user to reopen the taskpane.
 class DefaultTaskpaneErrorBoundry extends React.Component<PropsType, {hasError: boolean}> {
-  constructor(props: PropsType) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error: Error) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true };
-  }
-
-  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    // You can also log the error to an error reporting service
-    this.props.setUIState(prevUIState => {
-        return {
-            ...prevUIState,
-            currOpenTaskpane: {type: TaskpaneType.NONE}
-        }
-    })
-
-    void this.props.mitoAPI?.log('taskpane_render_failed', {
-        js_error: error,
-        js_error_info: errorInfo,
-        js_taskpane_header: this.props.taskpaneHeader
-    });
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <></>;
+    constructor(props: PropsType) {
+        super(props);
+        this.state = { hasError: false };
     }
 
-    return this.props.children;
-  }
+    static getDerivedStateFromError() {
+    // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    // You can also log the error to an error reporting service
+        this.props.setUIState(prevUIState => {
+            return {
+                ...prevUIState,
+                currOpenTaskpane: {type: TaskpaneType.NONE}
+            }
+        })
+
+        void this.props.mitoAPI?.log('taskpane_render_failed', {
+            js_error: error,
+            js_error_info: errorInfo,
+            js_taskpane_header: this.props.taskpaneHeader
+        });
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <></>;
+        }
+
+        return this.props.children;
+    }
 }
 
 export default DefaultTaskpaneErrorBoundry;
