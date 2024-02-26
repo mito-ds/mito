9d696bf67fc89fcb27694700a1470e9c96f75f22
Martha Cryan <martha.cryan326@gmail.com>
2024-02-26 15:55:50 -0600

    Add comments

--- tests/notebook_ui_tests/notebook.mitosheet.spec.ts
+++ tests/notebook_ui_tests/notebook.mitosheet.spec.ts
@@ -16,11 +16,20 @@ test.describe('Mitosheet Jupyter Notebook integration', () => {
   });
   
   test('renders a mitosheet.sheet()', async ({ page, tmpPath }) => {
+    // Open the notebook (see beforeAll hook)
     void page.goto(`notebooks/${tmpPath}/${NOTEBOOK}`);
+
+    // Check that the page has loaded with the correct cell contents.
     await expect(page.getByText('import mitosheet')).toBeVisible();
+
+    // Run the cell using shift+enter
     await page.locator('.cell').first().click();
     await page.keyboard.press('Shift+Enter');
+
+    // Check that the mitosheet has been rendered
     await expect(page.locator('.mito-toolbar-button', { hasText: 'Insert' })).toBeVisible();
+
+    // Check that clicking the Insert button adds a new column
     expect(await page.locator('.endo-column-header-final-container').count()).toBe(3);
     await page.locator('.mito-toolbar-button', { hasText: 'Insert' }).click();
     await awaitResponse(page);
