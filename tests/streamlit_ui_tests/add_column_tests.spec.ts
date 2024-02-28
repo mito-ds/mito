3eaadd0f9e1faec768b732ba79f074df408810d9
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-02-28 17:39:20 -0500

    tests: add test conditions to actual tests

--- tests/streamlit_ui_tests/add_column_tests.spec.ts
+++ tests/streamlit_ui_tests/add_column_tests.spec.ts
@@ -3,19 +3,41 @@ import { getMitoFrameWithTypeCSV, createNewColumn, clickButtonAndAwaitResponse }
 
 
 test('Add new column to end using context menu', async ({ page }) => {
+    const columnHeader = 'Column4';
+
     const mito = await getMitoFrameWithTypeCSV(page);
-    await createNewColumn(page, mito, 3, 'Column4');
+    await createNewColumn(page, mito, 3, columnHeader);
+
+    // Check that the new column was added in correct position
+    const lastColumnHeader = await mito.locator('.endo-column-header-container').last().innerText();
+    expect(lastColumnHeader == columnHeader);
 });
 
 test('Add new column to start using context menu', async ({ page }) => {
+    const columnHeader = 'Column4'
+
     const mito = await getMitoFrameWithTypeCSV(page);
-    await createNewColumn(page, mito, 0, 'Column4');
+    await createNewColumn(page, mito, 0, columnHeader);
+
+    // Check that the new column was added in correct position
+    const lastColumnHeader = await mito.locator('.endo-column-header-container').first().innerText();
+    expect(lastColumnHeader == columnHeader);
 });
 
 test('Add multiple new columns using context menu', async ({ page }) => {
+    const columnHeader4 = 'Column4'
+    const columnHeader5 = 'Column4'
+
     const mito = await getMitoFrameWithTypeCSV(page);
-    await createNewColumn(page, mito, 2, 'Column4');
-    await createNewColumn(page, mito, 2, 'Column5');
+    await createNewColumn(page, mito, 2, columnHeader4);
+    await createNewColumn(page, mito, 2, columnHeader5);
+
+    // Check that the new column was added in correct position
+    const lastColumnHeader = await mito.locator('.endo-column-header-container').nth(2).innerText();
+    expect(lastColumnHeader == columnHeader4);
+
+    const secondLastColumnHeader = await mito.locator('.endo-column-header-container').nth(3).innerText();
+    expect(secondLastColumnHeader == columnHeader5);
 });
 
 
