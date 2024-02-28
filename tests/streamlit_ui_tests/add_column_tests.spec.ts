a159c1679292fddcffb34f1a350ed949e0f3f8eb
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-02-28 17:20:37 -0500

    tests: ad add column tests

---
+++ tests/streamlit_ui_tests/add_column_tests.spec.ts
@@ -0,0 +1,27 @@
+import { FrameLocator, Page, expect, test } from '@playwright/test';
+import { getMitoFrameWithTypeCSV, createNewColumn, clickButtonAndAwaitResponse } from './utils';
+
+
+test('Add new column to end using context menu', async ({ page }) => {
+    const mito = await getMitoFrameWithTypeCSV(page);
+    await createNewColumn(page, mito, 3, 'Column4');
+});
+
+test('Add new column to start using context menu', async ({ page }) => {
+    const mito = await getMitoFrameWithTypeCSV(page);
+    await createNewColumn(page, mito, 0, 'Column4');
+});
+
+test('Add new column to middle using context menu', async ({ page }) => {
+    const mito = await getMitoFrameWithTypeCSV(page);
+    await createNewColumn(page, mito, 2, 'Column4');
+});
+
+test('Add multiple new columns using context menu', async ({ page }) => {
+    const mito = await getMitoFrameWithTypeCSV(page);
+    await createNewColumn(page, mito, 2, 'Column4');
+    await createNewColumn(page, mito, 2, 'Column5');
+});
+
+
+
