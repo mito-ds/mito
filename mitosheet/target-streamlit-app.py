3f62cf8094fa73cfdc11d31ec1cb2a79b24678c7
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-03-10 17:21:36 -0400

    mitosheet: update app

--- mitosheet/target-streamlit-app.py
+++ mitosheet/target-streamlit-app.py
@@ -1,20 +1,22 @@
 import streamlit as st
 from mitosheet.streamlit.v1 import spreadsheet
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
-st.title('Tesla Stock Volume Analysis')
+st.title('SKU analysis app')
 
-CSV_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv'
-new_dfs, code = spreadsheet(CSV_URL, conditional_formats={
-    'columnHeaders': ['open'],
-    'filters': {
-	    'condition': 'greater_than_or_equal',
-		'value': 5
-	},
-    'backgroundColor': '#000000',
-    'color': '#FFFFFF'
-})
-
-st.write(new_dfs)
-st.code(code)
+df = pd.read_csv('./SKU.csv')
+new_dfs, code = spreadsheet(
+    df, 
+    conditional_formats={
+        'columnHeaders': ['quantity'],
+        'filters': {
+            'condition': 'greater_than_or_equal',
+            'value': 5
+        },
+        'backgroundColor': '#ffc9bb',
+        'color': '#c61a09'
+    },
+    height='900px'
+)
