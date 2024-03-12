4059d826cc8ba97d9abc4bbaee76dae87db1af7a
Martha Cryan <martha.cryan326@gmail.com>
2024-03-11 19:30:24 -0500

    Reset test

--- tests/streamlit_test.py
+++ tests/streamlit_test.py
@@ -1,12 +1,8 @@
 from mitosheet.streamlit.v1 import spreadsheet
 import streamlit as st
-import pandas as pd
 
 st.set_page_config(layout="wide")
 
-file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
-if file is not None:
-    df = pd.read_csv(file)
 
-    dfs, code = spreadsheet(df, import_folder='data')
-    st.code(code)
+dfs, code = spreadsheet(import_folder='data')
+st.code(code)
