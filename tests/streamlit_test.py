0ca7ae485b5e70993d1cc334dd4136db619f49af
Martha Cryan <martha.cryan326@gmail.com>
2024-03-11 19:05:36 -0500

    try using the newest version of mac

--- tests/streamlit_test.py
+++ tests/streamlit_test.py
@@ -1,8 +1,12 @@
 from mitosheet.streamlit.v1 import spreadsheet
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
+file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
+if file is not None:
+    df = pd.read_csv(file)
 
-dfs, code = spreadsheet(import_folder='data', height='520px')
-st.code(code)
+    dfs, code = spreadsheet(df, import_folder='data')
+    st.code(code)
