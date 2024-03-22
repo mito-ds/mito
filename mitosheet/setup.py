b46ec11a9ca4a010fac116392b52344d2b5ce0c1
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-03-22 10:11:37 -0400

    mitosheet:revert my attempts to fix

--- mitosheet/setup.py
+++ mitosheet/setup.py
@@ -1,43 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
-
 """
 The mitosheet package is distributed under the mitosheet and mitosheet3
 package names on pip. The package.json will tell you all you need to 
 know about which one we are in currently using.
-
 As such, this setup.py script reads in the package.json and sets up
 the proper package.
 """
-
 import json
 import os
 from glob import glob
 from pathlib import Path
 from typing import List, Tuple
-
 from setuptools import find_packages, setup
-
 HERE = Path(__file__).parent.resolve()
-
 package_json = json.loads(open('package.json').read())
 lab_path = Path(HERE, 'mitosheet', 'labextension')
 notebook_path = Path(HERE, 'mitosheet', 'nbextension')
-
 data_files_spec = [
     # Notebook extension data files
     ('share/jupyter/nbextensions/mitosheet', notebook_path, '**'),
     ('etc/jupyter/nbconfig/notebook.d', str(HERE), 'mitosheet.json'),
-
     # Lab extension data files
     ("share/jupyter/labextensions/mitosheet", str(lab_path), "**"),
     ("share/jupyter/labextensions/mitosheet", str(HERE), "install.json"),
 ]
-
-
 def get_data_files_from_data_files_spec(
     data_specs: List[Tuple[str, str, str]],
 ):
@@ -46,16 +35,12 @@ def get_data_files_from_data_files_spec(
     this function will return a list of tuples of (data_file_path, [files])
     in the format that setuptools expects.
     """
-
     file_data = {}
-
     for (data_file_path, directory_to_search, pattern_to_find) in data_specs or []:
-
         # Get the directory to search ready
         if os.path.isabs(directory_to_search):
             directory_to_search = os.path.relpath(directory_to_search)
         directory_to_search = directory_to_search.rstrip("/")
-
         # Get all non-directory files that match the pattern, searching recursively
         files = [
             f for f in glob(
@@ -63,33 +48,27 @@ def get_data_files_from_data_files_spec(
                 recursive=True
             ) if not os.path.isdir(f)
         ]
-
         offset = len(directory_to_search) + 1
-
         for file_name in files:
             relative_path = str(Path(file_name).parent)[offset:]
             full_data_file_path = Path().joinpath(data_file_path, relative_path).as_posix()
-
             if full_data_file_path not in file_data:
                 file_data[full_data_file_path] = []
-
             file_data[full_data_file_path].append(file_name)
-
     # Turn to list and sort by length, to be consistent (and maybe cuz we need to for folder creation?)
     data_files = sorted(file_data.items(), key=lambda x: len(x[0]))
     
     return data_files
-
 data_files = get_data_files_from_data_files_spec(data_files_spec)   
-
 setup_args = dict(
     name                    = 'mitosheet',
     version                 = package_json["version"],
-    project_url             = package_json["homepage"],
+    url                     = package_json["homepage"],
     author                  = package_json["author"]["name"],
     author_email            = package_json["author"]["email"],
+    description             = package_json["description"],
     license                 = "GNU Affero General Public License v3",
-    description="""
+    long_description="""
     To learn more about Mito, checkout out our documentation: https://docs.trymito.io/getting-started/installing-mito\n\n
     Before installing Mito \n\n
     1. Check that you have Python 3.6 or above. To check your version of Python, open a new terminal, and type python3 --version. If you need to install or update Python, restart your terminal after doing so.\n\n
@@ -102,7 +81,7 @@ setup_args = dict(
     import mitosheet\n\n
     mitosheet.sheet()\n\n
     """,
-    description_content_type = "text/markdown",
+    long_description_content_type = "text/markdown",
     packages                 = find_packages(exclude=['deployment']),
     include_package_data     = True,
     package_data             = {'': ['*.js', '*.css', '*.html']},
@@ -145,12 +124,12 @@ setup_args = dict(
             "flask"
         ]
     },
-    zip_safe                = False,
-    python_requires         = ">=3.6",
-    platforms               = "Linux, Mac OS X, Windows",
-    classifiers             = [
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Programming Language :: Python",
+     zip_safe                = False,
+     python_requires         = ">=3.6",
+     platforms               = "Linux, Mac OS X, Windows",
+     classifiers             = [
+         "License :: OSI Approved :: GNU Affero General Public License v3",
+         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
@@ -161,6 +140,5 @@ setup_args = dict(
         "Framework :: Jupyter",
     ],
 )
-
 if __name__ == '__main__':
     setup(**setup_args)
