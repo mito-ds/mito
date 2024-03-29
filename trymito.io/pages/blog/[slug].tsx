2af4290e8ce47126a8fbc1681a78dc0b803e6a78
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-03-29 13:32:37 -0400

    trymito: use ghost og_image

--- trymito.io/pages/blog/[slug].tsx
+++ trymito.io/pages/blog/[slug].tsx
@@ -84,6 +84,8 @@ const PostPage = (props: {post: PostOrPage}) => {
         <meta
           name="description"
           content={props.post.meta_description || props.post.excerpt?.slice(0, 99)}
+          
+          og-image={props.post.og_image}
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
