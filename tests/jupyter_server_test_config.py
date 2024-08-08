6fe0381418f274e69f7e35d985f16764818efddc
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-08-08 16:49:08 -0400

    tests: fix jupyter lab frontend tests

--- tests/jupyter_server_test_config.py
+++ tests/jupyter_server_test_config.py
@@ -1,11 +1,3 @@
-from tempfile import mkdtemp
+from jupyterlab.galata import configure_jupyter_server
 
-c.ServerApp.port = 8888
-c.ServerApp.port_retries = 0
-c.ServerApp.open_browser = False
-
-c.ServerApp.root_dir = mkdtemp(prefix='galata-test-')
-c.ServerApp.token = ""
-c.ServerApp.password = ""
-c.ServerApp.disable_check_xsrf = True
-c.LabApp.expose_app_in_browser = True
+configure_jupyter_server(c)
--- tests/mac-setup.sh
+++ tests/mac-setup.sh
@@ -13,7 +13,7 @@ npm install
 # install only the necessary browsers.
 if [ $# -eq 0 ]
   then
-    npx playwright install chromium webkit firefox chrome
+    npx playwright install chromium webkit firefox # chrome
 else
     npx playwright install $1
     npx playwright install
@@ -26,5 +26,5 @@ cd ../mitosheet
 pip install -e ".[test]"
 
 # Install the npm dependences for Mitosheet, and build JS
-npm install
-npm run build
+yarn install
+yarn run build
