73724930c9903b973c33d343eed1f100a8fc540c
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-09-04 13:51:03 -0700

    mito-ai: update data file packaging for extension

--- mito-ai/setup.py
+++ mito-ai/setup.py
@@ -1,9 +1,55 @@
+from glob import glob
+from typing import List, Tuple
 from setuptools import setup, find_packages
 import json
 from pathlib import Path
+import os
 
 HERE = Path(__file__).parent.resolve()
 package_json = json.loads(open('package.json').read())
+lab_path = Path(HERE, 'mito-ai', 'labextension')
+
+
+data_files_spec = [
+    # Lab extension data files
+    ("share/jupyter/labextensions/mito-ai", str(lab_path), "**"),
+    ("share/jupyter/labextensions/mito-ai", str(HERE), "install.json")
+]
+
+def get_data_files_from_data_files_spec(
+    data_specs: List[Tuple[str, str, str]],
+):
+    """
+    Given tuples of (data_file_path, directory_to_search, pattern_to_find),
+    this function will return a list of tuples of (data_file_path, [files])
+    in the format that setuptools expects.
+    """
+    file_data = {}
+    for (data_file_path, directory_to_search, pattern_to_find) in data_specs or []:
+        # Get the directory to search ready
+        if os.path.isabs(directory_to_search):
+            directory_to_search = os.path.relpath(directory_to_search)
+        directory_to_search = directory_to_search.rstrip("/")
+        # Get all non-directory files that match the pattern, searching recursively
+        files = [
+            f for f in glob(
+                Path().joinpath(directory_to_search, pattern_to_find).as_posix(), 
+                recursive=True
+            ) if not os.path.isdir(f)
+        ]
+        offset = len(directory_to_search) + 1
+        for file_name in files:
+            relative_path = str(Path(file_name).parent)[offset:]
+            full_data_file_path = Path().joinpath(data_file_path, relative_path).as_posix()
+            if full_data_file_path not in file_data:
+                file_data[full_data_file_path] = []
+            file_data[full_data_file_path].append(file_name)
+    # Turn to list and sort by length, to be consistent (and maybe cuz we need to for folder creation?)
+    data_files = sorted(file_data.items(), key=lambda x: len(x[0]))
+    
+    return data_files
+
+data_files = get_data_files_from_data_files_spec(data_files_spec)   
 
 # Read the content of README.md for the long description
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -55,17 +101,8 @@ setup(
     },
     include_package_data=True,  # Ensures labextension files are included
     package_data={
-        "": ["labextension/**/*", "labextension/install.json"],
+        "": ["labextension/**/*"],
     },
-    data_files=[
-        (
-            "share/jupyter/labextensions/mito-ai", 
-            [
-                "mito-ai/labextension/static/style.js", 
-                "mito-ai/labextension/package.json",
-                "mito-ai/labextension/install.json"
-            ]
-        ),
-    ],
+    data_files=data_files,
     zip_safe=False,
 )
