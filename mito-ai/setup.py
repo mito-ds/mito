bae9d32cd799550681a1ee59f87a834cd3dce902
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-09-04 11:11:05 -0700

    mito-ai: update setup.py to use same version as package.json

--- mito-ai/setup.py
+++ mito-ai/setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+import json
+from pathlib import Path
+
+HERE = Path(__file__).parent.resolve()
+package_json = json.loads(open('package.json').read())
 
 # Read the content of README.md for the long description
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -6,13 +11,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mito-ai",
-    version="0.1.0",  # Version handling is now static, adjust manually
+    version=package_json['version'],
     author="Aaron Diamond-Reivich",
     author_email="aaron@sagacollab.com",
-    description="A short description of the project",  # If dynamic, handle manually
+    description="AI chat for JupyterLab",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license="BSD-3-Clause",  # Assuming the BSD license since you use the BSD classifier
+    license="GNU Affero General Public License v3",
     python_requires=">=3.8",
     classifiers=[
         "Framework :: Jupyter",
