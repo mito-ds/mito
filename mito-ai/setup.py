ed8598b1e45d11477215e091198b275bb8309b77
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-09-04 10:19:10 -0700

    mito-ai: cleanup

--- mito-ai/setup.py
+++ mito-ai/setup.py
@@ -35,6 +35,13 @@ setup(
         "jupyterlab>=4.0.0,<5",
         "openai",
     ],
+    extras_require = {
+        'deploy': [
+            'wheel==0.42.0', 
+            'twine==5.1.1',
+            "setuptools==56.0.0"
+        ],
+    },
     keywords=["AI", "Jupyter", "Mito"],
     entry_points={
         "jupyter_serverproxy_servers": [
