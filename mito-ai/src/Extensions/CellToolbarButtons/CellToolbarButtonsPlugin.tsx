cac1398235cfb78b700fe9a69b87d06037950c9e
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-10-17 14:46:13 -0400

    mito-ai: add explain code button

--- mito-ai/src/Extensions/CellToolbarButtons/CellToolbarButtonsPlugin.tsx
+++ mito-ai/src/Extensions/CellToolbarButtons/CellToolbarButtonsPlugin.tsx
@@ -1,25 +1,18 @@
 import {
     JupyterFrontEnd,
     JupyterFrontEndPlugin
-  } from '@jupyterlab/application';
-  import { INotebookTracker } from '@jupyterlab/notebook';
-  import { markdownIcon, runIcon } from '@jupyterlab/ui-components';
-  
-  const CommandIds = {
-    /**
-     * Command to render a markdown cell.
-     */
-    renderMarkdownCell: 'toolbar-button:render-markdown-cell',
-    /**
-     * Command to run a code cell.
-     */
-    runCodeCell: 'toolbar-button:run-code-cell'
-  };
-  
-  /**
-   * Initialization data for the @jupyterlab-examples/cell-toolbar extension.
-   */
-  const CellToolbarButtonsPlugin: JupyterFrontEndPlugin<void> = {
+} from '@jupyterlab/application';
+import { INotebookTracker } from '@jupyterlab/notebook';
+import { COMMAND_MITO_AI_SEND_MESSAGE } from '../../commands';
+import { LabIcon } from '@jupyterlab/ui-components';
+import LightbulbIcon from '../../../src/icons/LightbulbIcon.svg'
+
+export const lightBulbIcon = new LabIcon({
+    name: 'mito_ai',
+    svgstr: LightbulbIcon
+});
+
+const CellToolbarButtonsPlugin: JupyterFrontEndPlugin<void> = {
     // Important: The Cell Toolbar Buttons are added to the toolbar registry via the schema/plugin.json file.
     // The id here must be mito-ai:plugin otherwise the buttons are not successfull added. My understanding is that
     // the id must match the name of the package and `plugin` must be used when working with the schema/plugin.json file.
@@ -27,31 +20,22 @@ import {
     description: 'A JupyterLab extension to add cell toolbar buttons.',
     autoStart: true,
     requires: [INotebookTracker],
-    activate: (app: JupyterFrontEnd, tracker: INotebookTracker) => {
-      const { commands } = app;
+    activate: (app: JupyterFrontEnd, notebookTracker: INotebookTracker) => {
+        const { commands } = app;
 
-      console.log("HERE NOW")
-  
-      /* Adds a command enabled only on code cell */
-      commands.addCommand(CommandIds.runCodeCell, {
-        icon: runIcon,
-        caption: 'Run a code cell',
-        execute: () => {
-          commands.execute('notebook:run-cell');
-        },
-        isVisible: () => tracker.activeCell?.model.type === 'code'
-      });
-  
-      /* Adds a command enabled only on markdown cell */
-      commands.addCommand(CommandIds.renderMarkdownCell, {
-        icon: markdownIcon,
-        caption: 'Render a markdown cell',
-        execute: () => {
-          commands.execute('notebook:run-cell');
-        },
-        isVisible: () => tracker.activeCell?.model.type === 'markdown'
-      });
+        // Important: To add a button to the cell toolbar, the command must start with "toolbar-button:"
+        // and the command must match the command in the schema/plugin.json file.
+        commands.addCommand('toolbar-button:explain-code', {
+            icon: lightBulbIcon,
+            caption: 'Explain code',
+            execute: () => {
+                // In order to click on the cell toolbar button, that cell must be the active cell, 
+                // so the Ai Chat taskpane will take care of providing the cell context.
+                app.commands.execute(COMMAND_MITO_AI_SEND_MESSAGE, { input: `Explain this code` });
+            },
+            isVisible: () => notebookTracker.activeCell?.model.type === 'code'
+        });
     }
-  };
-  
-  export default CellToolbarButtonsPlugin;
+};
+
+export default CellToolbarButtonsPlugin;
