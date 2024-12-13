386fe7f828eab0992ca0b5b92eb96860cb2fbe41
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-12-13 10:44:29 -0500

    setup.py: pin specific version of setuptools

--- mito-ai/setup.py
+++ mito-ai/setup.py
@@ -96,7 +96,8 @@ setup(
         'deploy': [
             'wheel==0.42.0', 
             'twine==5.1.1',
-            'setuptools>=61.0.0,<70.0.0'
+            'setuptools==68.0.0'
+            
         ],
         'test': [
             'pytest==8.3.4',
