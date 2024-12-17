1e228d4d9b1fb76d841b001b71899845bdee8da3
Vibhatha Lakmal Abeykoon <vibhatha@gmail.com>
2024-12-17 20:30:24 +0530

    fix: adding test template

---
+++ tests/mitoai_ui_tests/markdownGeneration.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test } from '@jupyterlab/galata';
+
+test.describe.configure({ mode: 'parallel' });
+
+test.describe('Mito AI Markdown Generation', () => {
+  // TODO: Add tests here
+  test.only('Preview and Accept AI Generated Code', async ({ page }) => {
+    expect(true).toBe(true);
+  });
+});
+
