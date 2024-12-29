0048c4a6d9e9d0e9088cbd31cc7eb3288381d076
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-12-29 16:51:18 -0500

    tests: fix integration test

--- tests/mitoai_ui_tests/inlineComplete.spec.ts
+++ tests/mitoai_ui_tests/inlineComplete.spec.ts
@@ -1,10 +1,11 @@
 // TODO: DELETE THIS FILE LATER (maybe)
-import { expect, test } from '@jupyterlab/galata';
+import { expect, galata, test } from '@jupyterlab/galata';
 import {
     createAndRunNotebookWithCells,
     waitForIdle,
     typeInNotebookCell,
-    selectCell
+    selectCell,
+    getCodeFromCell
 } from '../jupyter_utils/jupyterlab_utils';
 
 const GHOST_SELECTOR = ".jp-GhostText";
@@ -16,24 +17,28 @@ test.beforeEach(async ({ page }) => {
 });
 
 
-test('Inline completion', async ({ page }) => {
-    await createAndRunNotebookWithCells(page, ['import pandas as pd']);
-    await waitForIdle(page);
+test.describe("inline completion integration test", () => {
 
-    await typeInNotebookCell(page, 1, 'def sum(a, b):');
-    await waitForIdle(page);
+    test.only('Inline completion', async ({ page }) => {
+        await createAndRunNotebookWithCells(page, ['import pandas as pd']);
+        await waitForIdle(page);
 
-    await selectCell(page, 1);
-    await waitForIdle(page);
+        await typeInNotebookCell(page, 1, 'def sum(a, b):');
+        await waitForIdle(page);
 
-    // sleep for 5s
-    await page.waitForTimeout(5000);
+        await selectCell(page, 1);
+        await waitForIdle(page);
 
-    expect.soft(page.locator(GHOST_SELECTOR)).toHaveCount(1);
-    expect
-        .soft((await page.notebook.getCellLocator(1))!.getByRole("textbox"))
-        .toContainText("def sum(a, b):");
-    expect
-        .soft((await page.notebook.getCellLocator(1))!.getByRole("textbox"))
-        .toContainText("return a + b");
+        // sleep for 5s
+        await page.waitForTimeout(5000);
+
+        expect.soft(page.locator(GHOST_SELECTOR)).toBeVisible();
+        expect.soft((await page.notebook.getCellLocator(1))!.getByRole("textbox")).toContainText("def sum(a, b):");
+
+        await page.keyboard.press("Tab");
+
+        expect.soft(page.locator(GHOST_SELECTOR)).toHaveCount(0);
+        expect((await page.notebook.getCellLocator(1))!.getByRole("textbox")).toHaveText("def sum(a, b):\n    return a + b");
+
+    })
 });
