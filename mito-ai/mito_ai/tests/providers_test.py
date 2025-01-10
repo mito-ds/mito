0fddae51bca1ecf33e850df5a91ce951329fe0e8
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-01-10 16:42:26 -0500

    Added inline limit tests

--- mito-ai/mito_ai/tests/providers_test.py
+++ mito-ai/mito_ai/tests/providers_test.py
@@ -2,7 +2,13 @@ import os
 from datetime import datetime
 from unittest.mock import patch
 from mito_ai.providers import OpenAIProvider
-from mito_ai.utils.open_ai_utils import OPEN_SOURCE_AI_COMPLETIONS_LIMIT
+from mito_ai.utils.open_ai_utils import (
+    OPEN_SOURCE_AI_COMPLETIONS_LIMIT,
+    OPEN_SOURCE_INLINE_COMPLETIONS_LIMIT,
+    check_mito_server_quota,
+)
+
+REALLY_OLD_DATE = "2020-01-01"
 
 
 def test_os_user_mito_server_below_limit():
@@ -27,17 +33,30 @@ def test_os_user_mito_server_below_limit():
 
 def test_os_user_mito_server_above_limit():
     """
-    Open source user, with no OpenAI API key set (Mito server), above chat limit.
+    Open source user, with no OpenAI API key set (Mito server), above both limits.
     An error should be thrown.
     """
 
     llm = OpenAIProvider()
     today = datetime.now().strftime("%Y-%m-%d")
 
+    # Above the chat limit
     with (
         patch.dict(os.environ, {"OPENAI_API_KEY": ""}),
         patch("mito_ai.providers._num_usages", OPEN_SOURCE_AI_COMPLETIONS_LIMIT + 1),
         patch("mito_ai.providers._first_usage_date", today),
+    ):
+        capabilities = llm.capabilities
+
+        assert capabilities.provider == "Mito server"
+        assert llm.last_error is not None
+        assert llm.last_error.title == "mito_server_free_tier_limit_reached"
+
+    # Above the inline limit
+    with (
+        patch.dict(os.environ, {"OPENAI_API_KEY": ""}),
+        patch("mito_ai.providers._num_usages", OPEN_SOURCE_AI_COMPLETIONS_LIMIT),
+        patch("mito_ai.providers._first_usage_date", REALLY_OLD_DATE),
     ):
         capabilities = llm.capabilities
 
@@ -67,16 +86,27 @@ def test_os_user_openai_key_set_below_limit():
 
 def test_os_user_openai_key_set_above_limit():
     """
-    Open source user, with OpenAI API key set, above chat limit.
+    Open source user, with OpenAI API key set, above both limits.
     No error should be thrown, since the user is using their own key.
     """
 
     llm = OpenAIProvider()
     today = datetime.now().strftime("%Y-%m-%d")
 
+    # Above the chat limit
     with (
         patch("mito_ai.providers._num_usages", OPEN_SOURCE_AI_COMPLETIONS_LIMIT + 1),
         patch("mito_ai.providers._first_usage_date", today),
+    ):
+        capabilities = llm.capabilities
+
+        assert "user key" in capabilities.provider
+        assert llm.last_error is None
+
+    # Above the inline limit
+    with (
+        patch("mito_ai.providers._num_usages", OPEN_SOURCE_AI_COMPLETIONS_LIMIT),
+        patch("mito_ai.providers._first_usage_date", REALLY_OLD_DATE),
     ):
         capabilities = llm.capabilities
 
@@ -107,17 +137,30 @@ def test_pro_user_mito_server_set_below_limit():
 
 def test_pro_user_mito_server_above_limit():
     """
-    Pro user, with no OpenAI API key set (Mito server), with usage above chat limit.
+    Pro user, with no OpenAI API key set (Mito server), with usage above both limits.
     No error should be thrown since pro users don't have limits.
     """
 
     llm = OpenAIProvider()
     today = datetime.now().strftime("%Y-%m-%d")
 
+    # Above the chat limit
     with (
         patch.dict(os.environ, {"OPENAI_API_KEY": ""}),
         patch("mito_ai.providers._num_usages", OPEN_SOURCE_AI_COMPLETIONS_LIMIT + 1),
         patch("mito_ai.providers._first_usage_date", today),
+        patch("mito_ai.utils.open_ai_utils.is_pro", return_value=True),
+    ):
+        capabilities = llm.capabilities
+
+        assert capabilities.provider == "Mito server"
+        assert llm.last_error is None
+
+    # Above the inline limit
+    with (
+        patch.dict(os.environ, {"OPENAI_API_KEY": ""}),
+        patch("mito_ai.providers._num_usages", OPEN_SOURCE_AI_COMPLETIONS_LIMIT),
+        patch("mito_ai.providers._first_usage_date", REALLY_OLD_DATE),
         patch("mito_ai.utils.open_ai_utils.is_pro", return_value=True),
     ):
         capabilities = llm.capabilities
@@ -148,16 +191,28 @@ def test_pro_user_openai_key_set_below_limit():
 
 def test_pro_user_openai_key_set_above_limit():
     """
-    Pro user, with OpenAI API key set, above chat limit.
+    Pro user, with OpenAI API key set, above both limits.
     No error should be thrown since pro users don't have limits.
     """
 
     llm = OpenAIProvider()
     today = datetime.now().strftime("%Y-%m-%d")
 
+    # Above the chat limit
     with (
         patch("mito_ai.providers._num_usages", OPEN_SOURCE_AI_COMPLETIONS_LIMIT + 1),
         patch("mito_ai.providers._first_usage_date", today),
+        patch("mito_ai.utils.open_ai_utils.is_pro", return_value=True),
+    ):
+        capabilities = llm.capabilities
+
+        assert "user key" in capabilities.provider
+        assert llm.last_error is None
+
+    # Above the inline limit
+    with (
+        patch("mito_ai.providers._num_usages", OPEN_SOURCE_AI_COMPLETIONS_LIMIT),
+        patch("mito_ai.providers._first_usage_date", REALLY_OLD_DATE),
         patch("mito_ai.utils.open_ai_utils.is_pro", return_value=True),
     ):
         capabilities = llm.capabilities
