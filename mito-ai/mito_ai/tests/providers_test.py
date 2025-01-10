853428db4b309b64e55d8428de497a592e52fba0
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-01-10 12:48:19 -0500

    Added open source user tests

---
+++ mito-ai/mito_ai/tests/providers_test.py
@@ -0,0 +1,67 @@
+import pytest
+from datetime import datetime
+from unittest.mock import patch
+import os
+from unittest.mock import mock_open
+from mito_ai.providers import OpenAIProvider
+
+CHAT_LIMIT = 500
+
+
+def test_os_user_mito_server_below_limit():
+    llm = OpenAIProvider()
+    today = datetime.now().strftime("%Y-%m-%d")
+
+    with (
+        patch.dict(os.environ, {"OPENAI_API_KEY": ""}),
+        patch("mito_ai.providers._num_usages", 1),
+        patch("mito_ai.providers._first_usage_date", today),
+    ):
+        capabilities = llm.capabilities
+
+        assert capabilities.provider == "Mito server"
+        assert llm.last_error is None
+
+
+def test_os_user_mito_server_above_limit():
+    llm = OpenAIProvider()
+    today = datetime.now().strftime("%Y-%m-%d")
+
+    with (
+        patch.dict(os.environ, {"OPENAI_API_KEY": ""}),
+        patch("mito_ai.providers._num_usages", CHAT_LIMIT + 1),
+        patch("mito_ai.providers._first_usage_date", today),
+    ):
+        capabilities = llm.capabilities
+
+        assert capabilities.provider == "Mito server"
+        assert llm.last_error is not None
+        assert llm.last_error.title == "mito_server_free_tier_limit_reached"
+
+
+def test_os_user_openai_key_set_below_limit():
+    llm = OpenAIProvider()
+    today = datetime.now().strftime("%Y-%m-%d")
+
+    with (
+        patch("mito_ai.providers._num_usages", 1),
+        patch("mito_ai.providers._first_usage_date", today),
+    ):
+        capabilities = llm.capabilities
+
+        assert "user key" in capabilities.provider
+        assert llm.last_error is None
+
+
+def test_os_user_openai_key_set_above_limit():
+    llm = OpenAIProvider()
+    today = datetime.now().strftime("%Y-%m-%d")
+
+    with (
+        patch("mito_ai.providers._num_usages", CHAT_LIMIT + 1),
+        patch("mito_ai.providers._first_usage_date", today),
+    ):
+        capabilities = llm.capabilities
+
+        assert "user key" in capabilities.provider
+        assert llm.last_error is None
