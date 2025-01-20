07d60fbd61534d1c1180a7a3558a4cb5081b5f3f
Frédéric Collonval <fcollonval@users.noreply.github.com>
2025-01-20 14:50:22 +0100

    Add placeholder for magic

--- mito-sql-cell/src/index.ts
+++ mito-sql-cell/src/index.ts
@@ -6,6 +6,11 @@ import { IEditorServices } from '@jupyterlab/codeeditor';
 import { NotebookPanel } from '@jupyterlab/notebook';
 import { cellTypeSwitcher } from './celltypeselector';
 import { NotebookContentFactory } from './contentfactory';
+import {
+  EditorExtensionRegistry,
+  IEditorExtensionRegistry
+} from '@jupyterlab/codemirror';
+import { hideSqlMagic } from './hidemagic';
 
 /**
  * Plugin using a custom cell widget for SQL cells.
@@ -15,9 +20,22 @@ const contentFactory: JupyterFrontEndPlugin<NotebookPanel.IContentFactory> = {
   description:
     'Plugin providing the notebook content factory with a special treatment for SQL cells.',
   provides: NotebookPanel.IContentFactory,
-  requires: [IEditorServices],
+  requires: [IEditorServices, IEditorExtensionRegistry],
   autoStart: true,
-  activate: (app: JupyterFrontEnd, editorServices: IEditorServices) => {
+  activate: (
+    app: JupyterFrontEnd,
+    editorServices: IEditorServices,
+    editorExtensionRegistry: IEditorExtensionRegistry
+  ) => {
+    editorExtensionRegistry.addExtension({
+      name: 'mito-sql-cell:hide-sql-magics',
+      // Don't add the extension to the file editor
+      factory: options =>
+        options.inline
+          ? EditorExtensionRegistry.createImmutableExtension(hideSqlMagic)
+          : null
+    });
+
     const editorFactory = editorServices.factoryService.newInlineEditor;
     return new NotebookContentFactory({
       editorFactory,
