ad91d25e261f21d866d7ccea655f949f3401ad48
Aaron Diamond-Reivich <aarondr77@gmail.com>
2025-02-03 15:44:23 -0500

    tests: add test for notification

---
+++ tests/mitoai_ui_tests/notifications.spec.ts
@@ -0,0 +1,76 @@
+import { expect, galata, test } from "@jupyterlab/galata";
+import { PromiseDelegate } from "@lumino/coreutils";
+import { createAndRunNotebookWithCells, waitForIdle } from "../jupyter_utils/jupyterlab_utils";
+
+test.describe.configure({ mode: 'parallel' });
+
+test.describe("Mito AI status item", () => {
+    test.use({
+        autoGoto: false,
+        mockSettings: {
+            ...galata.DEFAULT_SETTINGS,
+            "@jupyterlab/completer-extension:inline-completer": {
+                providers: {
+                    "@jupyterlab/inline-completer:history": {
+                        enabled: false,
+                        timeout: 5000,
+                        debouncerDelay: 0,
+                        maxSuggestions: 100,
+                    },
+                    "mito-ai": {
+                        enabled: true,
+                        timeout: 5000,
+                        debouncerDelay: 250,
+                        triggerKind: "any",
+                    },
+                },
+            },
+        },
+    });
+
+    test("should show upgrade to pro notification", async ({page, tmpPath}) => {
+        const replyDone = new PromiseDelegate<void>();
+
+        await page.routeWebSocket(/.*\/mito-ai\/completions/, (ws) => {
+            
+            ws.onMessage((message) => {
+                const payload = JSON.parse(message as string);
+                const messageId = payload.number;
+                if (
+                    payload.type === "inline_completion" &&
+                    payload.metadata.prefix.includes("print") &&
+                    payload.stream == false
+                ) {
+                    // Send the fetch message back to the client
+                    ws.send(JSON.stringify({
+                        type: "inline_completion",
+                        parent_id: '1',
+                        items: [],
+                        error: {
+                            type: "error",
+                            error_type: "builtins.PermissionError",
+                            title: "mito_server_free_tier_limit_reached",
+                            hint: "Upgrade to Mito Pro to continue using Mito AI",
+                        }
+                    }));
+                    replyDone.resolve();
+                }
+            });
+        });
+
+        await page.goto(`tree/${tmpPath}`);
+        const filename = "notifications.ipynb";
+        await page.notebook.createNew(filename);
+
+        console.log("HERE Filling cell");
+        await (await page.notebook.getCellLocator(0))!
+            .getByRole("textbox")
+            .fill("print('hel");
+
+        console.log("HERE Waiting for reply");
+        await replyDone.promise;
+
+        await expect(page.locator(".jp-toast-message")).toBeVisible();
+        await expect(page.locator('.jp-toast-button').getByRole('button', { name: 'Upgrade to Mito Pro' })).toBeVisible();
+    });
+});
