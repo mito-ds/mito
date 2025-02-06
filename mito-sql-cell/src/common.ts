a901328a92fa787029b29f54cf3b158fa610b862
Frédéric Collonval <fcollonval@users.noreply.github.com>
2025-02-06 17:09:55 +0100

    Add handling of magic in code cell

--- mito-sql-cell/src/common.ts
+++ mito-sql-cell/src/common.ts
@@ -1,9 +1,18 @@
 import type { ICodeCellModel } from '@jupyterlab/cells';
 
 /**
- * The code to inject to the kernel to load the sql magic.
+ * The jupysql configuration needed for Mito SQL.
+ *
+ * @param filename The configuration filename
+ * @returns The configuration code snippet
  */
-export const LOAD_MAGIC = '%load_ext sql';
+export function magicConfiguration(filename: string): string {
+  return `# This cell is required to use Mito SQL cells within the notebook.
+# It must be executed prior to any SQL cell.
+%load_ext sql
+%config SqlMagic.autopandas=True
+%config SqlMagic.dsn_filename=${filename}`;
+}
 
 /**
  * The expected magic.
@@ -24,99 +33,154 @@ export interface IMitoCodeCell {
   isSQL: boolean;
 }
 
+// Normalize magic options from short to long
+// See list at https://github.com/ploomber/jupysql/blob/6be0d4b5171dc4051eb36691067070b132e5ae61/src/sql/magic.py#L257
+const NORMALIZE_OPTIONS: Record<string, string> = {
+  '-l': '--connections',
+  '-x': '--close',
+  '-c': '--creator',
+  '-s': '--section',
+  '-p': '--persist',
+  '-P': '--persist-replace',
+  '-n': '--no-index',
+  '--append': '--append',
+  '-a': '--connection_arguments',
+  '-f': '--file',
+  '-S': '--save',
+  '-w': '--with',
+  '-N': '--no-execute',
+  '-A': '--alias',
+  '--interact': '--interact'
+};
+
 /**
  * The MagicLine namespace.
  */
 export namespace MagicLine {
   /**
-   * Return the database URL from the magic line of a cell.
-   *
-   * @param cellModel - the model of the cell to look for the database URL.
+   * SQL magic interface.
    */
-  export function getDatabaseUrl(
-    cellModel: ICodeCellModel | undefined
-  ): string | undefined {
+  export interface ISQLMagic {
+    /**
+     * Whether the cell is a SQL cell.
+     */
+    isSQL: boolean;
+    /**
+     * The variable name in which to store the SQL query result.
+     */
+    output?: string;
+    /**
+     * Magic arguments.
+     */
+    args: string[];
+    /**
+     * Magic options.
+     */
+    options: Record<string, string | undefined>;
+  }
+
+  /**
+   * Parse the SQL magic from a cell model.
+   *
+   * If the cell model is not a SQL cell, it returns undefined.
+   *
+   * @param cellModel Code cell model to parse
+   * @returns The SQL magic
+   */
+  export function parse(cellModel?: ICodeCellModel): ISQLMagic | undefined {
     if (!cellModel) {
       return;
     }
-    const magicLine = cellModel.sharedModel.source.split('\n')[0];
-    const regexp = new RegExp(`^${MAGIC}\\s+([^\\s]+)`);
-    const match = magicLine.match(regexp);
-    if (match && match.length > 1) {
-      return match[1];
+
+    const isSQL = isSQLCell(cellModel);
+    let output: string | undefined;
+    const options: Record<string, string | undefined> = {};
+    const args = new Array<string>();
+    if (isSQL) {
+      const firstLine = cellModel.sharedModel.source.split('\n', 1)[0];
+      const parts = firstLine.split(/\s+/);
+      const hasOutput = parts[parts.length - 1] == '<<';
+      if (hasOutput) {
+        output = parts[parts.length - 2].replace(/=?$/, '');
+      }
+
+      let option: string | null = null;
+      for (const part of parts.slice(1, hasOutput ? -2 : undefined)) {
+        if (part.startsWith('-')) {
+          option = part.startsWith('--')
+            ? part
+            : (NORMALIZE_OPTIONS[part] ?? part);
+          options[option] = undefined;
+        } else {
+          if (option) {
+            options[option] = part;
+            option = null;
+          } else {
+            args.push(part);
+          }
+        }
+      }
     }
+    return {
+      isSQL,
+      output,
+      args,
+      options
+    };
+  }
+
+  function isSQLCell(cellModel: ICodeCellModel): boolean {
+    const firstLine = cellModel.sharedModel.source.split('\n', 1)[0];
+    const matches = firstLine.match(new RegExp(`^${MAGIC}(\s|$)`));
+    return matches ? true : false;
+  }
+
+  function stringify(magic: ISQLMagic): string {
+    if (!magic.isSQL) {
+      return '';
+    }
+
+    let line = MAGIC;
+
+    for (const [key, value] of Object.entries(magic.options)) {
+      if (value) {
+        line += ` ${key} ${value}`;
+      } else {
+        line += ` ${key}`;
+      }
+    }
+
+    for (const arg of magic.args) {
+      line += ` ${arg}`;
+    }
+
+    if (magic.output) {
+      line += ` ${magic.output} <<`;
+    }
+
+    return line;
   }
 
   /**
-   * Update the contents of the magic line of the cell, accordingly to the selection.
+   * Update a cell with a SQL magic.
    *
-   * @param cellModel - the model of the cell whose contents are to be modified.
-   * @param database - the selected database.
+   * @param cellModel Cell model to update
+   * @param magic Magic to set
    */
-  export function setDatabaseUrl(
+  export function updateMagic(
     cellModel: ICodeCellModel,
-    database?: string
+    magic: ISQLMagic
   ): void {
-    const sourceArray = cellModel.sharedModel.source.split('\n');
-    const magicLine = sourceArray[0].split(/\s+/);
-    if (database) {
-      magicLine[1] = `${database}`;
-    }
-    sourceArray[0] = magicLine.join(' ');
-    cellModel.sharedModel.source = sourceArray.join('\n');
-  }
-
-  /**
-   * Return the variable from the magic line of a cell.
-   *
-   * @param cellModel - the model of the cell to look for the database URL.
-   */
-  export function getVariable(cellModel: ICodeCellModel): IVariable {
-    const magicLine = cellModel.sharedModel.source.split('\n')[0];
-    const regexp = new RegExp(`^${MAGIC}.*\\s(\\w+)(=?)\\s*<<$`);
-    const match = magicLine.match(regexp);
-    if (match && match.length > 1) {
-      return { value: match[1], displayOutput: match[2] === '=' };
-    }
-    return { value: '', displayOutput: false };
-  }
-
-  /**
-   * Update the content of the magic line to save the result in a variable.
-   *
-   * @param cellModel - the model of the cell whose contents are to be modified.
-   * @param value - the name of the variable.
-   */
-  export function setVariable(
-    cellModel: ICodeCellModel,
-    variable: IVariable
-  ): void {
-    const sourceArray = cellModel.sharedModel.source.split('\n');
-    let magicLine = sourceArray[0];
-    const regexp = new RegExp(`^${MAGIC}.*(\\s\\w+=?\\s*<<)$`);
-    const match = magicLine.match(regexp);
-
-    const variableText = variable.value
-      ? ` ${variable.value}${variable.displayOutput ? '=' : ''} <<`
-      : '';
-
-    if (match) {
-      magicLine = magicLine.replace(match[1], variableText);
+    const isSQL = isSQLCell(cellModel);
+    const source = cellModel.sharedModel.source;
+    const lines = source.split('\n', 1);
+    const magicLine = stringify(magic);
+    if (isSQL) {
+      lines[0] = magicLine;
     } else {
-      magicLine += `${variableText}`;
+      lines.unshift(magicLine);
     }
-    sourceArray[0] = magicLine;
-    cellModel.sharedModel.source = sourceArray.join('\n');
-  }
 
-  export interface IVariable {
-    /**
-     * The variable name.
-     */
-    value: string;
-    /**
-     * Whether the output should be displayed or not.
-     */
-    displayOutput: boolean;
+    cellModel.sharedModel.source = lines.join('\n');
   }
 }
