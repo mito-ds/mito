efa6ad65389df4c23ca7a451b13ce572eb02f32e
Frédéric Collonval <fcollonval@users.noreply.github.com>
2025-02-10 10:54:16 +0100

    Add unit tests for handling SQL magic

--- mito-sql-cell/src/__tests__/mito_sql_cell.spec.ts
+++ mito-sql-cell/src/__tests__/mito_sql_cell.spec.ts
@@ -1,9 +1,120 @@
-/**
- * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
- */
+import { MagicLine } from '../common';
 
 describe('mito-sql-cell', () => {
-  it('should be tested', () => {
-    expect(1 + 1).toEqual(2);
+  describe('MagicLine', () => {
+    it.each([
+      ['%%sql', true],
+      ['%%sql --section a --close df <<', true],
+      ['', false],
+      ['%sql', false],
+      ['def f():\n    return 22\n%%sql', false]
+    ])('%s isSQLCell %s', (text, expected) => {
+      expect(
+        MagicLine.isSQLCell({ sharedModel: { source: text } } as any)
+      ).toEqual(expected);
+    });
+
+    it.each([
+      ['%%sql', { isSQL: true, output: undefined, args: [], options: {} }],
+      [
+        '%%sql --section a -x df <<',
+        {
+          isSQL: true,
+          output: 'df',
+          args: [],
+          options: { '--section': 'a', '--close': undefined }
+        }
+      ],
+      [
+        '%%sql -s a df <<',
+        { isSQL: true, output: 'df', args: [], options: { '--section': 'a' } }
+      ],
+      [
+        '%%sql -s a df= <<',
+        { isSQL: true, output: 'df', args: [], options: { '--section': 'a' } }
+      ],
+      [
+        '%%sql -s a df',
+        {
+          isSQL: true,
+          output: undefined,
+          args: ['df'],
+          options: { '--section': 'a' }
+        }
+      ],
+      ['%sql', { isSQL: false, output: undefined, args: [], options: {} }],
+      ['', { isSQL: false, output: undefined, args: [], options: {} }]
+    ])('%s parse to %j', (text, expected) => {
+      expect(MagicLine.parse({ sharedModel: { source: text } } as any)).toEqual(
+        expected
+      );
+    });
+
+    it.each([
+      [
+        '',
+        {
+          isSQL: true,
+          output: 'db',
+          args: ['line'],
+          options: { '--section': 'sqlite1' }
+        },
+        '%%sql --section sqlite1 line db <<\n'
+      ],
+      [
+        '%%sql',
+        {
+          isSQL: true,
+          output: 'db',
+          args: ['line'],
+          options: { '--section': 'sqlite1', '--close': undefined }
+        },
+        '%%sql --section sqlite1 --close line db <<'
+      ],
+      [
+        '%%sql --section a df <<',
+        {
+          isSQL: true,
+          output: 'db',
+          args: ['line'],
+          options: { '--section': 'sqlite1' }
+        },
+        '%%sql --section sqlite1 line db <<'
+      ],
+      [
+        '%%sql --section a df <<',
+        {
+          isSQL: true,
+          output: 'db',
+          args: ['line'],
+          options: { '--file': 'f1' }
+        },
+        '%%sql --file f1 line db <<'
+      ],
+      [
+        '%%sql --section a df <<',
+        {
+          isSQL: true,
+          output: undefined,
+          args: [],
+          options: { '--file': 'f1' }
+        },
+        '%%sql --file f1'
+      ],
+      [
+        'def f(a, b):\n  return a + b',
+        {
+          isSQL: true,
+          output: 'db',
+          args: ['line'],
+          options: { '--section': 'sqlite1' }
+        },
+        '%%sql --section sqlite1 line db <<\ndef f(a, b):\n  return a + b'
+      ]
+    ])('%s parse to %j', (text, magic, expected) => {
+      const fakeCell = { sharedModel: { source: text } } as any;
+      MagicLine.update(fakeCell, magic);
+      expect(fakeCell.sharedModel.source).toEqual(expected);
+    });
   });
 });
