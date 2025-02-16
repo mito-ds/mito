9fb2717ed881b33d3669924fb2d84b80917974e3
Aaron Diamond-Reivich <aarondr77@gmail.com>
2025-02-16 17:24:39 -0500

    mito-ai: create metadatas

--- mito-ai/src/utils/websocket/models.ts
+++ mito-ai/src/utils/websocket/models.ts
@@ -3,7 +3,7 @@ import type {
   IInlineCompletionItem
 } from '@jupyterlab/completer';
 
-import { IChatMessageMetadata } from '../../Extensions/AiChat/ChatHistoryManager';
+import { IChatMessageMetadata, IChatSmartDebugMetadata } from '../../Extensions/AiChat/ChatHistoryManager';
 
 
 /**
@@ -27,6 +27,26 @@ export interface ICompletionRequest {
    */
   stream: boolean;
 }
+
+
+export interface IChatCompletionRequest extends ICompletionRequest {
+  type: 'chat',
+  metadata: IChatMessageMetadata
+}
+
+export interface ISmartDebugCompletionRequest extends ICompletionRequest {
+  type: 'smartDebug'
+  metadata: IChatSmartDebugMetadata
+}
+
+
+
+
+
+
+
+
+
 
 /**
  * AI capabilities.
