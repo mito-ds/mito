7c9a6fd73e403eb52138992d9abb399c7045cf80
Konstantin Taletskiy <konstantin@taletskiy.com>
2025-03-18 16:54:49 -0700

    chore: remove whitespace

--- mito-ai/mito_ai/message_history.py
+++ mito-ai/mito_ai/message_history.py
@@ -283,9 +283,11 @@ class GlobalMessageHistory:
         If there are no threads yet, create one.
         We also detect if we should set a short name for the thread.
         """
+
         if thread_id is None:
             with self._lock:
                 thread_id = self._get_newest_thread_id()
+
         if not thread_id:
             thread_id = self.create_new_thread()
 
