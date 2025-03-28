ce2b1d1cb3ea97be19eb0f2ae01248dc4020668b
Frédéric Collonval <fcollonval@users.noreply.github.com>
2025-03-28 15:39:33 +0100

    Add pytest for snowflake

--- mito-sql-cell/mito_sql_cell/tests/test_magic.py
+++ mito-sql-cell/mito_sql_cell/tests/test_magic.py
@@ -1,7 +1,19 @@
+import os
 import pandas
 import pytest
 
 from mito_sql_cell.magic import DEFAULT_VARIABLE_NAME
+
+
+def get_snowflake_credentials() -> dict[str, str] | None:
+    username = os.environ.get("SNOWFLAKE_USERNAME", "None")
+    password = os.environ.get("SNOWFLAKE_PASSWORD", "None")
+    account = os.environ.get("SNOWFLAKE_ACCOUNT", "None")
+
+    if username == "None" or password == "None" or account == "None":
+        return None
+
+    return {"username": username, "password": password, "account": account}
 
 
 def test_basic_magic(ipython_shell):
@@ -35,8 +47,31 @@ driver = sqlite
 """)
 
     r = ipython_shell.run_cell_magic(
-        "sql", f"{option} \"{CONFIG_FILE.absolute()!s}\" fake", "SELECT * FROM profiles"
+        "sql", f'{option} "{CONFIG_FILE.absolute()!s}" fake', "SELECT * FROM profiles"
     )
 
     assert isinstance(r, pandas.DataFrame)
     assert len(r) == 20
+
+
+def test_snowflake_connection(tmp_path, ipython_shell):
+    snowflake = get_snowflake_credentials()
+    if snowflake is None:
+        pytest.skip("No Snowflake credentials found")
+
+    CONFIG_FILE = tmp_path / "snowflake.ini"
+    CONFIG_FILE.write_text(f"""[snowflake]
+driver = snowflake
+user = {snowflake["username"]}
+password = {snowflake["password"]}
+account = {snowflake["account"]}
+""")
+
+    r = ipython_shell.run_cell_magic(
+        "sql",
+        f'-c "{CONFIG_FILE.absolute()!s}" snowflake',
+        "SELECT * FROM PYTESTDATABASE.PYTESTSCHEMA.SIMPLE_PYTEST_TABLE",
+    )
+
+    assert isinstance(r, pandas.DataFrame)
+    assert len(r) == 3
