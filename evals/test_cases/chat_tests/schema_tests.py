c593fb61bb85890ef20549eca39be769744014c9
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-04-24 11:51:12 -0400

    Added schema tests

--- evals/test_cases/chat_tests/schema_tests.py
+++ evals/test_cases/chat_tests/schema_tests.py
@@ -6,13 +6,21 @@ from evals.notebook_states import *
 
 SCHEMA_TESTS = [
     ChatTestCase(
-        name="schema_test_case",
+        name="simple_query",
         test_case_core=CodeGenTestCaseCore(
             notebook_state=EMPTY_NOTEBOOK_WITH_SCHEMA,
             expected_code="x=1",
             workflow_tags=["variable_declaration"],
         ),
         user_input="can you get me a list of all the cars that are less than 20k",
-        # user_input="can you get me a list of all the cars that have less than 10k miles",
     ),
+    ChatTestCase(
+        name="simple_query_for_non_existent_field",
+        test_case_core=CodeGenTestCaseCore(
+            notebook_state=EMPTY_NOTEBOOK_WITH_SCHEMA,
+            expected_code="x=1",
+            workflow_tags=["variable_declaration"],
+        ),
+        user_input="can you get me a list of all the cars that have less than 10k miles",
+    ),    
 ]
