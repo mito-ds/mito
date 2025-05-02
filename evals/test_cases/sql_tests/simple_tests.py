ddbbe4010606f1a0a6ed9a482c2168926d075ab4
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-05-02 15:31:59 -0400

    Moved schema names to const

--- evals/test_cases/sql_tests/simple_tests.py
+++ evals/test_cases/sql_tests/simple_tests.py
@@ -1,11 +1,12 @@
 from evals.notebook_states import *
 from evals.eval_types import SQLTestCase
+from evals.test_cases.sql_tests.constants import *
 
 SIMPLE_TESTS = [
     SQLTestCase(
         name="non_existent_table",
         user_input="which car brand sold the most cars in 2020?",
-        schema="small.json",
+        schema=SMALL_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
         expected_output=None,
         expected_tables=[],
@@ -14,7 +15,7 @@ SIMPLE_TESTS = [
     SQLTestCase(
         name="simple_test_1",
         user_input="get the company with the largest market cap",
-        schema="small.json",
+        schema=SMALL_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
         expected_output="SELECT * FROM SP_500.PUBLIC.SP500_COMPANIES ORDER BY MARKETCAP DESC LIMIT 1",
         expected_tables=["SP_500.PUBLIC.SP500_COMPANIES"],
