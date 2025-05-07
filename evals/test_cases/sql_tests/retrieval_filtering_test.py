48029d3deb8ff8e4657527a18f05ad7d7c5ad8cf
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-05-07 15:25:02 -0400

    Removed tests

--- evals/test_cases/sql_tests/retrieval_filtering_test.py
+++ evals/test_cases/sql_tests/retrieval_filtering_test.py
@@ -8,13 +8,6 @@ from evals.test_cases.sql_tests.constants import *
 BASIC_RETRIEVAL_AND_FILTERING_TESTS = [
     # SMALL SCHEMA
     SQLTestCase(
-        name="list_all_companies",
-        user_input="List all S&P 500 companies.",
-        schema=SMALL_SCHEMA,
-        notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT * FROM SP_500.PUBLIC.SP500_COMPANIES",
-    ),
-    SQLTestCase(
         name="tech_sector_companies",
         user_input="Which companies belong to the Technology sector?",
         schema=SMALL_SCHEMA,
@@ -35,21 +28,7 @@ BASIC_RETRIEVAL_AND_FILTERING_TESTS = [
         notebook_state=EMPTY_NOTEBOOK,
         expected_output="SELECT * FROM SP_500.PUBLIC.SP500_COMPANIES ORDER BY CURRENTPRICE DESC LIMIT 5",
     ),
-    SQLTestCase(
-        name="companies_named_inc",
-        user_input="Find all companies with 'Inc' in the name.",
-        schema=SMALL_SCHEMA,
-        notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT * FROM SP_500.PUBLIC.SP500_COMPANIES WHERE LONGNAME LIKE '%Inc%'",
-    ),
     # MEDIUM SCHEMA
-    SQLTestCase(
-        name="churned_customers_with_high_cltv",
-        user_input="Find all churned customers with a high CLTV (over 6k).",
-        schema=MEDIUM_SCHEMA,
-        notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT * FROM TELCO_CHRUN.PUBLIC.STATUS_ANALYSIS WHERE CHURN_LABEL = TRUE AND CLTV > 6000",
-    ),
     SQLTestCase(
         name="just_joined_low_satisfaction",
         user_input="Find all customers who just joined and have the lowest possible satisfaction score.",
@@ -71,13 +50,6 @@ BASIC_RETRIEVAL_AND_FILTERING_TESTS = [
         notebook_state=EMPTY_NOTEBOOK,
         expected_output="SELECT * FROM TELCO_CHRUN.PUBLIC.SERVICE_OPTIONS WHERE NUMBER_OF_REFERRALS > 0 AND MULTIPLE_LINES = FALSE",
     ),
-    SQLTestCase(
-        name="most_referrals",
-        user_input="Find the customer with the most referrals.",
-        schema=MEDIUM_SCHEMA,
-        notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT * FROM TELCO_CHRUN.PUBLIC.SERVICE_OPTIONS ORDER BY NUMBER_OF_REFERRALS DESC LIMIT 1",
-    ),
     # LARGE SCHEMA (mix and match from the previous tests, good to see if there is performance degradation)
     SQLTestCase(
         name="tech_sector_companies",
@@ -87,31 +59,17 @@ BASIC_RETRIEVAL_AND_FILTERING_TESTS = [
         expected_output="SELECT * FROM SP_500.PUBLIC.SP500_COMPANIES WHERE SECTOR = 'Technology'",
     ),
     SQLTestCase(
-        name="high_market_cap",
-        user_input="Show me companies with a market cap over 1 trillion.",
-        schema=LARGE_SCHEMA,
-        notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT * FROM SP_500.PUBLIC.SP500_COMPANIES WHERE MARKETCAP > 1000000000000",
-    ),
-    SQLTestCase(
-        name="just_joined_low_satisfaction",
-        user_input="Find all customers who just joined and have the lowest possible satisfaction score.",
-        schema=LARGE_SCHEMA,
-        notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT * FROM TELCO_CHRUN.PUBLIC.STATUS_ANALYSIS WHERE CUSTOMER_STATUS = 'Joined' AND SATISFACTION_SCORE = 1",
-    ),
-    SQLTestCase(
         name="churned_and_gave_a_reason",
         user_input="Find all customers who churned and gave a reason.",
-        schema=LARGE_SCHEMA,
+        schema=MEDIUM_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
         expected_output="SELECT * FROM TELCO_CHRUN.PUBLIC.STATUS_ANALYSIS WHERE CHURN_LABEL = TRUE AND CHURN_REASON IS NOT NULL",
     ),
     SQLTestCase(
-        name="most_referrals",
-        user_input="Find the customer with the most referrals.",
-        schema=LARGE_SCHEMA,
+        name="made_referral_but_does_not_have_multiple_lines",
+        user_input="Find all customers who made a referral but do not have multiple lines.",
+        schema=MEDIUM_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT * FROM TELCO_CHRUN.PUBLIC.SERVICE_OPTIONS ORDER BY NUMBER_OF_REFERRALS DESC LIMIT 1",
+        expected_output="SELECT * FROM TELCO_CHRUN.PUBLIC.SERVICE_OPTIONS WHERE NUMBER_OF_REFERRALS > 0 AND MULTIPLE_LINES = FALSE",
     ),
 ]
