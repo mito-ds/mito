a8c3cebbdb0c1e2dea3978ffd91a0bbc2120ec72
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-05-08 11:38:33 -0400

    more formatting

--- evals/test_cases/sql_tests/grouping_tests.py
+++ evals/test_cases/sql_tests/grouping_tests.py
@@ -12,21 +12,32 @@ AGGREGATION_AND_GROUPING_TESTS = [
         user_input="What is the average market cap of all companies in the S&P 500?",
         schema=SMALL_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT AVG(MARKETCAP) FROM SP_500.PUBLIC.SP500_COMPANIES",
+        expected_output="""
+            SELECT AVG(MARKETCAP) 
+            FROM SP_500.PUBLIC.SP500_COMPANIES
+        """.strip(),
     ),
     SQLTestCase(
         name="average_market_cap_by_sector",
         user_input="What is the average market cap of companies by sector?",
         schema=SMALL_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT SECTOR, AVG(MARKETCAP) FROM SP_500.PUBLIC.SP500_COMPANIES GROUP BY SECTOR",
+        expected_output="""
+            SELECT SECTOR, AVG(MARKETCAP) 
+            FROM SP_500.PUBLIC.SP500_COMPANIES 
+            GROUP BY SECTOR
+        """.strip(),
     ),
     SQLTestCase(
         name="tech_companies_w_positive_revenue_growth",
         user_input="How many tech companies have positive revenue growth?",
         schema=SMALL_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT COUNT(*) FROM SP_500.PUBLIC.SP500_COMPANIES WHERE SECTOR = 'Technology' AND REVENUEGROWTH > 0",
+        expected_output="""
+            SELECT COUNT(*) 
+            FROM SP_500.PUBLIC.SP500_COMPANIES 
+            WHERE SECTOR = 'Technology' AND REVENUEGROWTH > 0
+        """.strip(),
     ),
     # MEDIUM SCHEMA
     SQLTestCase(
@@ -34,21 +45,33 @@ AGGREGATION_AND_GROUPING_TESTS = [
         user_input="What is the average LTV for churned customers?",
         schema=MEDIUM_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT AVG(CLTV) FROM TELCO_CHRUN.PUBLIC.STATUS_ANALYSIS WHERE CHURN_LABEL = TRUE",
+        expected_output="""
+            SELECT AVG(CLTV) 
+            FROM TELCO_CHRUN.PUBLIC.STATUS_ANALYSIS 
+            WHERE CHURN_LABEL = TRUE
+        """.strip(),
     ),
     SQLTestCase(
         name="avg_referrals_for_all_services",
         user_input="What is the avg number of referrals for customer subscribed to all services?",
         schema=MEDIUM_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT AVG(NUMBER_OF_REFERRALS) FROM TELCO_CHRUN.PUBLIC.SERVICE_OPTIONS WHERE INTERNET_SERVICE = TRUE AND PHONE_SERVICE = TRUE",
+        expected_output="""
+            SELECT AVG(NUMBER_OF_REFERRALS) 
+            FROM TELCO_CHRUN.PUBLIC.SERVICE_OPTIONS 
+            WHERE INTERNET_SERVICE = TRUE AND PHONE_SERVICE = TRUE
+        """.strip(),
     ),
     SQLTestCase(
         name="avg_referrals_for_all_services",
         user_input="For each satisfaction score, what is the avg lifetime value?",
         schema=MEDIUM_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT SATISFACTION_SCORE, AVG(CLTV) FROM TELCO_CHRUN.PUBLIC.STATUS_ANALYSIS GROUP BY SATISFACTION_SCORE",
+        expected_output="""
+            SELECT SATISFACTION_SCORE, AVG(CLTV) 
+            FROM TELCO_CHRUN.PUBLIC.STATUS_ANALYSIS 
+            GROUP BY SATISFACTION_SCORE
+        """.strip(),
     ),
     # LARGE SCHEMA
     SQLTestCase(
@@ -56,20 +79,32 @@ AGGREGATION_AND_GROUPING_TESTS = [
         user_input="What is the average market cap of companies by sector?",
         schema=LARGE_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT SECTOR, AVG(MARKETCAP) FROM SP_500.PUBLIC.SP500_COMPANIES GROUP BY SECTOR",
+        expected_output="""
+            SELECT SECTOR, AVG(MARKETCAP) 
+            FROM SP_500.PUBLIC.SP500_COMPANIES 
+            GROUP BY SECTOR
+        """.strip(),
     ),
     SQLTestCase(
         name="avg_ltv_for_churned_customers",
         user_input="What is the average LTV for churned customers?",
         schema=LARGE_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT AVG(CLTV) FROM TELCO_CHRUN.PUBLIC.STATUS_ANALYSIS WHERE CHURN_LABEL = TRUE",
+        expected_output="""
+            SELECT AVG(CLTV) 
+            FROM TELCO_CHRUN.PUBLIC.STATUS_ANALYSIS 
+            WHERE CHURN_LABEL = TRUE
+        """.strip(),
     ),
     SQLTestCase(
         name="avg_referrals_for_all_services",
         user_input="What is the avg number of referrals for customer subscribed to all services?",
         schema=LARGE_SCHEMA,
         notebook_state=EMPTY_NOTEBOOK,
-        expected_output="SELECT AVG(NUMBER_OF_REFERRALS) FROM TELCO_CHRUN.PUBLIC.SERVICE_OPTIONS WHERE INTERNET_SERVICE = TRUE AND PHONE_SERVICE = TRUE",
+        expected_output="""
+            SELECT AVG(NUMBER_OF_REFERRALS) 
+            FROM TELCO_CHRUN.PUBLIC.SERVICE_OPTIONS 
+            WHERE INTERNET_SERVICE = TRUE AND PHONE_SERVICE = TRUE
+        """.strip(),
     ),
 ]
