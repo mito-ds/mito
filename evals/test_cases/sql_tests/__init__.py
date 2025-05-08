0c700066b38c155bd5e3fbb355b4898d7a1cc282
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-05-08 11:08:11 -0400

    Added time test

--- evals/test_cases/sql_tests/__init__.py
+++ evals/test_cases/sql_tests/__init__.py
@@ -11,10 +11,12 @@ from evals.test_cases.sql_tests.retrieval_filtering_test import (
 )
 from evals.test_cases.sql_tests.grouping_tests import AGGREGATION_AND_GROUPING_TESTS
 from evals.test_cases.sql_tests.join_tests import JOIN_TESTS
+from evals.test_cases.sql_tests.time_tests import TIME_BASED_TESTS
 
 SQL_TESTS: List[SQLTestCase] = [
     *SAMPLE_TESTS,
     *BASIC_RETRIEVAL_AND_FILTERING_TESTS,
     *AGGREGATION_AND_GROUPING_TESTS,
     *JOIN_TESTS,
+    *TIME_BASED_TESTS,
 ]
---
+++ evals/test_cases/sql_tests/time_tests.py
@@ -0,0 +1,15 @@
+# Copyright (c) Saga Inc.
+# Distributed under the terms of the GNU Affero General Public License v3.0 License.
+
+from evals.notebook_states import *
+from evals.eval_types import SQLTestCase
+from evals.test_cases.sql_tests.constants import *
+
+TIME_BASED_TESTS = [
+    # SMALL SCHEMA
+
+    # MEDIUM SCHEMA
+
+    # LARGE SCHEMA
+    
+]
