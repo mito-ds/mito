196214e4b8d28d8da629df6da3605e9fae773825
Aaron Diamond-Reivich <aarondr77@gmail.com>
2025-05-14 18:02:34 -0400

    mito-ai: bring back package-lock.json

--- mito-ai/mito_ai/__init__.py
+++ mito-ai/mito_ai/__init__.py
@@ -9,8 +9,7 @@ from mito_ai.providers import OpenAIProvider
 try:
     from _version import __version__
 except ImportError:
-    # Fallback when using the package in dev mode without installing
-    # in editable mode with pip. It is highly recommended to install
+    # Fallback when using the package in dev mode without installing in editable mode with pip. It is highly recommended to install
     # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
     import warnings
     import os
