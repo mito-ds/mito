74af490f2b9e05f91f14e62dad21607f627a2487
Aaron Diamond-Reivich <aarondr77@gmail.com>
2025-06-05 16:15:56 -0400

    mito-ai: allow azure on mito private

--- mito-ai/mito_ai/enterprise/utils.py
+++ mito-ai/mito_ai/enterprise/utils.py
@@ -4,12 +4,12 @@
 # Copyright (c) Saga Inc.
 # Distributed under the terms of the The Mito Enterprise license.
 
-from mito_ai.utils.version_utils import is_enterprise
+from mito_ai.utils.version_utils import is_enterprise, is_mitosheet_private
 from mito_ai.constants import AZURE_OPENAI_API_KEY, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_API_VERSION, AZURE_OPENAI_MODEL
 
 def is_azure_openai_configured() -> bool:
     """
     Azure OpenAI is only supported for Mito Enterprise users
     """
-    is_enterprise_user = is_enterprise()
-    return all([is_enterprise_user, AZURE_OPENAI_API_KEY, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_API_VERSION, AZURE_OPENAI_MODEL])
+    is_allowed_to_use_azure = is_enterprise() or is_mitosheet_private()
+    return all([is_allowed_to_use_azure, AZURE_OPENAI_API_KEY, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_API_VERSION, AZURE_OPENAI_MODEL])
