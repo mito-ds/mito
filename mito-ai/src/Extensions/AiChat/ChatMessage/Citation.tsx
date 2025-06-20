0487bf97367e480a1a5bd4700d4ce44d25389c55
Aaron Diamond-Reivich <aarondr77@gmail.com>
2025-06-20 11:23:44 -0400

    mito-ai: update scrollToCell function

--- mito-ai/src/Extensions/AiChat/ChatMessage/Citation.tsx
+++ mito-ai/src/Extensions/AiChat/ChatMessage/Citation.tsx
@@ -4,10 +4,9 @@
  */
 
 import React from 'react';
-import { getCellCodeByID, getCellByID, setActiveCellByID } from '../../../utils/notebook';
+import { scrollToCell } from '../../../utils/notebook';
 import { INotebookTracker } from '@jupyterlab/notebook';
 import '../../../../style/Citation.css';
-import { WindowedList } from '@jupyterlab/ui-components';
 
 // Citation component props interface
 export interface CitationProps {
@@ -21,27 +20,7 @@ export interface CitationProps {
 // Citation button component
 export const Citation: React.FC<CitationProps> = ({ citationIndex, cellId, line, notebookTracker }): JSX.Element => {
   const handleClick = (): void => {
-    // To determine how we should handle scrolling, 
-    // we need to first count the number of lines in the cell.
-    // If the line is closer to the top, 
-    // we set the scroll position to "start," otherwise we set it to "end."
-    const code = getCellCodeByID(notebookTracker, cellId);
-    const relativeLinePosition = line / (code?.split('\n').length || 1);
-
-    // These positions must be of type BaseScrollToAlignment defined in @jupyterlab/ui-components
-    const position: WindowedList.BaseScrollToAlignment = relativeLinePosition < 0.5 ? 'start' : 'end';
-
-    const cell = getCellByID(notebookTracker, cellId);
-
-    if (cell == null) {
-      console.log('cell is null');
-      return
-    }
-
-    console.log('position', position);
-    setActiveCellByID(notebookTracker, cellId);
-    notebookTracker.currentWidget?.content.scrollToCell(cell, position);
-    console.log('scrolled to cell', cell);
+    scrollToCell(notebookTracker, cellId, line);
   };
 
   return (
