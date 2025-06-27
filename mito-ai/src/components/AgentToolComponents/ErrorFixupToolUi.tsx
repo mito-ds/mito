139fdaa7accb0db02457ae040546333806489c6b
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-06-27 17:01:26 -0400

    Added expand icon

--- mito-ai/src/components/AgentToolComponents/ErrorFixupToolUi.tsx
+++ mito-ai/src/components/AgentToolComponents/ErrorFixupToolUi.tsx
@@ -4,6 +4,7 @@ import { getContentStringFromMessage } from '../../utils/strings';
 import OpenAI from 'openai';
 import ErrorIcon from '../../icons/ErrorIcon';
 import '../../../style/ErrorFixupToolUI.css';
+import ExpandIcon from '../../icons/ExpandIcon';
 
 interface IErrorFixupToolUiProps {
     message: OpenAI.Chat.ChatCompletionMessageParam;
@@ -13,26 +14,22 @@ const ErrorFixupToolUi: React.FC<IErrorFixupToolUiProps> = ({ message }) => {
     const [isExpanded, setIsExpanded] = useState(false);
     const messageContent = getContentStringFromMessage(message);
 
-    if (isExpanded) {
-        return (
-            <div 
-                className={classNames("error-fixup-container", "error-fixup-expanded")}
-                onClick={() => setIsExpanded(false)}
-            >
-                {messageContent}
-            </div>
-        );
-    }
-
     return (
-        <div 
+        <div
             className="error-fixup-container"
-            onClick={() => setIsExpanded(true)}
+            onClick={() => setIsExpanded(!isExpanded)}
         >
             <div className="error-fixup-content">
                 <ErrorIcon />
                 <span>Error detected - click to view details</span>
+                <ExpandIcon isExpanded={isExpanded} />
             </div>
+
+            {isExpanded && (
+                <div className={classNames("error-fixup-container", "error-fixup-expanded")}>
+                    {messageContent}
+                </div>
+            )}
         </div>
     );
 };
