31852b402b2451d35a8417e64f9ce0edde6d4d72
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-07-02 14:48:46 -0400

    Refactor ErrorFixupToolUI to support expandable message sections and improve UI hierarchy

--- mito-ai/src/components/AgentToolComponents/ErrorFixupToolUI.tsx
+++ mito-ai/src/components/AgentToolComponents/ErrorFixupToolUI.tsx
@@ -2,11 +2,9 @@ import React, { useState } from 'react';
 import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
 import { classNames } from '../../utils/classNames';
 import { getContentStringFromMessage, PYTHON_CODE_BLOCK_START_WITHOUT_NEW_LINE, splitStringWithCodeBlocks } from '../../utils/strings';
-// import OpenAI f`rom 'openai';
 import '../../../style/ErrorFixupToolUI.css';
 import ExpandIcon from '../../icons/ExpandIcon';
 import WrenchAndScrewdriverIcon from '../../icons/WrenchAndScrewdriverIcon';
-// import PythonCode from '../../Extensions/AiChat/ChatMessage/PythonCode';
 import { GroupedErrorMessages } from '../../utils/chatHistory';
 import PythonCode from '../../Extensions/AiChat/ChatMessage/PythonCode';
 
@@ -22,45 +20,71 @@ const parsePythonErrorType = (content: string | undefined): string => {
 };
 
 const ErrorFixupToolUI: React.FC<IErrorFixupToolUIProps> = ({ messages, renderMimeRegistry }) => {
-    const [isExpanded, setIsExpanded] = useState(false);
-    const messageContent = getContentStringFromMessage(messages[0]!.message);
-    if (!messageContent) return null;
+    const [expandedMessages, setExpandedMessages] = useState<Set<number>>(new Set());
+
+    const toggleMessage = (index: number) => {
+        const newExpanded = new Set(expandedMessages);
+        if (newExpanded.has(index)) {
+            newExpanded.delete(index);
+        } else {
+            newExpanded.add(index);
+        }
+        setExpandedMessages(newExpanded);
+    };
+
+    if (!messages || messages.length === 0) return null;
 
     return (
-        <div className={classNames('error-fixup-container', {
-            'error-fixup-collapsed': !isExpanded
-        })}>
-            <div
-                onClick={() => setIsExpanded(!isExpanded)}
-                className={classNames('error-fixup-toggle', {
-                    expanded: isExpanded
-                })}
-            >
-                <span className="error-fixup-toggle-content">
-                    <WrenchAndScrewdriverIcon />
-                    Fixing {parsePythonErrorType(messageContent)}
-                </span>
-                <ExpandIcon isExpanded={isExpanded} />
-            </div>
-            {isExpanded && (
-                 <div className="error-fixup-expanded">
-                    {messages.map((message, index) => {
-                        const messageContentParts = splitStringWithCodeBlocks(message.message);
-                        // const messageContent = getContentStringFromMessage(message.message);
+        <div className="error-fixup-messages-container">
+            {messages.map((message, messageIndex) => {
+                const messageContent = getContentStringFromMessage(message.message);
+                if (!messageContent) return null;
 
-                        return messageContentParts.map((messagePart, index) => {
-                            if (messagePart.startsWith(PYTHON_CODE_BLOCK_START_WITHOUT_NEW_LINE)) {
-                                return (
-                                    <PythonCode code={messagePart} renderMimeRegistry={renderMimeRegistry} />
-                                )
-                            }
-                            return (
-                                <div key={index}>{messagePart}</div>
-                            )
-                        })
-                    })}
-                </div>
-            )}
+                const isExpanded = expandedMessages.has(messageIndex);
+
+                return (
+                    <div 
+                        key={messageIndex}
+                        className={classNames('error-fixup-container', {
+                            'error-fixup-collapsed': !isExpanded
+                        })}
+                    >
+                        <div
+                            onClick={() => toggleMessage(messageIndex)}
+                            className={classNames('error-fixup-toggle', {
+                                expanded: isExpanded
+                            })}
+                        >
+                            <span className="error-fixup-toggle-content">
+                                <WrenchAndScrewdriverIcon />
+                                Fixing {parsePythonErrorType(messageContent)}
+                            </span>
+                            <ExpandIcon isExpanded={isExpanded} />
+                        </div>
+                        {isExpanded && (
+                            <div className="error-fixup-expanded">
+                                {(() => {
+                                    const messageContentParts = splitStringWithCodeBlocks(message.message);
+                                    return messageContentParts.map((messagePart, partIndex) => {
+                                        if (messagePart.startsWith(PYTHON_CODE_BLOCK_START_WITHOUT_NEW_LINE)) {
+                                            return (
+                                                <PythonCode 
+                                                    key={`${messageIndex}-${partIndex}`}
+                                                    code={messagePart} 
+                                                    renderMimeRegistry={renderMimeRegistry} 
+                                                />
+                                            );
+                                        }
+                                        return (
+                                            <div key={`${messageIndex}-${partIndex}`}>{messagePart}</div>
+                                        );
+                                    });
+                                })()}
+                            </div>
+                        )}
+                    </div>
+                );
+            })}
         </div>
     );
 };
