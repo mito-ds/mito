2af58b81b8704eb749e6d89af835b9bb8d9b848f
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-07-11 11:24:39 -0400

    Moved smart debug tests to seperate file

---
+++ tests/mitoai_ui_tests/smartDebug.spec.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) Saga Inc.
+ * Distributed under the terms of the GNU Affero General Public License v3.0 License.
+ */
+
+import { expect, test } from '@jupyterlab/galata';
+import {
+    createAndRunNotebookWithCells,
+    getCodeFromCell,
+    selectCell,
+    waitForIdle,
+    addNewCell,
+    updateCell
+} from '../jupyter_utils/jupyterlab_utils';
+import {
+    clearMitoAIChatInput,
+    clickAcceptButton,
+    clickDenyButton,
+    clickPreviewButton,
+    clickOnMitoAIChatTab,
+    editMitoAIMessage,
+    sendMessagetoAIChat,
+    waitForMitoAILoadingToDisappear,
+    startNewMitoAIChat,
+} from './utils';
+
+const MODEL = 'GPT 4.1';
+
+test.describe.parallel('Smart Debug (in Chat mode)', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await createAndRunNotebookWithCells(page, ['print(1']);
+        await waitForIdle(page);
+
+        await startNewMitoAIChat(page, MODEL);
+    });
+
+    test('Smart Debug button should update code', async ({ page }) => {
+        await page.getByRole('button', { name: 'Fix Error in AI Chat' }).click();
+        await waitForIdle(page);
+
+        await waitForMitoAILoadingToDisappear(page);
+
+        // Ensure the chat input is not focussed on 
+        await expect(page.locator('.chat-input')).not.toBeFocused();
+
+        // No code diffs should be visible before the user clicks preview
+        await expect(page.locator('.cm-codeDiffRemovedStripe')).not.toBeVisible();
+        await expect(page.locator('.cm-codeDiffInsertedStripe')).not.toBeVisible();
+
+        await clickPreviewButton(page);
+
+        await clickAcceptButton(page);
+        await waitForIdle(page);
+
+        const code = await getCodeFromCell(page, 0);
+        expect(code).toContain('print(1)');
+    });
+});
