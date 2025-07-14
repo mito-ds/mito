4f280b546d48d7c0cff19515bc9a1e7a76e8a9dc
Aaron Diamond-Reivich <aarondr77@gmail.com>
2025-07-14 14:57:00 -0400

    mito-ai: resolve eslint

--- mito-ai/src/Extensions/AiChat/jupyterSettingsManager.ts
+++ mito-ai/src/Extensions/AiChat/jupyterSettingsManager.ts
@@ -5,9 +5,10 @@ import { IDocumentManager } from "@jupyterlab/docmanager";
 const DOCMANAGER_PLUGIN_ID = '@jupyterlab/docmanager-extension:plugin';
 
 // Set renameUntitledFileOnSave to false when the extension loads
-export const setRenameUntitledFileOnSave = async (settingRegistry: ISettingRegistry, documentManager: IDocumentManager): Promise<void> => {
+export const setRenameUntitledFileOnSave = async (settingRegistry: ISettingRegistry, _documentManager: IDocumentManager): Promise<void> => {
     // Note we don't use the documentManager, but we require it as a parameter to make sure
     // that we leave the documentManager as a required token in the AiChatPlugin.ts file.
+    
     try {
         await settingRegistry.set(DOCMANAGER_PLUGIN_ID, 'renameUntitledFileOnSave', false);
     } catch (error) {
