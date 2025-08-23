981907a79091c32ec5abb2317f3f1e1ce2a449f8
PoojasreeDwarakanath <pdwaraka@usc.edu>
2025-08-22 19:29:36 -0700

    adding copyright

--- mito-ai/src/Extensions/AppManager/AppsList.tsx
+++ mito-ai/src/Extensions/AppManager/AppsList.tsx
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Saga Inc.
+ * Distributed under the terms of the GNU Affero General Public License v3.0 License.
+ */
+
 //app-list.tsx
 import * as React from 'react';
 import { copyIcon } from '@jupyterlab/ui-components';
--- mito-ai/src/Extensions/AppManager/ManageAppsPlugin.ts
+++ mito-ai/src/Extensions/AppManager/ManageAppsPlugin.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Saga Inc.
+ * Distributed under the terms of the GNU Affero General Public License v3.0 License.
+ */
+
 // ManageAppsPlugin.ts - Updated with IAppManagerService
 import {
   JupyterFrontEnd,
