a2cef858eb135c4e65aa269a2025e75f4c704202
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-09-30 11:28:07 -0400

    Removed .only

--- tests/mitoai_ui_tests/signup.spec.ts
+++ tests/mitoai_ui_tests/signup.spec.ts
@@ -67,7 +67,7 @@ test.describe.parallel('User Signup', () => {
         }
     });
 
-    test.only('User can send message after signing up', async ({ page }) => {
+    test('User can send message after signing up', async ({ page }) => {
         // First, we should see the signup form
         await expect(page.locator('[data-testid="signup-form-message"]')).toBeVisible();
 
