37d05bf41d8cb6532d371ff86caf11cc2c1131e3
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-10-08 11:54:12 -0400

    Updated test name

--- tests/mitoai_ui_tests/signup.spec.ts
+++ tests/mitoai_ui_tests/signup.spec.ts
@@ -90,7 +90,7 @@ test.describe.parallel('User Signup', () => {
         }
     });
 
-    test('User can send message after signing up', async ({ page }) => {
+    test('New users can send message after signing up', async ({ page }) => {
         // First, we should see the signup form
         await expect(page.locator('[data-testid="signup-form-message"]')).toBeVisible();
 
