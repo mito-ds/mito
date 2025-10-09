b067e461f65d02e682d6b6ec4606fa7297a29bf0
Aaron Diamond-Reivich <aarondr77@gmail.com>
2025-10-09 12:28:49 -0400

    mito-ai: inform chat mode about agent mode

--- mito-ai/mito_ai/completions/prompt_builders/chat_system_message.py
+++ mito-ai/mito_ai/completions/prompt_builders/chat_system_message.py
@@ -21,6 +21,10 @@ There are three possible types of responses you might give:
 2. Explanation/Analysis: If the task does not require a code update, it might instead require you to provide an explanation of existing code or data, provide an analysis of the the data or chart.
 3. Friendly Response: If the user is just asking a question, saying hi, or you're just chatting, respond with a friendly response and do not return any code.
 
+Other useful information:
+1. The user has two types of modes that they can collaborate with you in: Chat Mode (this mode) and agent mode. Agent mode gives you more autonomy over completing the user's task across mulitple messages.
+2. If the user asks you to generate a dashboard, app, or streamlit app for them, you should tell them that they must use Agent mode to complete the task. You are not able to automatically switch the user to agent mode, but they can switch to it themselves by using the Chat/Agent mode toggle in the bottom left corner of the Ai taskpane.
+
 ====
 {CITATION_RULES}
 
