bf186f0f60e070dfbb21851a818f7eb57d586908
Aaron Diamond-Reivich <aarondr77@gmail.com>
2025-11-04 10:37:13 -0500

    mito-ai: real uuid

--- mito-ai/src/utils/notebookMetadata.ts
+++ mito-ai/src/utils/notebookMetadata.ts
@@ -1,10 +1,14 @@
 import { NotebookPanel } from '@jupyterlab/notebook';
+import { UUID } from '@lumino/coreutils';
 
 const MITO_NOTEBOOK_ID = 'mito-notebook-id'
 
 export const setNotebookID = (notebookPanel: NotebookPanel): void => {
     console.log("SET NOTEBOOK ID")
-    notebookPanel.model?.setMetadata(MITO_NOTEBOOK_ID, 'test-notebook-id')
+    const MITO_NOTEBOOK_ID_PREFIX = 'mito-notebook'
+    // TODO: Generate a shorter UUID
+    const mitoNotebookID = `${MITO_NOTEBOOK_ID_PREFIX}-${UUID.uuid4()}`
+    notebookPanel.model?.setMetadata(MITO_NOTEBOOK_ID, mitoNotebookID)
 }
 
 export const getNotebookID = (notebookPanel: NotebookPanel): string => {
