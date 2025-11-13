ebf94085bdd3b95f7cff139531a22977fdbc7610
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-11-13 11:09:15 -0500

    Added with Open Graph meta tags

--- trymito.io/pages/_app.tsx
+++ trymito.io/pages/_app.tsx
@@ -6,9 +6,14 @@
 import '../styles/globals.css'
 import '../styles/margins.css'
 import type { AppProps } from 'next/app'
-import React, { useEffect } from 'react';
+import React from 'react';
 import Head from 'next/head';
 
+const DOMAIN = "trymito.io";
+const URL = `http://${DOMAIN}`;
+const TITLE = "Best Python Spreadsheet Automation & Code Generation | Mito";
+const DESCRIPTION = "Mito is the fastest way to do Python data science. Edit your data in a spreadsheet, and generate Python code automatically.";
+const IMAGE = "https://images.ctfassets.net/mmgv7yhaaeds/4x9blfyWFdoEphlq9tbJ2r/7a873e8c0dc5861a0f3f5801082436a4/social-sharing-home-new.png";
 
 function MyApp({ Component, pageProps }: AppProps) {
 
@@ -16,7 +21,24 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="viewport-fit=cover" />
-        <meta name="description" content="Mito is the fastest way to do Python data science. Edit your data in a spreadsheet, and generate Python code automatically."/>
+        <meta name="description" content={DESCRIPTION}/>
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:url" content={URL} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:image" content={IMAGE} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content={DOMAIN} />
+        <meta property="twitter:url" content={URL} />
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={DESCRIPTION} />
+        <meta name="twitter:image" content={IMAGE} />
       </Head>
       <Component {...pageProps} />
 
