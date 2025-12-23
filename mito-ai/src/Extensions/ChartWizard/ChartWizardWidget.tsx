f13eb7e20e9971b75dcce813cc2bd0044c5511ba
Nawaz Gafar <gafar.nawaz@gmail.com>
2025-12-23 10:36:08 -0500

    Added react widget

---
+++ mito-ai/src/Extensions/ChartWizard/ChartWizardWidget.tsx
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) Saga Inc.
+ * Distributed under the terms of the GNU Affero General Public License v3.0 License.
+ */
+
+import React from 'react';
+import { ReactWidget } from '@jupyterlab/apputils';
+
+/**
+ * A simple empty widget for the Chart Wizard panel.
+ */
+export class ChartWizardWidget extends ReactWidget {
+    constructor() {
+        super();
+        this.addClass('chart-wizard-widget');
+    }
+
+    render(): React.ReactElement {
+        return (
+            <div>
+                <h2>Chart Wizard</h2>
+                <p>Chart Wizard panel content will go here.</p>
+            </div>
+        );
+    }
+}
+
