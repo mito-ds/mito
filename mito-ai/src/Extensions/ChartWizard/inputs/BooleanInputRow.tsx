1e6204a598fa5fe22702c84809b1760497254fad
Nawaz Gafar <gafar.nawaz@gmail.com>
2026-01-07 11:47:55 -0500

    Styled toggle

--- mito-ai/src/Extensions/ChartWizard/inputs/BooleanInputRow.tsx
+++ mito-ai/src/Extensions/ChartWizard/inputs/BooleanInputRow.tsx
@@ -8,14 +8,19 @@ import { InputRowProps } from './types';
 
 export const BooleanInputRow: React.FC<InputRowProps> = ({ variable, label, onVariableChange }) => {
     return (
-        <div key={variable.name} className="chart-wizard-input-row">
-            <label className="chart-wizard-input-label">{label}</label>
-            <input
-                type="checkbox"
-                checked={variable.value as boolean}
-                onChange={(e) => onVariableChange(variable.name, e.target.checked)}
-                className="chart-wizard-checkbox"
-            />
+        <div key={variable.name} className="chart-wizard-input-row chart-wizard-boolean-row">
+            <label className="chart-wizard-input-label chart-wizard-boolean-label">
+                {label}
+            </label>
+            <label className="chart-wizard-toggle-container">
+                <input
+                    type="checkbox"
+                    checked={variable.value as boolean}
+                    onChange={(e) => onVariableChange(variable.name, e.target.checked)}
+                    className="chart-wizard-toggle-input"
+                />
+                <span className="chart-wizard-toggle-slider"></span>
+            </label>
         </div>
     );
 };
