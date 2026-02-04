c6887ada28a7ea10cf6509fc76d97da057e9da2f
Aaron Diamond-Reivich <aarondr77@gmail.com>
2026-02-04 14:13:14 -0500

    trymito: style HomneTagline section

--- trymito.io/components/HomeTagline/HomeTagline.tsx
+++ trymito.io/components/HomeTagline/HomeTagline.tsx
@@ -8,11 +8,11 @@ import taglineStyles from './HomeTagline.module.css';
 
 const TOOLTIPS = {
   yourSystems:
-    "Run Mito completely locally on your user's computers or through your existing JupyterHub. Configure it to use your API keys through your Azure OpenAI Enterprise account, LiteLLM or other preferred LLM provider.",
+    "Mito runs completely locally on your user's computers and is configurable to use your API keys with Azure, LiteLLM or other preferred LLM provider.",
   jupyter:
-    "Mito has a deep understanding of how to work with Jupyter notebooks unlike other AI tools which treat notebooks as second class. Because Mito is designed for Jupyter it is compatible with all of your existing Jupyter extensions and environments.",
+    "Mito is compatible with your existing Jupyter extensions and JupyterHub. And it knows how to edit notebook file formats unlike other AI tools.",
   everyLevel:
-    "Mito is used by everyone from citizen data scientists at the world's largest financial institutions who are automating Excel reporting with Python through ML researchers at labs who are making breakthrough discoveries with data.",
+    "Mito is used by everyone from citizen data scientists at the world's largest banks who are automating Excel reporting Python through ML researchers at labs.",
 };
 
 const HomeTagline = (): JSX.Element => (
@@ -25,21 +25,20 @@ const HomeTagline = (): JSX.Element => (
           {TOOLTIPS.yourSystems}
         </span>
       </span>
-      , designed for{' '}
+      , purpose built for{' '}
       <span className={taglineStyles.tooltipTrigger}>
         <span className={taglineStyles.decorativeTextPurple}>Jupyter</span>
         <span className={taglineStyles.tooltipContent} role="tooltip">
           {TOOLTIPS.jupyter}
         </span>
       </span>
-      , and works for{' '}
+      , and designed for{' '}
       <span className={taglineStyles.tooltipTrigger}>
-        <span className={taglineStyles.decorativeTextPurple}>every level</span>
+        <span className={taglineStyles.decorativeTextPurple}>citizen data scientists</span>
         <span className={taglineStyles.tooltipContent} role="tooltip">
           {TOOLTIPS.everyLevel}
         </span>
-      </span>
-      {' '}of Python.
+      </span>.
     </p>
   </div>
 );
